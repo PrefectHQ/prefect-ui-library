7dbe4d7493df850cad550a9d8688b4f5e8ae6905
Marichka Offen <40467112+marichka-offen@users.noreply.github.com>
2022-05-17 17:59:57 -0400

    Update demo/router/index.ts
    Co-authored-by: Nicholas Brown <znicholasbrown@gmail.com>

--- demo/router/index.ts
+++ demo/router/index.ts
@@ -4,29 +4,13 @@ import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router'
 
 const routeRecords: RouteRecordRaw[] = [
   {
-    name: 'flows',
-    path: '/flows',
-    component: () => import('../App.vue'),
-  },
-  {
-    name: 'deployments',
-    path: '/deployments',
-    component: () => import('../App.vue'),
-  },
-  {
-    name: 'queues',
-    path: '/queues',
-    component: () => import('../App.vue'),
-  },
-  {
-    name: 'settings',
-    path: '/settings',
-    component: () => import('../App.vue'),
-  },
-  {
     path: '/',
-    redirect: 'flows',
+    component: () => import('../App.vue'),
   },
+  {
+    path: '*',
+    redirect: '/'
+  }
 ]
 
 const router = createRouter({
