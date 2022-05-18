eaf677f5e2f72b89a7a6409e8edf0c26e52c72fd
Marichka Offen <marichka@prefect.io>
2022-05-18 10:16:02 -0400

    use classes

--- src/components/ContextSidebar.vue
+++ src/components/ContextSidebar.vue
@@ -1,25 +1,25 @@
 <template>
-  <p-context-sidebar>
+  <p-context-sidebar class="context-sidebar">
     <template #header>
       <slot name="header" />
     </template>
 
     <template #upper-links>
       <slot name="leading-content">
-        <hr class="border-none sm:h-0 sm:my-0 h-[1px] bg-slate-600 my-1 -mx-3">
+        <hr class="context-sidebar__divider context-sidebar__divider--mobile">
         <p-context-nav-item title="Flows" icon="Flow" to="/flows" />
 
-        <hr class="border-none sm:h-0 sm:my-0 h-[1px] bg-slate-600 my-1 -mx-3">
+        <hr class="context-sidebar__divider context-sidebar__divider--mobile">
         <p-context-nav-item title="Deployments" icon="LocationMarkerIcon" to="/deployments" />
 
-        <hr class="border-none sm:h-0 sm:my-0 h-[1px] bg-slate-600 my-1 -mx-3">
+        <hr class="context-sidebar__divider context-sidebar__divider--mobile">
         <p-context-nav-item title="Queues" icon="DatabaseIcon" to="/queues" />
       </slot>
     </template>
 
     <template #bottom-links>
       <slot name="trailing-content">
-        <hr class="border-none h-[1px] bg-slate-600 my-1 -mx-3">
+        <hr class="context-sidebar__divider">
         <p-context-nav-item title="Settings" icon="CogIcon" to="/settings" />
       </slot>
     </template>
@@ -29,3 +29,20 @@
 <script lang="ts" setup>
   import { PContextSidebar, PContextNavItem } from '@prefecthq/prefect-design'
 </script>
+
+<style>
+.context-sidebar__divider {
+  @apply
+  border-none
+  h-[1px]
+  bg-slate-600
+  my-1
+  -mx-3
+}
+
+.context-sidebar__divider--mobile {
+  @apply
+  sm:h-0
+  sm:my-0
+}
+</style>
