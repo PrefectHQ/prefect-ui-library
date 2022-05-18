6674a370bd21cdfe155529c4695aacb914f63cca
Craig Harshbarger <pleek91@gmail.com>
2022-05-18 13:45:49 -0500

    Add list components

--- demo/sections/ListsSection.vue
+++ demo/sections/ListsSection.vue
@@ -1,30 +1,12 @@
 <template>
   <Section heading="Lists">
-    <!--
-      <SubSection heading="Flow List">
-      <div class="grid gap-2">
-      <template v-for="flow in flows" :key="flow.id">
-      <FlowListItemInput v-model:selected="selected" :flow="flow" />
-      </template>
-      </div>
-      </SubSection>
-    -->
-
     <SubSection heading="Flow Run List" class="mt-auto">
-      <div class="grid gap-2">
-        <template v-for="flowRun in flowRuns" :key="flowRun.id">
-          <FlowRunListItemInput v-model:selected="selectedFlowRuns" :value="flowRun.id" :flow-run="flowRun" />
-        </template>
-      </div>
+      <FlowRunList v-model:selected="selectedFlowRuns" :flow-runs="flowRuns" />
       {{ selectedFlowRuns }}
     </SubSection>
 
     <SubSection heading="Task Run List" class="mt-auto">
-      <div class="grid gap-2">
-        <template v-for="taskRun in taskRuns" :key="taskRun.id">
-          <TaskRunListItemInput v-model:selected="selectedTaskRuns" :value="taskRun.id" :task-run="taskRun" />
-        </template>
-      </div>
+      <TaskRunList v-model:selected="selectedTaskRuns" :task-runs="taskRuns" />
       {{ selectedTaskRuns }}
     </SubSection>
   </Section>
@@ -34,12 +16,10 @@
   import { ref } from '@vue/reactivity'
   import Section from '../components/section.vue'
   import SubSection from '../components/subSection.vue'
-  import FlowListItemInput from '@/components/FlowListItemInput.vue'
-  import FlowRunListItemInput from '@/components/FlowRunListItemInput.vue'
-  import TaskRunListItemInput from '@/components/TaskRunListItemInput.vue'
+  import FlowRunList from '@/components/FlowRunList.vue'
+  import TaskRunList from '@/components/TaskRunList.vue'
   import { mocker } from '@/services'
 
-  const flows = mocker.createMany('flow', 3)
   const flowRuns = mocker.createMany('flowRun', 4)
   const taskRuns = mocker.createMany('taskRun', 4)
   const selectedFlowRuns = ref<string[]>([])
---
+++ src/components/TaskRunList.vue
@@ -0,0 +1,33 @@
+<template>
+  <VirtualScroller :items="taskRuns" class="task-run-list">
+    <template #default="{ item: taskRun }">
+      <TaskRunListItem v-model:selected="model" v-bind="{ taskRun, disabled }" />
+    </template>
+  </VirtualScroller>
+</template>
+
+<script lang="ts" setup>
+  import { computed } from 'vue'
+  import TaskRunListItem from './TaskRunListItem.vue'
+  import VirtualScroller from './VirtualScroller.vue'
+  import { TaskRun } from '@/models/TaskRun'
+
+  const props = defineProps<{
+    selected: string[] | null,
+    taskRuns: TaskRun[],
+    disabled?: boolean,
+  }>()
+
+  const emit = defineEmits<{
+    (event: 'update:selected', value: string[]): void,
+  }>()
+
+  const model = computed({
+    get() {
+      return props.selected ?? []
+    },
+    set(value: string[]) {
+      emit('update:selected', value)
+    },
+  })
+</script>
