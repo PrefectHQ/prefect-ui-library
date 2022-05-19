7497a0f81df6aea9c1b539284b3ed45ec0e33e29
Marichka Offen <marichka@prefect.io>
2022-05-19 13:56:21 -0400

    Set up demo for deployment meta well

--- demo/sections/MetaWellSection.vue
+++ demo/sections/MetaWellSection.vue
@@ -1,7 +1,11 @@
 <template>
   <Section heading="Meta Wells">
     <SubSection heading="Flow Meta Well">
-      <FlowMetaWell :flow-id="flowData.id" :created="flowData.created" :tags="flowData.tags" class="w-[250px]" />
+      <FlowMetaWell :flow-meta="flowData" class="w-[250px] border-2 border-slate-100" />
+    </SubSection>
+
+    <SubSection heading="Deployment Meta Well">
+      <DeploymentMetaWell :deployment-meta="deploymentData" class="w-[250px] border-2 border-slate-100" />
     </SubSection>
   </Section>
 </template>
@@ -9,10 +13,18 @@
     <script lang="ts" setup>
   import Section from '../components/section.vue'
   import SubSection from '../components/subSection.vue'
+  import DeploymentMetaWell from '@/components/DeploymentMetaWell.vue'
   import FlowMetaWell from '@/components/FlowMetaWell.vue'
 
   const flowData = {
-    id: '3fa85f64',
+    flowId: '3fa85f64',
+    created: '2022-04-27T21:22:32.524Z',
+    tags: ['db', 'etl', 'production', 'dev', 'marketing', 'sales', 'k8s', 'eks', 'staging'],
+  }
+
+  const deploymentData = {
+    deploymentId: '93cd33ac',
+    flowId: '74df52bg',
     created: '2022-04-27T21:22:32.524Z',
     tags: ['db', 'etl', 'production', 'dev', 'marketing', 'sales', 'k8s', 'eks', 'staging'],
   }
