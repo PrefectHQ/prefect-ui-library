2c4f6733e582be57224c6e41888f0e03e561cc4c
Marichka Offen <marichka@prefect.io>
2022-05-19 13:55:50 -0400

    Create Deployment meta well

---
+++ src/components/DeploymentMetaWell.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flow-meta-well">
+    <p-key-value label="Flow ID" :value="deploymentMeta.deploymentId" />
+    <p-key-value label="Flow ID" :value="deploymentMeta.flowId" />
+    <p-key-value label="Created" :value="deploymentMeta.created" />
+    <p-key-value label="Tags">
+      <template #value>
+        <p-tag v-for="tag in deploymentMeta.tags" :key="tag" class="flow-meta-well__tags">
+          {{ tag }}
+        </p-tag>
+      </template>
+    </p-key-value>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { PKeyValue } from '@prefecthq/prefect-design'
+  import { DeploymentMeta } from '@/types/meta'
+
+  defineProps<{
+    deploymentMeta: DeploymentMeta,
+  }>()
+</script>
+
+<style>
+  .flow-meta-well {
+    @apply
+    flex
+    flex-col
+    gap-3
+  }
+
+  .flow-meta-well__tags {
+    @apply
+    mb-1
+    mr-1
+  }
+</style>
