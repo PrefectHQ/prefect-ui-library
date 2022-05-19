7c9ad9f88ffa2dba62800cf22f5355fa1c0ed8c1
Marichka Offen <marichka@prefect.io>
2022-05-19 14:52:56 -0400

    Create FlowRunMetatype

--- src/types/meta.ts
+++ src/types/meta.ts
@@ -1,3 +1,5 @@
+import { IState } from '@/models'
+
 export type FlowMeta = {
   flowId: string,
   created: string,
@@ -8,5 +10,22 @@ export type DeploymentMeta = {
   deploymentId: string,
   flowId: string,
   created: string,
+  tags: string[],
+}
+
+export type FlowRunMeta = {
+  stateBadge: IState,
+  duration: string,
+  startTime: string,
+  flowName: string,
+  deploymentName: string,
+  flowRunId: string,
+  flowId: string,
+  deploymentId: string,
+  created: string,
+  flowVersion: string,
+  idempotencyKey: string,
+  runCount: number,
+  flowRunner: string,
   tags: string[],
 }
