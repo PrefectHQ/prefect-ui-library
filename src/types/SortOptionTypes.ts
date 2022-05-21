d52681d6111d364ffafa1fd756e1a798f5db694c
Jennifer Grange <jenny@prefect.io>
2022-05-21 16:29:53 -0400

    add sort option types

---
+++ src/types/SortOptionTypes.ts
@@ -0,0 +1,3 @@
+export type FlowRunSortKeys = 'NAME_DESC' | 'NAME_ASC' | 'EXPECTED_START_TIME_DESC' | 'EXPECTED_START_TIME_ASC' | 'NEXT_SCHEDULED_START_TIME_ASC'
+
+export type FlowRunSortOptions = { label: string, value: FlowRunSortKeys }[]
