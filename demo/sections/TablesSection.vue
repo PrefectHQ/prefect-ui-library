0483d03bb621b631c1c34f264d6fdbacebffeef6
Marichka Offen <marichka@prefect.io>
2022-05-23 17:00:59 -0400

    Create deployments table and demo for it

--- demo/sections/TablesSection.vue
+++ demo/sections/TablesSection.vue
@@ -3,14 +3,20 @@
     <DemoSubSection heading="Flows table">
       <FlowsTable :flows="flowsData" />
     </DemoSubSection>
+
+    <DemoSubSection heading="Deployments table">
+      <DeploymentsTable :deployments="deploymentsData" />
+    </DemoSubSection>
   </DemoSection>
 </template>
 
 <script lang="ts" setup>
   import DemoSection from '../components/DemoSection.vue'
   import DemoSubSection from '../components/DemoSubSection.vue'
+  import DeploymentsTable from '@/components/DeploymentsTable.vue'
   import FlowsTable from '@/components/FlowsTable.vue'
   import { mocker } from '@/services'
 
   const flowsData = mocker.createMany('flow', 3)
+  const deploymentsData = mocker.createMany('deployment', 3)
 </script>
---
+++ src/components/DeploymentsTable.vue
@@ -0,0 +1,40 @@
+<template>
+  <p-table :data="deployments" :columns="columns">
+    <template #tags="{ row }">
+      <p-tag-wrapper :tags="row.tags" justify="left" />
+    </template>
+    <template #action>
+      No Actions
+    </template>
+  </p-table>
+</template>
+
+<script lang="ts" setup>
+  import { PTable, PTagWrapper } from '@prefecthq/prefect-design'
+  import { Deployment } from '@/models'
+
+  defineProps<{
+    deployments: Deployment[],
+  }>()
+
+  const columns = [
+    {
+      property: 'name',
+      label: 'Name',
+      width: '150px',
+    },
+    {
+      property: 'schedule',
+      label: 'Schedule',
+      width: '200px',
+    },
+    {
+      property: 'tags',
+      label: 'Tags',
+    },
+    {
+      label: 'Action',
+      width: '100px',
+    },
+  ]
+</script>
