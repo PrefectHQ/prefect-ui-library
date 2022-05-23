a80a2d0b3863ccae168f007e78905bc2f50f4e61
Jennifer Grange <jenny@prefect.io>
2022-05-23 14:43:04 -0400

    make settings type exportable for use in orion settings page

--- src/services/AdminApi.ts
+++ src/services/AdminApi.ts
@@ -1,4 +1,4 @@
-import { ISettingsResponse } from '@/models/ISettingsResponse'
+import { Settings } from '@/models/Settings'
 import { Api, ApiRoute } from '@/services/Api'
 
 
@@ -6,8 +6,8 @@ export class AdminApi extends Api {
 
   protected override route: ApiRoute = '/admin'
 
-  public getSettings(): Promise<ISettingsResponse> {
-    return this.get<ISettingsResponse>('/settings').then(({ data }) => data)
+  public getSettings(): Promise<Settings> {
+    return this.get<Settings>('/settings').then(({ data }) => data)
   }
 
   public async getVersion(): Promise<string> {
