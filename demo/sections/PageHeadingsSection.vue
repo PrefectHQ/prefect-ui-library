4106848072d89cd1514fb893dd7ccbb05c241eb1
nicholas <znicholasbrown@gmail.com>
2022-05-23 15:20:58 -0400

    Add flow runs component

--- demo/sections/PageHeadingsSection.vue
+++ demo/sections/PageHeadingsSection.vue
@@ -1,9 +1,10 @@
 <template>
   <DemoSection heading="Page headings">
-    <DemoSubSection heading="List Pages">
+    <DemoSubSection heading="Top-level Pages">
       <page-heading-flows />
       <page-heading-deployments />
       <page-heading-queues />
+      <page-heading-flow-runs />
     </DemoSubSection>
   </DemoSection>
 </template>
@@ -12,6 +13,7 @@
   import DemoSection from '../components/DemoSection.vue'
   import DemoSubSection from '../components/DemoSubSection.vue'
   import PageHeadingDeployments from '@/components/PageHeadingDeployments.vue'
+  import PageHeadingFlowRuns from '@/components/PageHeadingFlowRuns.vue'
   import PageHeadingFlows from '@/components/PageHeadingFlows.vue'
   import PageHeadingQueues from '@/components/PageHeadingQueues.vue'
 </script>
---
+++ src/components/PageHeadingFlowRuns.vue
@@ -0,0 +1,9 @@
+<template>
+  <page-heading :crumbs="crumbs" />
+</template>
+
+<script lang="ts" setup>
+  import PageHeading from '@/components/PageHeading.vue'
+
+  const crumbs = [{ text: 'Runs' }]
+</script>
