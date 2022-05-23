4b9efa34821f4a64990513a1fd91af263230cff9
Marichka Offen <marichka@prefect.io>
2022-05-23 17:14:53 -0400

    create queuesTable component and demo for it

--- demo/sections/TablesSection.vue
+++ demo/sections/TablesSection.vue
@@ -7,6 +7,10 @@
     <DemoSubSection heading="Deployments table">
       <DeploymentsTable :deployments="deploymentsData" />
     </DemoSubSection>
+
+    <DemoSubSection heading="Deployments table">
+      <QueuesTable :queues="queuesData" />
+    </DemoSubSection>
   </DemoSection>
 </template>
 
@@ -15,8 +19,10 @@
   import DemoSubSection from '../components/DemoSubSection.vue'
   import DeploymentsTable from '@/components/DeploymentsTable.vue'
   import FlowsTable from '@/components/FlowsTable.vue'
+  import QueuesTable from '@/components/QueuesTable.vue'
   import { mocker } from '@/services'
 
   const flowsData = mocker.createMany('flow', 3)
   const deploymentsData = mocker.createMany('deployment', 3)
+  const queuesData = mocker.createMany('workQueue', 3)
 </script>
---
+++ src/components/QueuesTable.vue
@@ -0,0 +1,32 @@
+<template>
+  <p-table :data="queues" :columns="columns">
+    <template #action>
+      No Actions
+    </template>
+  </p-table>
+</template>
+
+<script lang="ts" setup>
+  import { PTable } from '@prefecthq/prefect-design'
+  import { WorkQueue } from '@/models'
+
+  defineProps<{
+    queues: WorkQueue[],
+  }>()
+
+  const columns = [
+    {
+      property: 'name',
+      label: 'Name',
+      width: '150px',
+    },
+    {
+      property: 'concurrencyLimit',
+      label: 'Concurrency',
+    },
+    {
+      label: 'Action',
+      width: '100px',
+    },
+  ]
+</script>
