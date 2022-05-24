81fbebccf08bd19227aba21f9b4f23451bd7e9b1
Jennifer Grange <jenny@prefect.io>
2022-05-24 12:23:08 -0400

    add to components

--- demo/pages/Components.vue
+++ demo/pages/Components.vue
@@ -8,6 +8,7 @@
     <TablesSection />
     <ToggleSection />
     <MetaWellSection />
+    <SortSections />
   </p-layout-default>
 </template>
 
@@ -18,6 +19,7 @@
   import ListsSection from '../sections/ListsSection.vue'
   import LogSection from '../sections/LogsSection.vue'
   import MetaWellSection from '../sections/MetaWellSection.vue'
+  import SortSections from '../sections/SortSections.vue'
   import StatesSection from '../sections/StatesSection.vue'
   import TablesSection from '../sections/TablesSection.vue'
   import ToggleSection from '../sections/ToggleSection.vue'
