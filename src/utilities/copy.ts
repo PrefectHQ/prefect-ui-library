5e8b83a0d4e758cf9045ee5b3c155c42d9d0f35d
Marichka Offen <marichka@prefect.io>
2022-05-24 17:46:50 -0400

    Update copy.ts

--- src/utilities/copy.ts
+++ src/utilities/copy.ts
@@ -1,7 +1,7 @@
 import { showToast } from '@prefecthq/prefect-design'
 
-export async function copyId(id: string): Promise<void> {
-  await navigator.clipboard.writeText(id)
+export function copyId(id: string): void {
+  navigator.clipboard.writeText(id)
 
   showToast('ID copied to clipboard!', 'success', undefined, 3000)
 }
