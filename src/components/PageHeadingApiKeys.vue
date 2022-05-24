c59bc1cbc9884559111a6057389c0d0576fd1c19
nicholas <znicholasbrown@gmail.com>
2022-05-24 09:02:29 -0400

    Add API Keys page heading

---
+++ src/components/PageHeadingApiKeys.vue
@@ -0,0 +1,11 @@
+<template>
+  <page-heading class="page-heading-api-keys" :crumbs="crumbs">
+    <p-button inset size="xs" icon="PlusIcon" />
+  </page-heading>
+</template>
+
+<script lang="ts" setup>
+  import PageHeading from '@/components/PageHeading.vue'
+
+  const crumbs = [{ text: 'API Keys' }]
+</script>
