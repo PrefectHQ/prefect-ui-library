0792ea6c822af71fe3f025acdf32fb2925c350f2
Jennifer Grange <jenny@prefect.io>
2022-05-24 12:19:13 -0400

    fix app merge

--- demo/App.vue
+++ demo/App.vue
@@ -1,29 +1,7 @@
 <template>
-  <div class="p-10">
-    <div class="md:flex md:items-center md:justify-between">
-      <div class="flex-1 min-w-0">
-        <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
-          Orion Design
-        </h2>
-      </div>
-      <div class="mt-4 flex md:mt-0 md:ml-4">
-        <a href="https://github.com/PrefectHQ/orion-design" class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-          Github
-        </a>
-      </div>
-    </div>
-
-    <StatesSection />
-    <LogSection />
-    <EmptyStateSection />
-    <ContextSidebarSection />
-    <ListsSection />
-    <MetaWellSection />
-    <SortSection />
-    <div class="app">
-      <app-sidebar />
-      <router-view />
-    </div>
+  <div class="app">
+    <app-sidebar />
+    <router-view />
   </div>
 </template>
 
