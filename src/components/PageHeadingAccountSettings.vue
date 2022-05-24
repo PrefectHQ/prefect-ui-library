0ce5fba1f4cd0ba8de9ddb260608ccdf76373538
nicholas <znicholasbrown@gmail.com>
2022-05-24 09:04:05 -0400

    Add account settints heading

---
+++ src/components/PageHeadingAccountSettings.vue
@@ -0,0 +1,9 @@
+<template>
+  <page-heading class="page-heading-user-settings" :crumbs="crumbs" />
+</template>
+
+<script lang="ts" setup>
+  import PageHeading from '@/components/PageHeading.vue'
+
+  const crumbs = [{ text: 'Account Settings' }]
+</script>
