2b5998f464aac2b30e71e324f25beaf7df65b13e
Jennifer Grange <jenny@prefect.io>
2022-05-24 15:28:22 -0400

    add task run sort component and types

---
+++ src/components/TaskRunsSort.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="task-runs-sort">
+    <p-select v-model="internalValue" :options="taskRunSortOptions" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { PSelect, SelectModelValue } from '@prefecthq/prefect-design'
+  import { computed } from 'vue'
+  import { TaskRunSortOptions } from  '../types/SortOptionTypes'
+
+  const props = defineProps<{
+    modelValue: string,
+  }>()
+
+  const emits = defineEmits<{
+    (event: 'update:modelValue', value: SelectModelValue): void,
+  }>()
+
+  const internalValue = computed({
+    get() {
+      return props.modelValue
+    },
+    set(value: SelectModelValue) {
+      emits('update:modelValue', value)
+    },
+  })
+
+  const taskRunSortOptions: TaskRunSortOptions = [
+    { label: 'Newest to Oldest', value: 'EXPECTED_START_TIME_DESC' },
+    { label: 'Oldest to Newest', value: 'EXPECTED_START_TIME_ASC' },
+  ]
+</script>
+
+<style>
+  .task-runs-sort{}
+</style>
--- src/types/SortOptionTypes.ts
+++ src/types/SortOptionTypes.ts
@@ -2,3 +2,7 @@ export type FlowRunSortValues = 'NAME_DESC' | 'NAME_ASC' | 'EXPECTED_START_TIME_
 
 export type FlowRunSortOptions = { label: string, value: FlowRunSortValues }[]
 
+export type TaskRunSortValues = 'ID_DESC'| 'EXPECTED_START_TIME_ASC' | 'EXPECTED_START_TIME_DESC' | 'NEXT_SCHEDULED_START_TIME_ASC' | 'END_TIME_DESC'
+
+export type TaskRunSortOptions = { label: string, value: TaskRunSortValues }[]
+
