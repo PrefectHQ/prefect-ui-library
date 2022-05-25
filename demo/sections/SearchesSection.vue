0a2a3611fbe61bc07e40759c62fcade4dc986160
Jennifer Grange <jenny@prefect.io>
2022-05-25 14:34:39 -0400

    export, add to demo and update types

---
+++ demo/sections/SearchesSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <DemoSection heading="Searches">
+    <DemoSubSection heading="Flow Runs Search">
+      <FlowRunsSearch v-model="flowRunSearchInput" />
+    </DemoSubSection>
+  </DemoSection>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue'
+  import DemoSection from '../components/DemoSection.vue'
+  import DemoSubSection from '../components/DemoSubSection.vue'
+  import { FlowRunsSearch } from '@/components'
+
+  const flowRunSearchInput = ref<string | null | undefined>(null)
+</script>
--- src/components/FlowRunsSearch.vue
+++ src/components/FlowRunsSearch.vue
@@ -15,7 +15,7 @@
 
 
   const props = defineProps<{
-    modelValue: string,
+    modelValue: string | null | undefined,
   }>()
 
   const emits = defineEmits<{
