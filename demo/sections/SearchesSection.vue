7a2bb3334b6dfd339305e50a2597a5383d596943
Jennifer Grange <jenny@prefect.io>
2022-05-26 14:36:25 -0400

    add flow search demo

--- demo/sections/SearchesSection.vue
+++ demo/sections/SearchesSection.vue
@@ -1,7 +1,10 @@
 <template>
   <DemoSection heading="Searches">
-    <DemoSubSection heading="Flow Runs Search">
+    <DemoSubSection heading="Runs Search">
       <SearchInput v-model="runSearchInput" />
+    </DemoSubSection>
+    <DemoSubSection heading="Flows Search">
+      <FlowSearch v-model="flowSearchInput" />
     </DemoSubSection>
   </DemoSection>
 </template>
@@ -10,7 +13,12 @@
   import { ref } from 'vue'
   import DemoSection from '../components/DemoSection.vue'
   import DemoSubSection from '../components/DemoSubSection.vue'
-  import { SearchInput } from '@/components'
+  import { SearchInput, FlowSearch } from '@/components'
+  import { Flow } from '@/models'
+  import { mocker } from '@/services'
+
+  const flowsData = mocker.createMany('flow', 8)
 
   const runSearchInput = ref<string | null | undefined>(null)
+  const flowSearchInput = ref<Flow[]>(flowsData)
 </script>
