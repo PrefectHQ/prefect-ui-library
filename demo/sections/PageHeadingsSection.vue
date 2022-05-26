cb113fd1a121be23ba9b50b86dd9d5a5ffc6fee6
Marichka Offen <marichka@prefect.io>
2022-05-26 11:36:31 -0400

    Create demo of refresh

--- demo/sections/PageHeadingsSection.vue
+++ demo/sections/PageHeadingsSection.vue
@@ -11,15 +11,16 @@
     </DemoSubSection>
 
     <DemoSubSection heading="Object Pages">
-      <page-heading-flow :flow="flow" />
-      <page-heading-deployment :deployment="deployment" />
-      <page-heading-queue :queue="queue" />
-      <page-heading-flow-run :flow-run="flowRun" />
+      <page-heading-flow :flow="flow" @refresh="router.push('charts')" />
+      <page-heading-deployment :deployment="deployment" @refresh="router.push('charts')" />
+      <page-heading-queue :queue="queue" @refresh="router.push('charts')" />
+      <page-heading-flow-run :flow-run="flowRun" @refresh="router.push('charts')" />
     </DemoSubSection>
   </DemoSection>
 </template>
 
 <script lang="ts" setup>
+  import { useRouter } from 'vue-router'
   import DemoSection from '../components/DemoSection.vue'
   import DemoSubSection from '../components/DemoSubSection.vue'
   import PageHeadingAccountSettings from '@/components/PageHeadingAccountSettings.vue'
@@ -34,6 +35,8 @@
   import PageHeadingQueues from '@/components/PageHeadingQueues.vue'
   import PageHeadingWorkspaceSettings from '@/components/PageHeadingWorkspaceSettings.vue'
   import { mocker } from '@/services'
+
+  const router = useRouter()
 
   const flow = mocker.create('flow', [{ name: 'ETL' }])
   const deployment = mocker.create('deployment', [{ name: 'Production' }])
