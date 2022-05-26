6a9b975cba1095acd63ec4ee5175928b9cbf5103
Marichka Offen <marichka@prefect.io>
2022-05-26 12:06:44 -0400

    Set up delete for PageHeadingQueue

--- src/components/PageHeadingQueue.vue
+++ src/components/PageHeadingQueue.vue
@@ -7,7 +7,7 @@
         <template #default="{ close }">
           <copy-overflow-menu-item label="Copy ID" :item="queue.id" @click="close" />
           <p-overflow-menu-item label="Edit" />
-          <p-overflow-menu-item label="Delete" />
+          <delete-overflow-menu-item :name="queue.name" @remove="deleteWorkQueue(queue.id)" />
         </template>
       </p-icon-button-menu>
     </template>
@@ -15,16 +15,34 @@
 </template>
 
 <script lang="ts" setup>
-  import { PIconButtonMenu } from '@prefecthq/prefect-design'
+  import { PIconButtonMenu, showToast } from '@prefecthq/prefect-design'
   import { computed } from 'vue'
   import CopyOverflowMenuItem from './CopyOverflowMenuItem.vue'
+  import DeleteOverflowMenuItem from './DeleteOverflowMenuItem.vue'
   import PageHeading from '@/components/PageHeading.vue'
   import WorkQueueToggle from '@/components/WorkQueueToggle.vue'
   import { WorkQueue } from '@/models'
+  import { workQueuesApiKey } from '@/services/WorkQueuesApi'
+  import { inject } from '@/utilities/inject'
 
   const props = defineProps<{
     queue: WorkQueue,
   }>()
 
+  const workQueuesApi = inject(workQueuesApiKey)
+
   const crumbs = computed(() => [{ text: props.queue.name }])
+
+  const emit = defineEmits(['refresh'])
+
+  const deleteWorkQueue = async (id: string): Promise<void> => {
+    try {
+      await workQueuesApi.deleteWorkQueue(id)
+      showToast('Work queue deleted successfully!', 'success', undefined, 3000)
+      emit('refresh')
+    } catch (error) {
+      showToast('Failed to delete work queue', 'error', undefined, 3000)
+      console.error(error)
+    }
+  }
 </script>
