0b4ac5a1f12f50b584007aa71990740f5b160c84
Marichka Offen <marichka@prefect.io>
2022-05-26 11:52:13 -0400

    Set up delete for PageHeadingFlow

--- src/components/PageHeadingFlow.vue
+++ src/components/PageHeadingFlow.vue
@@ -4,7 +4,7 @@
       <p-icon-button-menu>
         <template #default="{ close }">
           <copy-overflow-menu-item label="Copy ID" :item="flow.id" @click="close" />
-          <p-overflow-menu-item label="Delete" />
+          <delete-overflow-menu-item :name="flow.name" @remove="deleteFlow(flow.id)" />
         </template>
       </p-icon-button-menu>
     </template>
@@ -12,15 +12,33 @@
 </template>
 
 <script lang="ts" setup>
-  import { PIconButtonMenu } from '@prefecthq/prefect-design'
+  import { PIconButtonMenu, showToast } from '@prefecthq/prefect-design'
   import { computed } from 'vue'
   import CopyOverflowMenuItem from './CopyOverflowMenuItem.vue'
+  import DeleteOverflowMenuItem from './DeleteOverflowMenuItem.vue'
   import PageHeading from '@/components/PageHeading.vue'
   import { Flow } from '@/models'
+  import { flowsApiKey } from '@/services/FlowsApi'
+  import { inject } from '@/utilities/inject'
 
   const props = defineProps<{
     flow: Flow,
   }>()
 
+  const flowsApi = inject(flowsApiKey)
+
   const crumbs = computed(() => [{ text: props.flow.name }])
+
+  const emit = defineEmits(['refresh'])
+
+  const deleteFlow = async (id: string): Promise<void> => {
+    try {
+      await flowsApi.deleteFlow(id)
+      showToast('Flow deleted successfully!', 'success', undefined, 3000)
+      emit('refresh')
+    } catch (error) {
+      showToast('Failed to delete flow', 'error', undefined, 3000)
+      console.error(error)
+    }
+  }
 </script>
