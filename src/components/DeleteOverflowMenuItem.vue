9c8207fb3b1081036b0baf712b19f374562a7558
Marichka Offen <marichka@prefect.io>
2022-05-26 12:28:26 -0400

    Update DeleteOverflowMenuItem with classes

--- src/components/DeleteOverflowMenuItem.vue
+++ src/components/DeleteOverflowMenuItem.vue
@@ -1,35 +1,37 @@
 <template>
-  <p-overflow-menu-item label="Delete" @click="showModal = true" />
+  <div class="delete-overflow-menu-item">
+    <p-overflow-menu-item label="Delete" @click="showModal = true" />
 
-  <p-modal
-    v-model:showModal="showModal"
-  >
-    <template #title>
-      <div class="flex gap-2 items-center pb-2">
-        <p-icon icon="ExclamationCircleIcon" class="h-6 w-6 stroke-red-500" />
-        <span class="font-bold text-xl">Delete {{ name }}</span>
-        <p-icon icon="XIcon" class="h-6 w-6 ml-auto stroke-gray-400 cursor-pointer" @click="showModal = false" />
-      </div>
-      <hr
-        class="border-t-1 border-gray-300 -mx-6 my-2"
-      >
-    </template>
+    <p-modal
+      v-model:showModal="showModal"
+    >
+      <template #title>
+        <div class="delete-overflow-menu-item__title">
+          <p-icon icon="ExclamationCircleIcon" class="delete-overflow-menu-item__title-icon" />
+          <span class="delete-overflow-menu-item__title-name">Delete {{ name }}</span>
+          <p-icon icon="XIcon" class="delete-overflow-menu-item__close-icon" @click="showModal = false" />
+        </div>
+        <hr
+          class="delete-overflow-menu-item__divider"
+        >
+      </template>
 
-    <template #content>
-      <slot name="message">
-        <p class="text-red-500 font-bold">
-          Are you sure you want to delete {{ name }}? This action cannot be undone.
-        </p>
-      </slot>
-      <slot name="details" />
-    </template>
+      <template #content>
+        <slot name="message">
+          <p class="delete-overflow-menu-item__message">
+            Are you sure you want to delete {{ name }}? This action cannot be undone.
+          </p>
+        </slot>
+        <slot name="details" />
+      </template>
 
-    <template #actions>
-      <p-button class="w-full bg-red-500 inline-flex justify-center sm:w-auto sm:text-sm" @click="remove">
-        Delete
-      </p-button>
-    </template>
-  </p-modal>
+      <template #actions>
+        <p-button class="delete-overflow-menu-item__delete-button" @click="remove">
+          Delete
+        </p-button>
+      </template>
+    </p-modal>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -48,3 +50,61 @@
     showModal.value = false
   }
 </script>
+
+<style>
+.delete-overflow-menu-item__title {
+  @apply
+  flex
+  gap-2
+  items-center
+  pb-2
+}
+
+.delete-overflow-menu-item__title-icon {
+  @apply
+  h-6
+  w-6
+  stroke-red-500
+}
+
+.delete-overflow-menu-item__title-name {
+  @apply
+  font-bold
+  text-xl
+}
+
+.delete-overflow-menu-item__close-icon {
+  @apply
+  h-6
+  w-6
+  ml-auto
+  stroke-gray-400
+  cursor-pointer
+}
+
+.delete-overflow-menu-item__divider {
+  @apply
+  border-gray-300
+  -mx-6
+  my-2
+  border-t-[1px]
+}
+
+.delete-overflow-menu-item__message {
+  @apply
+  text-red-500
+  font-bold
+}
+
+.delete-overflow-menu-item__delete-button {
+  @apply
+  w-full
+  bg-red-500
+  inline-flex
+  justify-center
+  sm:w-auto
+  sm:text-sm
+  focus:ring-red-600
+  hover:!bg-red-700
+}
+</style>
