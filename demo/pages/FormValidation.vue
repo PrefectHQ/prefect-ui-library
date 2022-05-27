c9b1e1607cb7d20b7a2f9020127590f3b1203e20
Evan Sutherland <stackoverfloweth@gmail.com>
2022-05-27 17:46:53 -0500

    added validate service with form-validation example using vee-validate

--- demo/pages/FormValidation.vue
+++ demo/pages/FormValidation.vue
@@ -1,57 +1,92 @@
 <template>
-  <p-form class="form-validation" @submit="submit">
+  <p-form class="form-validation" :loading="isSubmitting" @submit="submit" @cancel="handleReset">
     <template #heading>
-      Form with Vee Validation!
+      Form Validation Example
     </template>
 
-    <p-label label="First Name">
-      <p-text-input v-model="firstName" />
+    <p-label label="Name" :message="errors.name" :state="nameState">
+      <p-text-input v-model="name" :state="nameState" />
     </p-label>
 
-    <p-label label="Last Name">
-      <p-text-input v-model="lastName" />
+    <p-label label="Email" :message="errors.emailAddress" :state="emailAddressState">
+      <p-text-input v-model="emailAddress" :state="emailAddressState" />
     </p-label>
 
-    <p-label label="Birthday">
-      <p-date-input v-model="dateOfBirth" />
+    <p-label label="Birthday" :message="errors.dateOfBirth" :state="dateOfBirthState">
+      <p-date-input v-model="dateOfBirth" :state="dateOfBirthState" />
     </p-label>
 
-    <p-label label="Email">
-      <p-text-input v-model="emailAddress" />
+    <p-label label="Username (async)" :message="errors.username" :state="usernameState">
+      <p-text-input v-model="username" :state="usernameState" />
     </p-label>
 
-    <p-label label="Username (async)">
-      <p-text-input v-model="username" />
+    <p-label label="Favorite Tags" :message="errors.tags" :state="tagsState">
+      <p-tags-input v-model="tags" :state="tagsState" />
     </p-label>
 
-    <p-label label="Credit Card">
-      <p-text-input v-model="creditCardNumber" />
-    </p-label>
-
-    <p-label label="I agree to the terms and conditions.">
+    <p-label label="I agree to the terms and conditions." :message="errors.agreeToTerms" :state="agreeToTermsState">
       <p-checkbox v-model="agreeToTerms" />
     </p-label>
 
-    <code class="form-validation__json">
+    <code class="form-validation__json" :class="classes">
       <!-- eslint-disable-next-line vue/no-v-html -->
-      <pre v-html="JSON.stringify({ firstName, lastName, dateOfBirth }, null, 2)" />
+      <pre v-html="JSON.stringify({ name, dateOfBirth, emailAddress, username, tags, agreeToTerms }, null, 2)" />
     </code>
   </p-form>
 </template>
 
 <script lang="ts" setup>
-  import { ref } from 'vue'
+  import { addYears, format } from 'date-fns'
+  import { useForm, useField } from 'vee-validate'
+  import { computed } from 'vue'
+  import { isEmail, isRequired, withMessage, lessThan } from '@/services/validate'
 
+  const { handleSubmit, handleReset, isSubmitting, meta, errors, submitCount } = useForm()
 
-  const submit = (values: unknown): void => console.log(values)
+  const submit = handleSubmit(values => console.log(values))
 
-  const firstName = ref<string>()
-  const lastName = ref<string>()
-  const dateOfBirth = ref<Date>()
-  const emailAddress = ref<string>()
-  const username = ref<string>()
-  const agreeToTerms = ref<boolean>()
-  const creditCardNumber = ref<string>()
+  async function validateEmail(value: string): Promise<boolean> {
+    return await mockServerUsernameCheck(value)
+  }
+
+  function mockServerUsernameCheck(value: string): Promise<boolean> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(value === 'stackoverfloweth')
+      }, 2000)
+    })
+  }
+
+  const minDate = addYears(new Date(), -18)
+  const rules = {
+    name: [withMessage(isRequired, 'Name is Required')],
+    dateOfBirth: [
+      withMessage(isRequired, 'Birthday is Required'),
+      withMessage(lessThan(minDate), `Must be at least 18 years old (${format(minDate, 'MM/dd/yyyy')})`),
+    ],
+    emailAddress: [
+      withMessage(isRequired, 'Email Address is required'),
+      withMessage(isEmail, 'Email Address is invalid'),
+    ],
+    username: [
+      withMessage(isRequired, 'Username is Required'),
+      withMessage(validateEmail, 'Username is already taken (try stackoverfloweth)'),
+    ],
+    tags: [withMessage(isRequired, 'At least 1 tag is required')],
+    agreeToTerms: [withMessage(isRequired, 'Compliance is required')],
+  }
+
+  const { value:name, meta:nameState } = useField<string>('name', rules.name)
+  const { value:dateOfBirth, meta:dateOfBirthState } = useField<Date>('dateOfBirth', rules.dateOfBirth)
+  const { value:emailAddress, meta:emailAddressState } = useField<string>('emailAddress', rules.emailAddress)
+  const { value:username, meta:usernameState } = useField<string>('username', rules.username)
+  const { value:tags, meta:tagsState } = useField<string[]>('tags', rules.tags, { initialValue: [] })
+  const { value:agreeToTerms, meta:agreeToTermsState } = useField<boolean>('agreeToTerms', rules.agreeToTerms)
+
+  const classes = computed(() => ({
+    'form-validation__json--failed': !meta.value.valid && submitCount.value > 0,
+    'form-validation__json--passed': meta.value.valid && submitCount.value > 0,
+  }))
 </script>
 
 <style>
@@ -66,5 +101,17 @@
   border-prefect-400
   text-prefect-800
   p-4
+}
+
+.form-validation__json--failed { @apply
+  bg-red-200
+  border-red-400
+  text-red-800
+}
+
+.form-validation__json--passed { @apply
+  bg-green-200
+  border-green-400
+  text-green-800
 }
 </style>
---
+++ src/services/validate.ts
@@ -0,0 +1,126 @@
+/* eslint-disable no-redeclare */
+import { isDateAfter, isDateAfterOrEqual, isDateBefore, isDateBeforeOrEqual } from '@prefecthq/prefect-design'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type ValidationValue = any
+type ValidateMethod = (value: ValidationValue) => boolean
+
+function isDate(value: ValidationValue): value is Date {
+  return typeof value.getTime === 'function'
+}
+
+export function withMessage(validate: (value: ValidationValue) => boolean | Promise<boolean>, message: string) {
+  return async (value: ValidationValue) => await validate(value) ? true : message
+}
+
+export function isRequired(value: ValidationValue): boolean {
+  if (!value) {
+    return false
+  }
+
+  if (typeof value === 'string' && value.trim().length === 0) {
+    return false
+  }
+
+  if (Array.isArray(value) && value.length === 0) {
+    return false
+  }
+
+  return true
+}
+
+export function isEmail(value: ValidationValue): boolean {
+  if (!value || typeof value !== 'string') {
+    return false
+  }
+
+  return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value)
+}
+
+export function lessThan(max: ValidationValue): ValidateMethod {
+  return (value) => {
+    if (!value) {
+      return false
+    }
+
+    if (typeof value === 'string' && typeof max === 'number') {
+      return value.length < max
+    }
+
+    if (typeof value === 'number' && typeof max === 'number') {
+      return value < max
+    }
+
+    if (isDate(value) && isDate(max)) {
+      return isDateBefore(value, max)
+    }
+
+    return true
+  }
+}
+
+export function lessThanOrEqual(max: ValidationValue): ValidateMethod {
+  return (value) => {
+    if (!value) {
+      return false
+    }
+
+    if (typeof value === 'string' && typeof max === 'number') {
+      return value.length <= max
+    }
+
+    if (typeof value === 'number' && typeof max === 'number') {
+      return value <= max
+    }
+
+    if (isDate(value) && isDate(max)) {
+      return isDateBeforeOrEqual(value, max)
+    }
+
+    return true
+  }
+}
+
+export function greaterThan(min: ValidationValue): ValidateMethod {
+  return (value) => {
+    if (!value) {
+      return false
+    }
+
+    if (typeof value === 'string' && typeof min === 'number') {
+      return value.length > min
+    }
+
+    if (typeof value === 'number' && typeof min === 'number') {
+      return value > min
+    }
+
+    if (isDate(value) && isDate(min)) {
+      return isDateAfter(value, min)
+    }
+
+    return true
+  }
+}
+
+export function greaterThanOrEqual(min: ValidationValue): ValidateMethod {
+  return (value) => {
+    if (!value) {
+      return false
+    }
+
+    if (typeof value === 'string' && typeof min === 'number') {
+      return value.length >= min
+    }
+
+    if (typeof value === 'number' && typeof min === 'number') {
+      return value >= min
+    }
+
+    if (isDate(value) && isDate(min)) {
+      return isDateAfterOrEqual(value, min)
+    }
+
+    return true
+  }
+}
