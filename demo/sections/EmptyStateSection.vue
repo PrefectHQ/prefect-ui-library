32d61b7f17990381343e6e52ca91e7bba5b1a3dd
Jennifer Grange <jenny@prefect.io>
2022-05-27 15:35:36 -0400

    add empty result component

--- demo/sections/EmptyStateSection.vue
+++ demo/sections/EmptyStateSection.vue
@@ -9,13 +9,28 @@
     <DemoSubSection heading="Queues">
       <WorkQueuesPageEmptyState />
     </DemoSubSection>
+    <DemoSubSection heading="Empty Result List - for filters">
+      <EmptyResults>
+        <template #message>
+          No runs matched your filters
+        </template>
+        <template #actions>
+          <p-link>Clear Filters</p-link>
+        </template>
+      </EmptyResults>
+    </DemoSubSection>
+    <DemoSubSection heading="Empty Results List - Plain">
+      <EmptyResults />
+    </DemoSubSection>
   </DemoSection>
 </template>
 
 <script lang="ts" setup>
+  import { PLink } from '@prefecthq/prefect-design'
   import DemoSection from '../components/DemoSection.vue'
   import DemoSubSection from '../components/DemoSubSection.vue'
   import DeploymentsPageEmptyState from '@/components/DeploymentsPageEmptyState.vue'
+  import EmptyResults from '@/components/EmptyResults.vue'
   import FlowsPageEmptyState from '@/components/FlowsPageEmptyState.vue'
   import WorkQueuesPageEmptyState from '@/components/WorkQueuesPageEmptyState.vue'
 </script>
---
+++ src/components/EmptyResults.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="empty-list">
+    <div class="empty-list--message">
+      <slot name="message">
+        No runs
+      </slot>
+    </div>
+    <slot name="actions" />
+  </div>
+</template>
+
+<style>
+.empty-list {
+  @apply text-center text-base
+}
+.empty-list--message {
+  @apply text-slate-500
+}
+</style>
