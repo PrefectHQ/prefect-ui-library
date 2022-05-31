687afa66232c3c21e7ed1ec08852242bba74bfd7
Marichka Offen <marichka@prefect.io>
2022-05-31 10:49:01 -0400

    Add missing close call

--- src/components/DeploymentsTable.vue
+++ src/components/DeploymentsTable.vue
@@ -17,7 +17,7 @@
         <template #default="{ close }">
           <copy-overflow-menu-item label="Copy ID" :item="row.id" @click="close" />
           <p-overflow-menu-item label="Run" class="deployments-table__hide-on-desktop" />
-          <delete-overflow-menu-item :name="row.name" @delete="deleteDeployment(row.id)" />
+          <delete-overflow-menu-item :name="row.name" @delete="deleteDeployment(row.id, close)" />
         </template>
       </p-icon-button-menu>
     </template>
@@ -64,8 +64,9 @@
 
   const emit = defineEmits(['delete'])
 
-  const deleteDeployment = async (id: string): Promise<void> => {
+  const deleteDeployment = async (id: string, close: () => void): Promise<void> => {
     await deleteItem(id, deploymentsApi.deleteDeployment, 'Deployment')
+    close()
     emit('delete', id)
   }
 </script>
