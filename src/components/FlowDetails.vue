c33c943a26b57f509dddcfd4affe8ba1f10cc664
Craig Harshbarger <pleek91@gmail.com>
2022-06-01 13:52:22 -0500

    Always show tags

--- src/components/FlowDetails.vue
+++ src/components/FlowDetails.vue
@@ -3,13 +3,11 @@
     <p-key-value label="Flow ID" :value="flow.id" />
     <p-key-value label="Created" :value="formatDateTimeNumeric(flow.created)" />
 
-    <template v-if="flow.tags.length">
-      <p-key-value label="Tags">
-        <template #value>
-          <p-tags :tags="flow.tags" class="flow-details__tags" />
-        </template>
-      </p-key-value>
-    </template>
+    <p-key-value label="Tags">
+      <template v-if="flow.tags.length" #value>
+        <p-tags :tags="flow.tags" class="flow-details__tags" />
+      </template>
+    </p-key-value>
   </div>
 </template>
 
