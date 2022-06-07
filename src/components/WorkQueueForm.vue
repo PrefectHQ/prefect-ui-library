2a795021aa0414ee777513e66e044dbf1b9c32df
Marichka Offen <marichka@prefect.io>
2022-06-07 15:13:03 -0400

    Update WorkQueueForm.vue

--- src/components/WorkQueueForm.vue
+++ src/components/WorkQueueForm.vue
@@ -1,30 +1,19 @@
 <template>
-  <p-form class="queue-form" @submit="submit" @cancel="cancel">
-    <p-label label="Name">
-      <p-text-input v-model="internalValue.name" />
+  <p-form class="queue-form" :loading="isSubmitting" @submit="submit" @cancel="cancel">
+    <p-label label="Name " :message="errors.name" :state="nameState">
+      <p-text-input v-model="name" :state="nameState" />
     </p-label>
 
     <p-label label="Description">
-      <p-text-input v-model="internalValue.description" />
+      <p-text-input v-model="description" />
     </p-label>
 
     <p-label label="Status">
-      <p-toggle v-model="isActive">
-        <template #append>
-          <div>
-            <template v-if="isActive">
-              Active
-            </template>
-            <template v-else>
-              Paused
-            </template>
-          </div>
-        </template>
-      </p-toggle>
+      <WorkQueueToggle v-model:workQueue="internalValue" />
     </p-label>
 
     <p-label label="Flow Run Concurrency (Optional)">
-      <p-number-input v-model="internalValue.concurrencyLimit" placeholder="Unlimited" />
+      <p-number-input v-model="concurrencyLimit" placeholder="Unlimited" />
     </p-label>
 
     <p class="queue-form__section-header">
@@ -32,59 +21,60 @@
     </p>
 
     <p-label label="Tags">
-      <p-tags-input v-model:tags="internalValue.filter.tags" empty-message="Add tag to filter..." />
+      <p-tags-input v-model:tags="tags" empty-message="Add tag to filter..." />
     </p-label>
 
     <p-label label="Deployments">
-      <DeploymentCombobox v-model:selected="internalValue.filter.deploymentIds" empty-message="Select Deployments to filter..." />
+      <DeploymentCombobox v-model:selected="deployments" empty-message="Select Deployments to filter..." />
     </p-label>
 
     <p-label label="Flow Runners">
-      <FlowRunnerCheckboxes v-model:selected="internalValue.filter.flowRunnerTypes" />
+      <FlowRunnerCheckboxes v-model:selected="flowRunnerTypes" />
     </p-label>
   </p-form>
 </template>
 
 <script lang="ts" setup>
-  import { PLabel, PTextInput, PNumberInput, PTagsInput, PToggle, PForm } from '@prefecthq/prefect-design'
-  import { computed, reactive, watch } from 'vue'
+  import { PLabel, PTextInput, PNumberInput, PTagsInput, PForm } from '@prefecthq/prefect-design'
+  import { useField, useForm } from 'vee-validate'
+  import { reactive } from 'vue'
   import FlowRunnerCheckboxes from './FlowRunnerCheckboxes.vue'
   import DeploymentCombobox from '@/components/DeploymentCombobox.vue'
+  import WorkQueueToggle from '@/components/WorkQueueToggle.vue'
   import { IWorkQueueRequest, WorkQueue, WorkQueueFormValues } from '@/models'
+  import { isRequired, withMessage } from '@/services/validate'
+  import { FlowRunnerType } from '@/types/FlowRunnerType'
 
   const props = defineProps<{
     workQueue?: WorkQueue,
   }>()
+
+  const internalValue = reactive(new WorkQueueFormValues(props.workQueue))
+  const { handleSubmit, isSubmitting, errors } = useForm({ initialValues: internalValue })
+
+  const rules = {
+    name: [withMessage(isRequired, 'Queue name is Required')],
+  }
+
+  const { value: name, meta: nameState } = useField<string>('name', rules.name)
+  const { value: description } = useField<string|null>('description')
+  const { value: concurrencyLimit } = useField<number|null>('concurrencyLimit')
+  const { value: tags } = useField<string[]>('filter.tags')
+  const { value: deployments } = useField<string[]>('filter.deploymentIds')
+  const { value: flowRunnerTypes } = useField<FlowRunnerType[]>('filter.flowRunnerTypes')
 
   const emit = defineEmits<{
     (event: 'submit', value: IWorkQueueRequest): void,
     (event: 'cancel'): void,
   }>()
 
-  const internalValue = reactive(new WorkQueueFormValues(props.workQueue))
-
-  const isActive = computed({
-    get() {
-      return !internalValue.isPaused
-    },
-    set() {
-      internalValue.isPaused = !internalValue.isPaused
-    },
+  const submit = handleSubmit(workQueueData => {
+    console.log(workQueueData.getWorkQueueRequest())
+    emit('submit', workQueueData.getWorkQueueRequest())
   })
-
-  function submit(): void {
-    emit('submit', internalValue.getWorkQueueRequest())
-  }
-
   function cancel(): void {
     emit('cancel')
   }
-
-  watch(() => props.workQueue, (val) => {
-    if (val) {
-      Object.assign(internalValue, new WorkQueueFormValues(val))
-    }
-  })
 </script>
 
 <style>
