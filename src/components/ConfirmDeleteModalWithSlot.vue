e4a13700096fe0ba07e9c1787527ba2fe3e2268c
Evan Sutherland <stackoverfloweth@gmail.com>
2022-06-10 07:35:29 -0500

    exposing ConfirmDeleteModal slots through parent component

--- src/components/ConfirmDeleteModalWithSlot.vue
+++ src/components/ConfirmDeleteModalWithSlot.vue
@@ -1,7 +1,11 @@
 <template inheritAttrs="false">
   <slot :show-modal="showModal" :is-open="isOpen" :open="open" :close="close" :toggle="toggle" />
   <!-- eslint-disable-next-line vue/no-duplicate-attr-inheritance -->
-  <ConfirmDeleteModal v-model:showModal="showModal" v-bind="$attrs" />
+  <ConfirmDeleteModal v-model:showModal="showModal" v-bind="$attrs">
+    <template v-for="(index, name) in $slots" #[name]="data">
+      <slot :name="name" v-bind="data" />
+    </template>
+  </ConfirmDeleteModal>
 </template>
 
 <script lang="ts" setup>
