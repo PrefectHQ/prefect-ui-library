f81dd66de6823f1cc78df21a979c5f0e6ac232c9
Evan Sutherland <stackoverfloweth@gmail.com>
2022-06-11 14:25:55 -0500

    created simplefied verion of FlowActivityChart that takes runHistory array

---
+++ src/components/ActivityChart.vue
@@ -0,0 +1,87 @@
+<template>
+  <DivergingBarChart
+    class="activity-chart"
+    :items="items"
+    :interval-start="intervalStart"
+    :interval-end="intervalEnd"
+    :interval-seconds="intervalSeconds"
+    :positive-sentiment-keys="positiveSentimentKeys"
+    :negative-sentiment-keys="negativeSentimentKeys"
+    static-median
+    :chart-padding="{ top: 0, bottom: 0, middle: 4 }"
+  >
+    <template #default="{ key }">
+      <div class="activity-chart__bar" :class="getStateColor(key)" />
+    </template>
+
+    <template #median>
+      <div class="activity-chart__median" />
+    </template>
+
+    <template #empty>
+      <div class="activity-chart__empty" />
+    </template>
+  </DivergingBarChart>
+</template>
+
+<script lang="ts" setup>
+  import { DivergingBarChart, DivergingBarChartData, DivergingBarChartItem } from '@prefecthq/vue-charts'
+  import { computed } from 'vue'
+  import { RunHistory, StateType } from '@/models'
+
+  const StateDirections: ReadonlyMap<StateType, 1 | -1> = new Map([
+    ['completed', -1],
+    ['running', -1],
+    ['scheduled', -1],
+    ['pending', -1],
+    ['failed', 1],
+    ['cancelled', 1],
+  ])
+
+  const positiveSentimentKeys = [...StateDirections.entries()].filter(([, direction]) => direction < 0).map(([key]) => key)
+  const negativeSentimentKeys = [...StateDirections.entries()].filter(([, direction]) => direction > 0).map(([key]) => key)
+
+  const props = defineProps<{
+    intervalStart: Date,
+    intervalEnd: Date,
+    intervalSeconds: number,
+    history: RunHistory[],
+  }>()
+
+  const getStateColor = (state: StateType): string => `bg-state-${state.toLowerCase()}-500`
+
+  const items = computed<DivergingBarChartItem[]>(() => {
+    const data = props.history.map(item => {
+      return {
+        intervalStart: item.intervalStart,
+        intervalEnd: item.intervalEnd,
+        data: item.states.reduce<DivergingBarChartData>((data, state) => {
+          data[state.stateType] = state.countRuns
+          return data
+        }, {}),
+      }
+    }).sort((itemA, itemB) => {
+      return itemA.intervalStart.getTime() - itemB.intervalStart.getTime()
+    })
+
+    return data
+  })
+</script>
+
+<style>
+.activity-chart__bar {
+  @apply
+  h-full
+  rounded-full
+  m-auto
+  max-w-[10px]
+  w-1/2
+}
+
+.activity-chart__median {
+  @apply
+  h-px
+  bg-slate-300
+  w-full
+}
+</style>
