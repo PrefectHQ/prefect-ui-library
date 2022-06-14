2db81304e96c5b39eb7fc59b4bd96909eaafa264
Craig Harshbarger <pleek91@gmail.com>
2022-06-14 15:37:57 -0500

    Add menu to the block page heading. Add injection keys for block apis

--- src/components/PageHeadingBlock.vue
+++ src/components/PageHeadingBlock.vue
@@ -1,22 +1,53 @@
 <template>
-  <page-heading class="page-heading-blocks" :crumbs="crumbs" />
+  <page-heading class="page-heading-blocks" :crumbs="crumbs">
+    <template #actions>
+      <router-link :to="blockEditRoute(block.id)">
+        <p-overflow-menu-item label="Edit" />
+      </router-link>
+      <p-icon-button-menu>
+        <template #default>
+          <p-overflow-menu-item label="Delete" @click="open" />
+        </template>
+      </p-icon-button-menu>
+      <ConfirmDeleteModal
+        v-model:showModal="showModal"
+        :name="block.name!"
+        @delete="deleteBlock(block.id)"
+      />
+    </template>
+  </page-heading>
 </template>
 
 <script lang="ts" setup>
   import { BreadCrumbs } from '@prefecthq/prefect-design'
   import PageHeading from '@/components/PageHeading.vue'
+  import { useShowModal } from '@/compositions/useShowModal'
   import { BlockDocument } from '@/models/BlockDocument'
-  import { blocksRouteKey } from '@/router/routes'
-  import { inject } from '@/utilities'
+  import { blockEditRouteKey, blocksRouteKey } from '@/router/routes'
+  import { blockDocumentsApiKey } from '@/services'
+  import { deleteItem, inject } from '@/utilities'
 
   const props = defineProps<{
     block: BlockDocument,
   }>()
 
   const blockRoute = inject(blocksRouteKey)
+  const blockEditRoute = inject(blockEditRouteKey)
+  const blockDocumentsApi = inject(blockDocumentsApiKey)
+
+  const { showModal, open } = useShowModal()
 
   const crumbs: BreadCrumbs = [
     { text: 'Blocks', to: blockRoute() },
     { text: props.block.name },
   ]
+
+  const emit = defineEmits<{
+    (event: 'update' | 'delete'): void,
+  }>()
+
+  const deleteBlock = async (id: string): Promise<void> => {
+    await deleteItem(id, blockDocumentsApi.deleteBlockDocument, 'Work queue')
+    emit('delete')
+  }
 </script>
