6765aeae509a12e5cadd367ecf2f12cfde2eab27
Craig Harshbarger <pleek91@gmail.com>
2022-06-14 17:29:18 -0500

    No need to declare a type here

--- src/utilities/mocks.ts
+++ src/utilities/mocks.ts
@@ -1,8 +1,6 @@
 import { hasProperty } from '@/mocks/objects'
 
-type MockedObject<T extends object> = Record<keyof T, T[keyof T]>
-
-export function mockClass<T extends object>(constructor: new () => T, mocked: MockedObject<T>): T {
+export function mockClass<T extends object>(constructor: new () => T, mocked: Record<keyof T, T[keyof T]>): T {
   return new Proxy(new constructor(), {
     get: (target, prop) => {
       if (hasProperty(mocked, prop)) {
