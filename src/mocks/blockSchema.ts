cbbe4401e5125432fc8af84a6f8e2dcac9941588
Craig Harshbarger <pleek91@gmail.com>
2022-06-15 10:17:09 -0500

    Update mocker to the new pattern

--- src/mocks/blockSchema.ts
+++ src/mocks/blockSchema.ts
@@ -1,15 +1,16 @@
 import { BlockSchema } from '@/models/BlockSchema'
 import { MockFunction } from '@/services/Mocker'
 
-export const randomBlockSchema: MockFunction<BlockSchema> = function(blockSchema: Partial<BlockSchema> = {}) {
+export const randomBlockSchema: MockFunction<BlockSchema> = function(overrides: Partial<BlockSchema> = {}) {
   return new BlockSchema({
-    id: blockSchema.id ?? this.create('string'),
-    created: blockSchema.created ?? this.create('date'),
-    updated: blockSchema.updated ?? this.create('date'),
-    checksum: blockSchema.checksum ?? this.create('string'),
-    fields: blockSchema.fields ?? this.create('blockSchemaFields'),
-    blockTypeId: blockSchema.blockTypeId ?? this.create('string'),
-    blockType: blockSchema.blockType ?? this.create('blockType'),
-    capabilities: blockSchema.capabilities ?? [],
+    id: this.create('string'),
+    created: this.create('date'),
+    updated: this.create('date'),
+    checksum: this.create('string'),
+    fields: this.create('blockSchemaFields'),
+    blockTypeId: this.create('string'),
+    blockType: this.create('blockType'),
+    capabilities: [],
+    ...overrides,
   })
 }
