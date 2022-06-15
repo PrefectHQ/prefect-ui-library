7d3a7bf54eba1a1c0667dd98b075359780980921
Craig Harshbarger <pleek91@gmail.com>
2022-06-15 09:09:22 -0500

    Fix linting warning

--- src/utilities/strings.ts
+++ src/utilities/strings.ts
@@ -40,7 +40,7 @@ export function kebabCase(string: string): string {
 }
 
 export function camelCase(str: string): string {
-  return str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase())
+  return str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (match, chr) => chr.toUpperCase())
 }
 
 export function isString(value: unknown): value is string {
