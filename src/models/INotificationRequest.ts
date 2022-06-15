7d0ba0b4b385718197bab58df5710fb490f90d6a
Jennifer Grange <jenny@prefect.io>
2022-06-15 14:10:35 -0400

    make non null

--- src/models/INotificationRequest.ts
+++ src/models/INotificationRequest.ts
@@ -2,7 +2,7 @@ import { IStateResponse } from './IStateResponse'
 export interface INotificationRequest {
   name: string,
   is_active: boolean,
-  state_names?: IStateResponse[],
-  tags?: string[],
+  state_names: IStateResponse[],
+  tags: string[],
   block_document_id: string,
 }
