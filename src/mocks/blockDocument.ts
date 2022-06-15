ce451873483ad9b958f80256cb8c7fdc4843b2a1
Craig Harshbarger <pleek91@gmail.com>
2022-06-15 10:08:07 -0500

    Review changes. Flush out the BlockDocumentsApi mock

--- src/mocks/blockDocument.ts
+++ src/mocks/blockDocument.ts
@@ -1,17 +1,18 @@
 import { BlockDocument } from '@/models/BlockDocument'
 import { MockFunction } from '@/services/Mocker'
 
-export const randomBlockDocument: MockFunction<BlockDocument> = function(blockDocument: Partial<BlockDocument> = {}) {
+export const randomBlockDocument: MockFunction<BlockDocument> = function(overrides: Partial<BlockDocument> = {}) {
   return new BlockDocument({
-    id: blockDocument.id ?? this.create('string'),
-    created: blockDocument.created ?? this.create('date'),
-    updated: blockDocument.updated ?? this.create('date'),
-    name: blockDocument.name ?? this.create('string'),
-    data: blockDocument.data ?? {},
-    blockSchemaId: blockDocument.blockSchemaId ?? this.create('string'),
-    blockSchema: blockDocument.blockSchema ?? this.create('blockSchema'),
-    blockTypeId: blockDocument.blockTypeId ?? this.create('string'),
-    blockType: blockDocument.blockType ?? this.create('blockType'),
-    blockDocumentReferences: blockDocument.blockDocumentReferences ?? {},
+    id: this.create('string'),
+    created: this.create('date'),
+    updated: this.create('date'),
+    name: this.create('string'),
+    data: {},
+    blockSchemaId: this.create('string'),
+    blockSchema: this.create('blockSchema'),
+    blockTypeId: this.create('string'),
+    blockType: this.create('blockType'),
+    blockDocumentReferences: {},
+    ...overrides,
   })
 }
--- src/mocks/blockDocumentsApi.ts
+++ src/mocks/blockDocumentsApi.ts
@@ -1,14 +1,48 @@
-import { BlockDocumentCreate, BlockDocumentUpdate } from '@/models'
+import { AxiosError } from 'axios'
+import { BlockDocument, BlockDocumentCreate, BlockDocumentUpdate } from '@/models'
 import { BlockDocumentsApi } from '@/services'
 import { MockFunction } from '@/services/Mocker'
 import { mockClass } from '@/utilities/mocks'
 
 export const blockDocumentsApiMockFactory: MockFunction<BlockDocumentsApi> = function(overrides: Partial<BlockDocumentsApi> = {}) {
+  const blockDocuments = this.createMany('blockDocument', this.create('number', [2, 20]))
+
   return mockClass(BlockDocumentsApi, {
-    getBlockDocuments: () => Promise.resolve(this.createMany('blockDocument', this.create('number', [2, 20]))),
-    getBlockDocument: () => Promise.resolve(this.create('blockDocument')),
-    createBlockDocument: (request: BlockDocumentCreate) => Promise.resolve(this.create('blockDocument', [request])),
-    updateBlockDocument: (blockDocumentId: string, request: BlockDocumentUpdate) => Promise.resolve(this.create('blockDocument', [request])),
+    getBlockDocuments: () => Promise.resolve(blockDocuments),
+
+    getBlockDocument: (blockDocumentId: string) => {
+      return new Promise<BlockDocument>((resolve, reject) => {
+        const blockDocument = blockDocuments.find(blockDocument => blockDocument.id == blockDocumentId)
+
+        if (blockDocument === undefined) {
+          return reject(new AxiosError('Block document not found', '404'))
+        }
+
+        resolve(blockDocument)
+      })
+    },
+
+    createBlockDocument: (request: BlockDocumentCreate) => {
+      const blockDocument = this.create('blockDocument', [request])
+      blockDocuments.push(blockDocument)
+
+      return Promise.resolve(blockDocument)
+    },
+
+    updateBlockDocument: (blockDocumentId: string, request: BlockDocumentUpdate) => {
+      return new Promise<BlockDocument>((resolve, reject) => {
+        const blockDocument = blockDocuments.find(blockDocument => blockDocument.id == blockDocumentId)
+
+        if (blockDocument === undefined) {
+          return reject(new AxiosError('Block document not found', '404'))
+        }
+
+        Object.assign(blockDocument, request)
+
+        resolve(blockDocument)
+      })
+    },
+
     ...overrides,
   })
 }
