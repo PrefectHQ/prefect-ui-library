e7955dbdcbe77dcf2c69009bc029b569ab854667
Jennifer Grange <jenny@prefect.io>
2022-06-15 23:09:58 -0400

    add toggle and update call - not sure we really need to pass all the notification info

---
+++ src/components/NotificationToggle.vue
@@ -0,0 +1,50 @@
+<template>
+  <p-toggle v-model="internalValue" :loading="loading" />
+</template>
+
+<script lang="ts" setup>
+  import { PToggle, showToast } from '@prefecthq/prefect-design'
+  import { computed, ref } from 'vue'
+  import { Notification } from '@/models'
+  import { notificationsApiKey } from '@/services/NotificationsApi'
+  import { inject } from '@/utilities'
+
+  const props = defineProps<{
+    notification: Notification,
+  }>()
+
+  const emit = defineEmits<{
+    (event: 'update'): void,
+  }>()
+
+  const notificationsApi = inject(notificationsApiKey)
+
+  const internalValue = computed({
+    get() {
+      return !!props.notification.isActive
+    },
+    set(value: boolean) {
+      toggleNotification(value)
+    },
+  })
+
+  const loading = ref(false)
+
+  const toggleNotification = async (value: boolean): Promise<void> => {
+    loading.value = true
+
+    try {
+      const notification = { name: props.notification.name, state_names: props.notification.stateNames, is_active: props.notification.isActive, tags: props.notification.tags, block_document_id: props.notification.blockDocumentId   }
+      notification.is_active = value
+      await notificationsApi.updateNotification(props.notification.id, notification)
+      const activeOrPaused = value ? 'Active' : 'Paused'
+      showToast(`${props.notification.name} ${activeOrPaused}`, 'success', undefined, 3000)
+      emit('update')
+    } catch (error) {
+      showToast(`${error}`, 'error', undefined, 3000)
+    } finally {
+      loading.value = false
+    }
+  }
+</script>
+
