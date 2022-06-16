1691e4cb25e9de4f0025ae31a313f5d33e347228
Craig Harshbarger <pleek91@gmail.com>
2022-06-16 11:54:21 -0500

    Add a page for blocks to the demo

---
+++ demo/pages/Blocks.vue
@@ -0,0 +1,12 @@
+<template>
+  <p-layout-default class="blocks">
+    <BlocksPageHeadingsSection />
+    <BlockDocumentsSection />
+  </p-layout-default>
+</template>
+
+<script lang="ts" setup>
+  import { PLayoutDefault } from '@prefecthq/prefect-design'
+  import BlockDocumentsSection from '../sections/BlockDocumentsSection.vue'
+  import BlocksPageHeadingsSection from '../sections/BlocksPageHeadingsSection.vue'
+</script>
---
+++ demo/sections/BlockDocumentsSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <DemoSection heading="Block Documents">
+    <DemoSubSection heading="Table">
+      <BlockDocumentsTable :block-documents="blockDocuments" />
+    </DemoSubSection>
+  </DemoSection>
+</template>
+
+<script lang="ts" setup>
+  import DemoSection from '../components/DemoSection.vue'
+  import DemoSubSection from '../components/DemoSubSection.vue'
+  import BlockDocumentsTable from '@/components/BlockDocumentsTable.vue'
+  import { mocker } from '@/services/Mocker'
+
+  const blockDocuments = mocker.createMany('blockDocument', 5)
+</script>
