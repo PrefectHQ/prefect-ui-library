df92c9d3528a3a2ab7e53c2f9fa5ee9f82dd284f
nicholas <znicholasbrown@gmail.com>
2022-06-17 16:50:01 -0400

    Scaffold interval form

---
+++ demo/sections/ScheduleFormsSection.vue
@@ -0,0 +1,23 @@
+<template>
+  <DemoSection heading="Schedule Forms">
+    <DemoSubSection heading="Interval">
+      <IntervalScheduleForm v-model="intervalSchedule" />
+    </DemoSubSection>
+
+    <p-divider class="my-8" />
+
+    <DemoSubSection heading="Cron">
+      <em>Coming soon</em>
+    </DemoSubSection>
+  </DemoSection>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue'
+  import DemoSection from '../components/DemoSection.vue'
+  import DemoSubSection from '../components/DemoSubSection.vue'
+  import IntervalScheduleForm from '@/components/IntervalScheduleForm.vue'
+  import { IntervalSchedule } from '@/models'
+
+  const intervalSchedule = ref(new IntervalSchedule({ interval: 60, timezone: null, anchorDate: null }))
+</script>
---
+++ src/components/IntervalScheduleForm.vue
@@ -0,0 +1,5 @@
+<template>
+  <p-form>
+    Hello!
+  </p-form>
+</template>
