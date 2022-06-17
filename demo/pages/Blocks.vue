1dce91149db72aa0018f45fb0b99c8614e8b9283
Craig Harshbarger <pleek91@gmail.com>
2022-06-17 09:18:09 -0500

    Add the blocks page empty state component

--- demo/pages/Blocks.vue
+++ demo/pages/Blocks.vue
@@ -1,6 +1,7 @@
 <template>
   <p-layout-default class="blocks">
     <BlocksPageHeadingsSection />
+    <BlocksEmptyStateSection />
     <BlockDocumentsSection />
     <BlockSchemaCapabilitiesSection />
     <BlockTypesSection />
@@ -11,6 +12,7 @@
   import { PLayoutDefault } from '@prefecthq/prefect-design'
   import BlockDocumentsSection from '../sections/BlockDocumentsSection.vue'
   import BlockSchemaCapabilitiesSection from '../sections/BlockSchemaCapabilitiesSection.vue'
+  import BlocksEmptyStateSection from '../sections/BlocksEmptyStateSection.vue'
   import BlocksPageHeadingsSection from '../sections/BlocksPageHeadingsSection.vue'
   import BlockTypesSection from '../sections/BlockTypesSection.vue'
 </script>
---
+++ src/components/BlocksPageEmptyState.vue
@@ -0,0 +1,32 @@
+<template>
+  <p-empty-state class="blocks-page-empty-state">
+    <template #icon>
+      <p-icon icon="CubeIcon" />
+    </template>
+
+    <template #heading>
+      Create a block to get started
+    </template>
+
+    <template #description>
+      Blocks securely store credentials and configuration to connect to and use external systems.
+    </template>
+
+    <template #actions>
+      <router-link :to="blockCatalogRoute()">
+        <p-button>
+          Create a Block
+          <p-icon icon="PlusIcon" class="empty-work-queue--link-icon" />
+        </p-button>
+      </router-link>
+    </template>
+  </p-empty-state>
+</template>
+
+<script lang="ts" setup>
+  import { PEmptyState, PButton, PIcon } from '@prefecthq/prefect-design'
+  import { blockCatalogRouteKey } from '@/router'
+  import { inject } from '@/utilities'
+
+  const blockCatalogRoute = inject(blockCatalogRouteKey)
+</script>
