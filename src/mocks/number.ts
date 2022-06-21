22a4a75ff503ffb0de586d4be6e716f71c23fc90
Craig Harshbarger <pleek91@gmail.com>
2022-06-21 14:26:28 -0500

    Remove the type from the args

--- src/mocks/number.ts
+++ src/mocks/number.ts
@@ -2,6 +2,6 @@
 import { MockFunction } from '@/services'
 import { uniform } from '@/utilities/math'
 
-export const randomNumber: MockFunction<number, [number?, number?]> = function(min: number = 0, max: number = 100) {
+export const randomNumber: MockFunction<number, [number?, number?]> = function(min = 0, max = 100) {
   return uniform(min, max)
 }
