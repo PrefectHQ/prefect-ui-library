4c7cb67f08218661297d9c749286d14b4a0ee499
nicholas <znicholasbrown@gmail.com>
2022-06-22 12:51:38 -0400

    Add timezone selection support

--- src/components/TimezoneSelect.vue
+++ src/components/TimezoneSelect.vue
@@ -5,7 +5,9 @@
 <script lang="ts" setup>
   import { SelectOption } from '@prefecthq/prefect-design'
   import { ref } from 'vue'
+  import { timezones } from '@/utilities'
+
 
   const internalValue = ref('Local')
-  const options: SelectOption[] = []
+  const options: SelectOption[] = timezones.map((timezone) => ({ label: timezone, value: timezone }))
 </script>
