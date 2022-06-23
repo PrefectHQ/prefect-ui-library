4e423d685da04e73ed07b887f2ecdc567fe4454f
nicholas <znicholasbrown@gmail.com>
2022-06-23 16:31:15 -0400

    Move public cron to its own file

--- src/models/CronSchedule.ts
+++ src/models/CronSchedule.ts
@@ -1,7 +1,5 @@
-/* eslint-disable max-classes-per-file */
-import { default as Cron } from 'cronstrue'
-import { Options as CronstrueOptions } from 'cronstrue/dist/options'
 import cronstrue from 'cronstrue/i18n'
+import { PublicCron } from './PublicCron'
 import { Schedule } from '@/models'
 import { CronKeyword, isCronKeyword, containsCronRandomExpression, cronKeywordMap } from '@/types/cron'
 import { capitalize } from '@/utilities'
@@ -14,91 +12,6 @@ export interface ICronScheduleRaw {
 }
 
 export type ICronSchedule = ICronScheduleRaw & Schedule
-
-// This class modifies the underlying Cron class to expose string utilities as public methods
-class PublicCron extends Cron {
-  public getFullDescription(): string {
-    try {
-      return super.getFullDescription()
-    } catch {
-      return ''
-    }
-  }
-
-  public getSecondsDescription(): string {
-    try {
-      return super.getSecondsDescription()
-    } catch {
-      return ''
-    }
-  }
-
-  public getMinutesDescription(): string {
-    try {
-      return super.getMinutesDescription()
-    } catch {
-      return ''
-    }
-  }
-
-  public getHoursDescription(): string {
-    try {
-      return super.getHoursDescription()
-    } catch {
-      return ''
-    }
-  }
-
-  public getTimeOfDayDescription(): string {
-    try {
-      return super.getTimeOfDayDescription()
-    } catch {
-      return ''
-    }
-  }
-
-  public getDayOfMonthDescription(): string | null {
-    try {
-      return super.getDayOfMonthDescription()
-    } catch {
-      return ''
-    }
-  }
-
-  public getMonthDescription(): string {
-    try {
-      return super.getMonthDescription()
-    } catch {
-      return ''
-    }
-  }
-
-  public getDayOfWeekDescription(): string {
-    try {
-      return super.getDayOfWeekDescription()
-    } catch {
-      return ''
-    }
-  }
-
-  public getYearDescription(): string {
-    try {
-      return super.getYearDescription()
-    } catch {
-      return ''
-    }
-  }
-
-  public transformVerbosity(description: string, useVerboseFormat: boolean): string {
-    return super.transformVerbosity(description, useVerboseFormat)
-  }
-
-  public constructor(expression: string, options: CronstrueOptions) {
-    super(expression, options)
-    this.getFullDescription()
-  }
-}
-
 
 export class CronSchedule implements ICronSchedule {
   public timezone: string | null
