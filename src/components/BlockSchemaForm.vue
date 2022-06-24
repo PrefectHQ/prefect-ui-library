90776bb5c136c7d52ca53da83f4d1632a76bbd1f
Craig Harshbarger <pleek91@gmail.com>
2022-06-24 09:10:06 -0500

    Add BlockSchemaFormFields component

--- src/components/BlockSchemaForm.vue
+++ src/components/BlockSchemaForm.vue
@@ -5,35 +5,16 @@
         <p-text-input v-model="nameModel" />
       </p-label>
 
-      <template v-for="(property, key) in blockSchema.fields.properties" :key="property.title">
-        <template v-if="isBlockSchemaReferenceProperty(property)">
-          <BlockSchemaPropertyReferenceInput
-            :selected="getReferenceValue(key)"
-            :block-type-name="getReferenceTypeName(key)!"
-            :required="isRequired(key)"
-            @update:selected="setReferenceValue(key, $event!)"
-          />
-        </template>
-        <template v-else>
-          <BlockSchemaPropertyInput
-            :value="getPropertyValue(key)"
-            :property="property"
-            :required="isRequired(key)"
-            @update:value="setPropertyValue(key, $event)"
-          />
-        </template>
-      </template>
+      <BlockSchemaFormFields v-model:data="dataModel" :block-schema="blockSchema" />
     </p-content>
   </p-form>
 </template>
 
 <script lang="ts" setup>
   import { computed } from 'vue'
-  import BlockSchemaPropertyInput from './BlockSchemaPropertyInput.vue'
-  import BlockSchemaPropertyReferenceInput from '@/components/BlockSchemaPropertyReferenceInput.vue'
+  import BlockSchemaFormFields from './BlockSchemaFormFields.vue'
   import { BlockDocumentData } from '@/models/BlockDocument'
   import { BlockSchema } from '@/models/BlockSchema'
-  import { isBlockDocumentDataReference, isBlockSchemaReferenceProperty } from '@/utilities/blocks'
 
   const props = defineProps<{
     blockSchema: BlockSchema,
@@ -72,55 +53,4 @@
   function cancel(): void {
     emit('cancel')
   }
-
-  function isRequired(blockSchemaPropertyKey: string): boolean {
-    return props.blockSchema.fields.required.includes(blockSchemaPropertyKey)
-  }
-
-  function getPropertyValue(blockSchemaPropertyKey: string): unknown {
-    return props.data[blockSchemaPropertyKey]
-  }
-
-  function setPropertyValue(blockSchemaPropertyKey: string, value: unknown): void {
-    dataModel.value = { ...dataModel.value, [blockSchemaPropertyKey]: value }
-  }
-
-  function getReferenceValue(blockSchemaPropertyKey: string): string | null {
-    const value = getPropertyValue(blockSchemaPropertyKey)
-
-    if (!isBlockDocumentDataReference(value)) {
-      return null
-    }
-
-    return value.$ref.blockDocumentId
-  }
-
-  function getReferenceTypeName(blockSchemaPropertyKey: string): string | null {
-    const reference = props.blockSchema.fields.blockSchemaReferences[blockSchemaPropertyKey]
-
-    if (reference === undefined) {
-      return null
-    }
-
-    return reference.blockTypeName
-  }
-
-  function setReferenceValue(blockSchemaPropertyKey: string, value: string): void {
-    dataModel.value = {
-      ...dataModel.value,
-      [blockSchemaPropertyKey]: {
-        $ref: {
-          blockDocumentId: value,
-        },
-      },
-    }
-  }
 </script>
-
-<style>
-.block-schema-form__actions { @apply
-  flex
-  justify-end
-  gap-2
-}
-</style>
