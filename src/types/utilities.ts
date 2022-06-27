ebdac50b8101989bee64d8a9d39ed75a80eb78a5
nicholas <znicholasbrown@gmail.com>
2022-06-27 13:20:27 -0400

    Lint utilities

--- src/types/utilities.ts
+++ src/types/utilities.ts
@@ -5,12 +5,12 @@ export type KeysMatching<T, V> = {
 export type Require<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>
 
 export type AnyCase<T extends string> =
-  string extends T 
-    ? string 
-    : T extends `${infer F1}${infer F2}${infer R}` 
-      ? (`${Uppercase<F1> | Lowercase<F1>}${Uppercase<F2> | Lowercase<F2>}${AnyCase<R>}`) 
-      : T extends `${infer F}${infer R}` 
-        ? `${Uppercase<F> | Lowercase<F>}${AnyCase<R>}` 
+  string extends T
+    ? string
+    : T extends `${infer F1}${infer F2}${infer R}`
+      ? (`${Uppercase<F1> | Lowercase<F1>}${Uppercase<F2> | Lowercase<F2>}${AnyCase<R>}`)
+      : T extends `${infer F}${infer R}`
+        ? `${Uppercase<F> | Lowercase<F>}${AnyCase<R>}`
         : ''
 
 
