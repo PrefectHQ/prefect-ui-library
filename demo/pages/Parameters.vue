0e9b12b18d72acfecb44f72110cb0f8a57dd9191
nicholas <znicholasbrown@gmail.com>
2022-06-29 19:26:35 -0400

    Add json editor section

--- demo/pages/Parameters.vue
+++ demo/pages/Parameters.vue
@@ -5,5 +5,11 @@
         Parameters
       </h1>
     </template>
+
+    <JsonEditorSection />
   </p-layout-default>
 </template>
+
+<script lang="ts" setup>
+  import JsonEditorSection from '../sections/JsonEditorSection.vue'
+</script>
---
+++ demo/sections/JsonEditorSection.vue
@@ -0,0 +1,10 @@
+<template>
+  <DemoSection heading="JSON Editor">
+    <JsonEditor />
+  </DemoSection>
+</template>
+
+<script lang="ts" setup>
+  import DemoSection from '../components/DemoSection.vue'
+  import JsonEditor from '@/components/JsonEditor.vue'
+</script>
