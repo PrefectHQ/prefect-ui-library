e21bf0f9f484beb0a00dc3aa54d52328a86a6bf4
nicholas <znicholasbrown@gmail.com>
2022-07-12 11:53:07 -0400

    Add comment on delete line

--- src/components/PydanticFormAllOfProperty.vue
+++ src/components/PydanticFormAllOfProperty.vue
@@ -20,6 +20,10 @@
     const _property = { ...props.property } as PydanticTypeProperty
     // This is to make sure we're passing the sum of all properties of the type
     // while unwrapping the allOf property and bypassing another level of recursion from PydanticFormProperty
+    // ☠️☠️ DANGER PERIGO 危险 NEBEZPEČÍ OPASNOST 위험 GEVAAR خطر ACHTUNG ΚΊΝΔΥΝΟΣ 危険 АЮУЛ FARE ☠️☠️
+    // Removing this line will cause a stack overflow since the component
+    // will never exit recursion
+    // ☠️☠️ DANGER PERIGO 危险 NEBEZPEČÍ OPASNOST 위험 GEVAAR خطر ACHTUNG ΚΊΝΔΥΝΟΣ 危険 АЮУЛ FARE ☠️☠️
     delete _property.allOf
     return { ...props.property.allOf[0], ..._property }
   })
