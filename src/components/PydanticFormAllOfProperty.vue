dfd7b67bab59f5bc7351475c9dc2cb2afcf3333b
nicholas <znicholasbrown@gmail.com>
2022-07-18 14:42:54 -0400

    Simplify allof form prop

--- src/components/PydanticFormAllOfProperty.vue
+++ src/components/PydanticFormAllOfProperty.vue
@@ -5,7 +5,7 @@
 <script lang="ts" setup>
   import { computed, withDefaults } from 'vue'
   import PydanticFormProperty from './PydanticFormProperty.vue'
-  import type { PydanticPropertyRecordAllOf, PydanticTypeProperty } from '@/types/Pydantic'
+  import type { PydanticPropertyRecordAllOf } from '@/types/Pydantic'
 
   const props = withDefaults(defineProps<{
     level?: number,
@@ -17,14 +17,13 @@
   })
 
   const property = computed(() => {
-    const _property = { ...props.property } as PydanticTypeProperty
+    const { allOf, ...rest } = props.property
     // This is to make sure we're passing the sum of all properties of the type
     // while unwrapping the allOf property and bypassing another level of recursion from PydanticFormProperty
     // ☠️☠️ DANGER PERIGO 危险 NEBEZPEČÍ OPASNOST 위험 GEVAAR خطر ACHTUNG ΚΊΝΔΥΝΟΣ 危険 АЮУЛ FARE ☠️☠️
     // Removing this line will cause a stack overflow since the component
     // will never exit recursion
     // ☠️☠️ DANGER PERIGO 危险 NEBEZPEČÍ OPASNOST 위험 GEVAAR خطر ACHTUNG ΚΊΝΔΥΝΟΣ 危険 АЮУЛ FARE ☠️☠️
-    delete _property.allOf
-    return { ...props.property.allOf[0], ..._property }
+    return { ...allOf[0], ...rest }
   })
 </script>
