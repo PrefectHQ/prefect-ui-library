d3a2fdf1438452991469c7046445afa9a2269e27
Jennifer Grange <jenny@prefect.io>
2022-07-18 00:01:03 -0400

    use h and create run button toast button component - need to add link and better format

--- src/components/RunButton.vue
+++ src/components/RunButton.vue
@@ -3,13 +3,16 @@
     Run
     <p-icon class="run-button__run-icon" icon="PlayIcon" solid />
   </p-button>
+  <RunButtonToastMessage v-if="flowyRun" v-show="false" ref="fr" :name="name" />
 </template>
 
 <script lang="ts" setup>
   import  { PButton, showToast } from '@prefecthq/prefect-design'
-  import { ref } from 'vue'
+  import { ref, h } from 'vue'
+  import RunButtonToastMessage from './RunButtonToastMessage.vue'
   import { Deployment } from '@/models'
   import { deploymentsApiKey } from '@/services/DeploymentsApi'
+  // import { useStore } from '@/stores/toast'
   import { canKey } from '@/types'
   import { inject } from '@/utilities'
 
@@ -20,19 +23,29 @@
   const can = inject(canKey)
   const deploymentsApi = inject(deploymentsApiKey)
   const loading = ref(false)
+  const fr = ref(null)
+  const name = ref()
+  const flowyRun = ref()
+
+  // const store = useStore()
+
 
   const run = async (deployment: Deployment): Promise<void> => {
     loading.value = true
     try {
-      await deploymentsApi.createDeploymentFlowRun(deployment.id, {
+      flowyRun.value = await deploymentsApi.createDeploymentFlowRun(deployment.id, {
         state: {
           type: 'scheduled',
           message: 'Run through UI',
         },
       },
       )
-      showToast('Flow run scheduled', 'success')
+      name.value=flowyRun.value.name
+      console.log('flowRun', flowyRun, fr)
+      const p = h(RunButtonToastMessage, { name: name.value })
+      showToast(p)
     } catch (errorMessage) {
+      console.log(errorMessage)
       showToast('Failed to schedule flow run', 'error')
     } finally {
       loading.value = false
---
+++ src/components/RunButtonToastMessage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="toast-message">
+    Flow Run {{ runName }} Scheduled
+    <p-button @click="handleClick">
+      Go To Run
+    </p-button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { PButton } from '@prefecthq/prefect-design'
+  import { computed } from '@vue/reactivity'
+  // import { FlowRun } from '@/models/FlowRun'
+
+  import { useStore } from '@/stores/toast'
+
+
+  // const store = useStore()
+
+  const props = defineProps<{
+    name: string,
+  }>()
+
+
+  const runName = computed(() => {
+    console.log('name', props.name)
+    // return store.runName
+    return props.name
+  })
+  const handleClick = (): void=> console.log('clicked')
+</script>
