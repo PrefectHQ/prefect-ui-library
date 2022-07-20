942d56fc9bef98f8ec927ee8249bc6b2987944f6
Evan Sutherland <stackoverfloweth@gmail.com>
2022-07-20 17:09:44 -0500

    using PermissionValue type

--- src/types/permissions.ts
+++ src/types/permissions.ts
@@ -1,5 +1,7 @@
 import { InjectionKey } from 'vue'
 
+type PermissionValue = boolean | undefined
+
 const permissionActions = [
   'create',
   'read',
@@ -23,13 +25,13 @@ const workspacePermissionKeys = [
   'workspace_user_access',
 ] as const
 export type WorkspacePermissionKey = typeof workspacePermissionKeys[number]
-type WorkspacePermissions = Record<WorkspacePermissionKey, boolean>
+type WorkspacePermissions = Record<WorkspacePermissionKey, PermissionValue>
 
 export function isWorkspacePermissionKey(key: string): key is WorkspacePermissionKey {
   return workspacePermissionKeys.includes(key as WorkspacePermissionKey)
 }
 
-function getWorkspacePermissions(check: (key: WorkspacePermissionKey) => boolean | undefined): WorkspacePermissions {
+function getWorkspacePermissions(check: (key: WorkspacePermissionKey) => PermissionValue): WorkspacePermissions {
   return workspacePermissionKeys.reduce<WorkspacePermissions>((reduced, key) => ({
     ...reduced,
     [key]: check(key),
@@ -48,13 +50,13 @@ const accountPermissionKeys = [
   'workspace_role',
 ] as const
 export type AccountPermissionKey = typeof accountPermissionKeys[number]
-type AccountPermissions = Record<AccountPermissionKey, boolean>
+type AccountPermissions = Record<AccountPermissionKey, PermissionValue>
 
 export function isAccountPermissionKey(key: string): key is AccountPermissionKey {
   return accountPermissionKeys.includes(key as AccountPermissionKey)
 }
 
-function getAccountPermissions(check: (key: AccountPermissionKey) => boolean | undefined): AccountPermissions {
+function getAccountPermissions(check: (key: AccountPermissionKey) => PermissionValue): AccountPermissions {
   return accountPermissionKeys.reduce<AccountPermissions>((reduced, key) => ({
     ...reduced,
     [key]: check(key),
@@ -68,13 +70,13 @@ const featureFlags = [
   'organizations',
 ] as const
 export type FeatureFlag = typeof featureFlags[number]
-type FeatureFlagPermissions = Record<FeatureFlag, boolean | undefined>
+type FeatureFlagPermissions = Record<FeatureFlag, PermissionValue>
 
 export function isFeatureFlag(key: string): key is FeatureFlag {
   return featureFlags.includes(key as FeatureFlag)
 }
 
-function getFeatureFlagPermissions(check: (key: FeatureFlag) => boolean | undefined): FeatureFlagPermissions {
+function getFeatureFlagPermissions(check: (key: FeatureFlag) => PermissionValue): FeatureFlagPermissions {
   return featureFlags.reduce<FeatureFlagPermissions>((reduced, key) => ({
     ...reduced,
     [key]: check(key),
@@ -84,12 +86,12 @@ function getFeatureFlagPermissions(check: (key: FeatureFlag) => boolean | undefi
 export type AccountPermissionString = `${PermissionAction}:${AccountPermissionKey}`
 export type WorkspacePermissionString = `${PermissionAction}:${WorkspacePermissionKey}`
 
-export type AppPermissions = Record<PermissionAction, Record<AccountPermissionKey, boolean | undefined> & Record<WorkspacePermissionKey, boolean | undefined>>
+export type AppPermissions = Record<PermissionAction, Record<AccountPermissionKey, PermissionValue> & Record<WorkspacePermissionKey, PermissionValue>>
 export type AppFeatureFlags = Record<'access', FeatureFlagPermissions>
 
 export function getAppPermissions(
-  checkPermission: (action: PermissionAction, key: AccountPermissionKey | WorkspacePermissionKey) => boolean | undefined,
-  checkFeatureFlag: (key: FeatureFlag) => boolean | undefined,
+  checkPermission: (action: PermissionAction, key: AccountPermissionKey | WorkspacePermissionKey) => PermissionValue,
+  checkFeatureFlag: (key: FeatureFlag) => PermissionValue,
 ): Can {
   return {
     ...permissionActions.reduce<Can>((result, action) => ({
