3cafd29c721aa8132ce2c00444610524fb9046f1
Craig Harshbarger <pleek91@gmail.com>
2022-07-22 16:32:56 -0500

    Fix the demo

--- demo/sections/BlockSchemasSection.vue
+++ demo/sections/BlockSchemasSection.vue
@@ -1,7 +1,7 @@
 <template>
   <DemoSection heading="Block Schemas">
     <DemoSubSection heading="Form">
-      <BlockSchemaForm v-model:data="data" v-model:name="name" :block-schema="blockSchema" />
+      <BlockSchemaCreateForm v-model:data="data" v-model:name="name" :block-schema="blockSchema" />
       {{ data }}
     </DemoSubSection>
   </DemoSection>
@@ -11,7 +11,7 @@
   import { ref } from 'vue'
   import DemoSection from '../components/DemoSection.vue'
   import DemoSubSection from '../components/DemoSubSection.vue'
-  import BlockSchemaForm from '@/components/BlockSchemaForm.vue'
+  import BlockSchemaCreateForm from '@/components/BlockSchemaCreateForm.vue'
   import { BlockDocumentData } from '@/models'
   import { mocker } from '@/services'
 
