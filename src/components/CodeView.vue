d497cfaa323a7f21413024cfa4f1b2778a1fb9e8
Craig Harshbarger <pleek91@gmail.com>
2022-07-25 16:56:55 -0500

    Support syntax highlighting in the CodeSnippet component

---
+++ src/components/CodeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <p-code class="code-view" multiline>
+    {{ value }}
+  </p-code>
+</template>
+
+<script lang="ts" setup>
+  defineProps<{
+    value?: string,
+  }>()
+</script>
+
+<style>
+.code-view { @apply
+  p-4
+}
+</style>
