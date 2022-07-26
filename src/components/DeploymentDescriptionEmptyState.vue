54886fcbc60dc4b0c435647770b765cd7131e1ae
nicholas <znicholasbrown@gmail.com>
2022-07-25 21:23:09 -0400

    Add empty state for deployment description component

---
+++ src/components/DeploymentDescriptionEmptyState.vue
@@ -0,0 +1,25 @@
+<template>
+  <p-empty-state>
+    <template #icon>
+      <p-icon icon="MenuAlt2Icon" />
+    </template>
+
+    <template #heading>
+      You haven't added a description of this deployment yet
+    </template>
+
+    <template #description>
+      You can do so by modifying your deployment here or by adding it as part of your
+      deployment manifest configuration.
+    </template>
+
+    <template #actions>
+      <DocumentationButton topic="deployments" />
+    </template>
+  </p-empty-state>
+</template>
+
+<script lang="ts" setup>
+  import { PEmptyState, PIcon } from '@prefecthq/prefect-design'
+  import DocumentationButton from './DocumentationButton.vue'
+</script>
