381dfbea27eb9297dab28f290a2b2a78921e6c74
nicholas <znicholasbrown@gmail.com>
2022-08-01 11:48:24 -0400

    Add some better form sections

--- src/components/RunForm.vue
+++ src/components/RunForm.vue
@@ -1,11 +1,33 @@
 <template>
   <slot :open="open" :close="close" />
   <p-modal v-model:showModal="showModal" title="Run">
-    <p-button-group v-model="nowOrLater" :options="nowOrLaterOptions" size="sm" />
+    <p-content>
+      <h3 class="run-form__section-header">
+        When?
+      </h3>
+
+      <p-button-group v-model="nowOrLater" :options="nowOrLaterOptions" size="sm" />
+
+      <template v-if="nowOrLater == 'later'">
+        <p-label label="Date">
+          <p-date-input v-model="date" show-time />
+        </p-label>
+      </template>
+    </p-content>
+
+    <p-divider v-if="deployment.parameters" />
+
+    <p-content v-if="deployment.parameters">
+      <h3 class="run-form__section-header">
+        Parameters
+      </h3>
+
+      <PydanticForm v-model="parameters" hide-footer :pydantic-schema="deployment.parameterOpenApiSchema" />
+    </p-content>
 
     <template #actions>
       <p-button type="submit" :disabled="disabled" @click="submit">
-        Run
+        Run {{ parametersHaveBeenModified ? '' : 'with default parameters' }}
       </p-button>
     </template>
   </p-modal>
@@ -13,8 +35,10 @@
 
 <script lang="ts" setup>
   import  { PButton, showToast, ButtonGroupOption } from '@prefecthq/prefect-design'
+  import { useField } from 'vee-validate'
   import { ref, h, computed } from 'vue'
   import { RouteLocationRaw, useRouter } from 'vue-router'
+  import PydanticForm from './PydanticForm.vue'
   import RunButtonToastMessage from './RunButtonToastMessage.vue'
   import { useShowModal } from '@/compositions'
   import { localization } from '@/localization'
@@ -24,13 +48,29 @@
   import { canKey } from '@/types'
   import { inject } from '@/utilities'
 
-  defineProps<{
+  const props = defineProps<{
     deployment: Deployment,
   }>()
+
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  const initialValues = { ...props.deployment.parameters ?? {} }
+  const initialTypes: Record<keyof typeof initialValues, string> = { }
+
+  Object.keys(initialValues).forEach((key: keyof typeof initialValues) => {
+    const parameter = initialValues[key]
+    if (typeof parameter == 'string' && Date.parse(parameter)) {
+      initialValues[key] = new Date(parameter)
+    }
+
+    initialTypes[key] = typeof parameter
+  })
+
   const { showModal, open, close } = useShowModal()
   const can = inject(canKey)
   const deploymentsApi = inject(deploymentsApiKey)
   const loading = ref(false)
+  const date = ref(new Date())
+  const { value: parameters } = useField<Record<string, unknown>>('parameters', undefined, { initialValue: initialValues })
   const nowOrLater = ref('now')
   const nowOrLaterOptions: ButtonGroupOption[] = [{ label: 'Now', value: 'now' }, { label: 'Later...', value: 'later' }]
 
@@ -43,6 +83,10 @@
 
   const router = useRouter()
   const flowRunRoute = inject(flowRunRouteKey)
+
+  const parametersHaveBeenModified = computed(() => {
+    return JSON.stringify(props.deployment.parameters) !== JSON.stringify(parameters.value)
+  })
 
   const submit = async (deployment: Deployment): Promise<void> => {
     loading.value = true
@@ -70,9 +114,10 @@
 </script>
 
 <style>
-.run-button__run-icon { @apply
-  w-5
-  h-5
+.run-form__section-header {
+  @apply
+  text-lg
+  font-semibold
 }
 </style>
 
