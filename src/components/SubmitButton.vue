4babbb29dee680c226fea8536e6a6c8c84b79dde
Marichka Offen <marichka@prefect.io>
2022-08-01 11:12:44 -0400

    Remove utility and create SubmitButton component with type FormAction

---
+++ src/components/SubmitButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <p-button type="submit" :disabled="disabled" :loading="loading">
+    {{ action }}
+  </p-button>
+</template>
+
+<script lang="ts" setup>
+  import { withDefaults } from 'vue'
+  import { FormAction } from '@/types/buttons'
+
+  withDefaults(defineProps<{
+    action?: FormAction,
+    disabled?: boolean,
+    loading?: boolean,
+  }>(), {
+    action: 'Save',
+  })
+</script>
