e98272e8bf453810673c733f9bbd835da0758b61
Marichka Offen <marichka@prefect.io>
2022-08-03 14:05:24 -0400

    Update headings for flow

--- src/components/PageHeadingFlow.vue
+++ src/components/PageHeadingFlow.vue
@@ -11,12 +11,19 @@
   import FlowMenu from '@/components/FlowMenu.vue'
   import PageHeading from '@/components/PageHeading.vue'
   import { Flow } from '@/models'
+  import { flowsRouteKey } from '@/router'
+  import { inject } from '@/utilities'
 
   const props = defineProps<{
     flow: Flow,
   }>()
 
-  const crumbs = computed(() => [{ text: props.flow.name }])
+  const flowsRoute = inject(flowsRouteKey)
+
+  const crumbs = computed(() => [
+    { text: 'Flows', to: flowsRoute() },
+    { text: props.flow.name },
+  ])
   const emit = defineEmits(['delete'])
   const deleteFlow = (id: string): void => {
     emit('delete', id)
