932842c09e8c2ea27bb118c38aeedfff663bc218
Marichka Offen <marichka@prefect.io>
2022-08-03 15:27:53 -0400

    Update PageHeadingDeploymentEdit.vue

--- src/components/PageHeadingDeploymentEdit.vue
+++ src/components/PageHeadingDeploymentEdit.vue
@@ -1,9 +1,14 @@
 <template>
-  <page-heading class="page-heading-deployment-edit" :crumbs="crumbs" />
+  <page-heading class="page-heading-deployment-edit" :crumbs="crumbs">
+    <slot>
+      <FlowIconText :flow-id="deployment.flowId" />
+    </slot>
+  </page-heading>
 </template>
 
 <script lang="ts" setup>
   import { computed } from 'vue'
+  import FlowIconText from '@/components/FlowIconText.vue'
   import PageHeading from '@/components/PageHeading.vue'
   import { Deployment } from '@/models'
   import { deploymentRouteKey, deploymentsRouteKey } from '@/router'
