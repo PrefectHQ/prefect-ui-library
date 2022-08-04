1a8dd5b358235e93ab5bce32b1d27c417acd5b64
nicholas <znicholasbrown@gmail.com>
2022-08-04 11:16:44 -0400

    Add missing empty state components

--- demo/sections/EmptyStateSection.vue
+++ demo/sections/EmptyStateSection.vue
@@ -6,13 +6,19 @@
     <DemoSubSection heading="Deployments">
       <DeploymentsPageEmptyState />
     </DemoSubSection>
+    <DemoSubSection heading="Deployment Description">
+      <DeploymentDescriptionEmptyState :deployment="deployment" />
+    </DemoSubSection>
     <DemoSubSection heading="Flow">
       <FlowsPageEmptyState />
+    </DemoSubSection>
+    <DemoSubSection heading="Flow Runs">
+      <FlowRunsPageEmptyState />
     </DemoSubSection>
     <DemoSubSection heading="Notifications">
       <NotificationsPageEmptyState />
     </DemoSubSection>
-    <DemoSubSection heading="Queues">
+    <DemoSubSection heading="Work Queues">
       <WorkQueuesPageEmptyState />
     </DemoSubSection>
   </DemoSection>
@@ -22,9 +28,14 @@
   import DemoSection from '../components/DemoSection.vue'
   import DemoSubSection from '../components/DemoSubSection.vue'
   import BlocksPageEmptyState from '@/components/BlocksPageEmptyState.vue'
+  import DeploymentDescriptionEmptyState from '@/components/DeploymentDescriptionEmptyState.vue'
   import DeploymentsPageEmptyState from '@/components/DeploymentsPageEmptyState.vue'
+  import FlowRunsPageEmptyState from '@/components/FlowRunsPageEmptyState.vue'
   import FlowsPageEmptyState from '@/components/FlowsPageEmptyState.vue'
   import NotificationsPageEmptyState from '@/components/NotificationsPageEmptyState.vue'
   import WorkQueuesPageEmptyState from '@/components/WorkQueuesPageEmptyState.vue'
+  import { mocker } from '@/services'
+
+  const deployment = mocker.create('deployment')
 </script>
 
