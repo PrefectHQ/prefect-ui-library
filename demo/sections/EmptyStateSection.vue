73fb8143f460fbf163dfc8efd372b46fd20d61d4
nicholas <znicholasbrown@gmail.com>
2022-08-04 11:14:36 -0400

    Add blocks empty state to empty state section

--- demo/sections/EmptyStateSection.vue
+++ demo/sections/EmptyStateSection.vue
@@ -1,16 +1,19 @@
 <template>
   <DemoSection heading="Empty States">
-    <DemoSubSection heading="Flow">
-      <FlowsPageEmptyState />
+    <DemoSubSection heading="Blocks">
+      <BlocksPageEmptyState />
     </DemoSubSection>
     <DemoSubSection heading="Deployments">
       <DeploymentsPageEmptyState />
     </DemoSubSection>
-    <DemoSubSection heading="Queues">
-      <WorkQueuesPageEmptyState />
+    <DemoSubSection heading="Flow">
+      <FlowsPageEmptyState />
     </DemoSubSection>
     <DemoSubSection heading="Notifications">
       <NotificationsPageEmptyState />
+    </DemoSubSection>
+    <DemoSubSection heading="Queues">
+      <WorkQueuesPageEmptyState />
     </DemoSubSection>
   </DemoSection>
 </template>
@@ -18,6 +21,7 @@
 <script lang="ts" setup>
   import DemoSection from '../components/DemoSection.vue'
   import DemoSubSection from '../components/DemoSubSection.vue'
+  import BlocksPageEmptyState from '@/components/BlocksPageEmptyState.vue'
   import DeploymentsPageEmptyState from '@/components/DeploymentsPageEmptyState.vue'
   import FlowsPageEmptyState from '@/components/FlowsPageEmptyState.vue'
   import NotificationsPageEmptyState from '@/components/NotificationsPageEmptyState.vue'
