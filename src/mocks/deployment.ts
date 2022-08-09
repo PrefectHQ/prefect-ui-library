0a64fc36feab9428be34202f6ec9b5650808eeef
Chris White <chris@prefect.io>
2022-08-08 17:32:31 -0700

    Expand references to path and entrypoint

--- src/mocks/deployment.ts
+++ src/mocks/deployment.ts
@@ -16,6 +16,8 @@ export const randomDeployment: MockFunction<Deployment, [Partial<Deployment>?]> 
     parameterOpenApiSchema: {},
     tags: this.createMany('string', 3),
     manifestPath: this.create('id'),
+    path: this.create('id'),
+    entrypoint: this.create('id'),
     storageDocumentId: this.create('id'),
     infrastructureDocumentId: this.create('id'),
     deprecated: false,
