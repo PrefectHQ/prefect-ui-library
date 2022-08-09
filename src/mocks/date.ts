24cd387894f9ce090ba0b2504508213427171b82
nicholas <znicholasbrown@gmail.com>
2022-08-09 10:14:16 -0400

    Simplify date mocker

--- src/mocks/date.ts
+++ src/mocks/date.ts
@@ -14,13 +14,5 @@ export const randomDate: MockFunction<Date, [Date?, Date?]> = function(start, en
 }
 
 export const randomDateString: MockFunction<DateString, [Date?, Date?]> = function(start, end) {
-  if (!start) {
-    start = new Date(0)
-  }
-
-  if (!end) {
-    end = new Date()
-  }
-
-  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toISOString()
+  return this.create('date', [start, end]).toISOString()
 }
