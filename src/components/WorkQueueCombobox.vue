681116396f3bae443e95a7f51d60dd3f023e4c96
nicholas <znicholasbrown@gmail.com>
2022-08-10 15:23:42 -0400

    Add work queue combo box component

---
+++ src/components/WorkQueueCombobox.vue
@@ -0,0 +1,49 @@
+<template>
+  <p-combobox v-model="internalValue" :options="options" :empty-message="emptyMessage">
+    <template #options-empty>
+      No work queues
+    </template>
+  </p-combobox>
+</template>
+
+<script lang="ts" setup>
+  import { PCombobox, SelectOption } from '@prefecthq/prefect-design'
+  import { useSubscription } from '@prefecthq/vue-compositions'
+  import { computed } from 'vue'
+  import { workQueuesApiKey } from '@/services/WorkQueuesApi'
+  import { inject } from '@/utilities/inject'
+
+  const props = defineProps<{
+    selected: string | string[] | null | undefined,
+    emptyMessage?: string,
+  }>()
+
+  const emits = defineEmits<{
+    (event: 'update:selected', value: string | string[] | null): void,
+  }>()
+
+  const multiple = computed(() => Array.isArray(props.selected))
+
+  const internalValue = computed({
+    get() {
+      return props.selected ?? null
+    },
+    set(value: string | string[] | null) {
+      if (!value) {
+        emits('update:selected', null)
+      } else if (multiple.value) {
+        emits('update:selected', Array.isArray(value) ? value : [value])
+      } else {
+        emits('update:selected', value)
+      }
+    },
+  })
+
+  const workQueuesApi = inject(workQueuesApiKey)
+  const workQueuesSubscription = useSubscription(workQueuesApi.getWorkQueues, [{}])
+  const workQueues = computed(() => workQueuesSubscription.response ?? [])
+  const options = computed<SelectOption[]>(() => workQueues.value.map(workQueue => ({
+    value: workQueue.id,
+    label: workQueue.name,
+  })))
+</script>
