da956a29fdb7607085da24fac1f32f952efb48a7
nicholas <znicholasbrown@gmail.com>
2022-08-17 14:13:08 -0400

    Add PageHeadingFlowRunRadar component

---
+++ src/components/PageHeadingFlowRunRadar.vue
@@ -0,0 +1,72 @@
+<template>
+  <page-heading class="page-heading-flow-run" :crumbs="crumbs">
+    <template #actions>
+      <p-icon-button-menu>
+        <template #default>
+          <copy-overflow-menu-item label="Copy ID" :item="flowRun.id" />
+          <p-overflow-menu-item v-if="can.delete.flow_run" label="Delete" @click="open" />
+        </template>
+      </p-icon-button-menu>
+      <ConfirmDeleteModal
+        v-model:showModal="showModal"
+        label="Flow Run"
+        :name="flowRun.name!"
+        @delete="deleteFlowRun(flowRun.id)"
+      />
+    </template>
+    <slot>
+      <div class="page-heading-flow-run__header-meta">
+        <StateBadge :state="flowRun.state" />
+        <DurationIconText :duration="flowRun.duration" />
+        <FlowIconText :flow-id="flowRun.flowId" />
+      </div>
+    </slot>
+  </page-heading>
+</template>
+
+<script lang="ts" setup>
+  import { PIconButtonMenu } from '@prefecthq/prefect-design'
+  import { computed } from 'vue'
+  import { StateBadge, PageHeading, DurationIconText, FlowIconText, CopyOverflowMenuItem, ConfirmDeleteModal } from '@/components'
+  import { useShowModal } from '@/compositions/useShowModal'
+  import { FlowRun } from '@/models'
+  import { flowRunRouteKey, flowRunsRouteKey } from '@/router'
+  import { flowRunsApiKey } from '@/services'
+  import { canKey } from '@/types'
+  import { deleteItem, inject } from '@/utilities'
+
+  const flowRunsApi = inject(flowRunsApiKey)
+  const can = inject(canKey)
+
+  const props = defineProps<{
+    flowRun: FlowRun,
+  }>()
+
+  const { showModal, open } = useShowModal()
+
+  const flowRunsRoute = inject(flowRunsRouteKey)
+  const flowRunRoute = inject(flowRunRouteKey)
+
+  const crumbs = computed(() => [
+    { text: 'Flow Runs', to: flowRunsRoute() },
+    { text: props.flowRun.name ?? '', to: flowRunRoute(props.flowRun.id) },
+    { text: 'Radar' },
+  ])
+
+  const emit = defineEmits(['delete'])
+
+  const deleteFlowRun = async (id: string): Promise<void> => {
+    await deleteItem(id, flowRunsApi.deleteFlowRun, 'Flow run')
+    emit('delete', id)
+  }
+</script>
+
+<style>
+.page-heading-flow-run__header-meta {
+  @apply
+  flex
+  gap-2
+  items-center
+  xl:hidden
+}
+</style>
