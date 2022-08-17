8b6aba0bdbfaf47b314222e4dcc7fd6b04f4e765
nicholas <znicholasbrown@gmail.com>
2022-08-17 15:16:02 -0400

    Scaffold new markdown view and input components

---
+++ src/components/MarkdownInput.vue
@@ -0,0 +1,118 @@
+<template>
+  <p-base-input class="markdown-input">
+    <template v-for="(index, name) in $slots" #[name]="scope">
+      <slot :name="name" v-bind="scope" />
+    </template>
+    <template #control="{ attrs }">
+      <textarea
+        ref="inputArea"
+        v-model="internalValue"
+        spellcheck="false"
+        class="markdown-input__input-area"
+        v-bind="attrs"
+        @scroll="handleScroll"
+      />
+      <div ref="viewArea" class="markdown-input__view-area">
+        <MarkdownView :value="internalValue" class="markdown-input__markdown-view" v-bind="attrs" />
+      </div>
+    </template>
+  </p-base-input>
+</template>
+
+<script lang="ts" setup>
+  import { computed, ref } from 'vue'
+  import MarkdownView from './MarkdownView.vue'
+
+  const props = defineProps<{
+    modelValue: string,
+  }>()
+
+  const emit = defineEmits<{
+    (event: 'update:modelValue', value: string): void,
+  }>()
+
+  const inputArea = ref<HTMLTextAreaElement>()
+  const viewArea = ref<HTMLDivElement>()
+
+
+  const internalValue = computed({
+    get() {
+      return props.modelValue
+    },
+    set(val: string) {
+      emit('update:modelValue', val)
+    },
+  })
+
+  // This produces a scroll-linking effect
+  const handleScroll = (): void => {
+    if (!inputArea.value || !viewArea.value) {
+      return
+    }
+
+    viewArea.value.scrollTop = inputArea.value.scrollTop
+    viewArea.value.scrollLeft = inputArea.value.scrollLeft
+
+  }
+</script>
+
+<style>
+.markdown-input {
+  resize: inherit;
+  z-index: 1;
+
+  @apply
+  relative
+  block
+  min-h-[100px]
+  font-mono
+  text-sm
+  overflow-hidden
+}
+
+.markdown-input__input-area {
+  font-size: inherit;
+  font-family: inherit;
+  line-height: inherit;
+  border: none !important;
+  outline: none !important;
+  box-shadow: none !important;
+  resize: inherit;
+  z-index: -1;
+
+  @apply
+  block
+  absolute
+  w-full
+  h-full
+  min-h-[inherit]
+  text-transparent
+  p-4
+  m-0
+  whitespace-nowrap
+}
+
+.markdown-input__view-area {
+  font-size: inherit;
+  font-family: inherit;
+  line-height: inherit;
+
+  @apply
+  absolute
+  w-full
+  h-full
+  min-h-[inherit]
+  overflow-hidden
+  top-0
+  left-0
+  p-4
+  pointer-events-none
+  z-0
+}
+
+.markdown-input__markdown-view {
+  @apply
+  !bg-transparent
+  !p-0
+}
+</style>
