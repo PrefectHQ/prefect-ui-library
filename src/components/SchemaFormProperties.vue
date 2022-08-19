54e0745d726661ded8fe3717215370c9f660b273
Craig Harshbarger <pleek91@gmail.com>
2022-08-19 13:40:03 -0400

    Better looking nested inputs

--- src/components/SchemaFormProperties.vue
+++ src/components/SchemaFormProperties.vue
@@ -1,8 +1,10 @@
 <template>
   <p-label :label="property.title" :description="property.description" class="schema-form-properties">
-    <template v-for="(prop, key) in property.properties" :key="key">
-      <SchemaFormProperty :property="prop" :prop-key="`${propKey}.${key}`" />
-    </template>
+    <p-content class="schema-form-properties__fields">
+      <template v-for="(prop, key) in property.properties" :key="key">
+        <SchemaFormProperty :property="prop" :prop-key="`${propKey}.${key}`" />
+      </template>
+    </p-content>
   </p-label>
 </template>
 
@@ -15,3 +17,12 @@
     property: SchemaProperty,
   }>()
 </script>
+
+<style>
+.schema-form-properties__fields { @apply
+  border-gray-300
+  border-[1px]
+  p-4
+  rounded
+}
+</style>
