28eb292cafa1004c999c44a85f7bc5bc9382ef6b
nicholas <znicholasbrown@gmail.com>
2022-09-01 14:35:14 -0400

    Add workos-auth feature flag

--- src/types/permissions.ts
+++ src/types/permissions.ts
@@ -102,6 +102,7 @@ const featureFlags = [
   'access:notifications',
   'access:organizations',
   'access:retention',
+  'access:workos-auth',
 ] as const
 
 export type FeatureFlagString = typeof featureFlags[number]
