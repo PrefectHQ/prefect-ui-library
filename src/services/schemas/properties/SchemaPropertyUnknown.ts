1985fe8d3144ff9297342bc7ca1eb89b01820679
Craig Harshbarger <pleek91@gmail.com>
2022-09-12 15:02:56 -0500

    Remove most of the logic from the unknown property service

--- src/services/schemas/properties/SchemaPropertyUnknown.ts
+++ src/services/schemas/properties/SchemaPropertyUnknown.ts
@@ -1,35 +1,17 @@
 import { SchemaPropertyService } from './SchemaPropertyService'
-import { isValidJsonString, ValidationRule, withMessage } from '@/services/validate'
 import { SchemaValue } from '@/types/schemas'
+import { parseUnknownJson } from '@/utilities/json'
 
 export class SchemaPropertyUnknown extends SchemaPropertyService {
 
-
-  // todo: I think sometime this should have a component. But it messes up allOf and anyOf
   public readonly component = null
   public readonly default = null
 
-  protected get validators(): ValidationRule[] {
-    return [withMessage(isValidJsonString, `${this.property.title} must be valid JSON`)]
-  }
-
   protected request(value: SchemaValue): unknown {
-    try {
-      if (typeof value === 'string') {
-        return JSON.parse(value)
-      }
-    } catch (error) {
-      console.error(error)
-    }
-
-    return value
+    return parseUnknownJson(value)
   }
 
   protected response(value: SchemaValue): unknown {
-    if (!isValidJsonString(value)) {
-      return JSON.stringify(value)
-    }
-
     return value
   }
 
