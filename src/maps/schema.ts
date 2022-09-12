8322d80d9686f852945c55c568fa9f3f2b332375
Craig Harshbarger <pleek91@gmail.com>
2022-09-12 13:28:05 -0500

    Fix $refs in the mapper

--- src/maps/schema.ts
+++ src/maps/schema.ts
@@ -1,18 +1,25 @@
 import { SchemaDefinitionsResponse, SchemaPropertiesResponse, SchemaPropertyResponse, SchemaResponse } from '@/models/api/SchemaResponse'
 import { MapFunction } from '@/services/Mapper'
+import { resolveSchema } from '@/services/schemas/resolvers/schemas'
 import { Schema, SchemaDefinitions, SchemaProperties, SchemaProperty } from '@/types/schemas'
 import { mapEntries, mapSnakeToCamelCase } from '@/utilities'
 
 export const mapSchemaResponseToSchema: MapFunction<SchemaResponse, Schema> = function(source: SchemaResponse): Schema {
   // eslint-disable-next-line camelcase, @typescript-eslint/no-unused-vars
-  const { definitions, block_schema_references, properties, ...rest } = source
+  const { definitions, block_schema_references, properties, $ref, ...rest } = source
 
-  return {
+  const mapped: Schema = {
     ...mapSnakeToCamelCase({ ...rest }),
     properties: this.map('SchemaPropertiesResponse', properties, 'SchemaProperties'),
     definitions: this.map('SchemaDefinitionsResponse', definitions, 'SchemaDefinitions'),
     // blockSchemaReferences: this.map('BlockSchemaReferencesResponse', block_schema_references, 'BlockSchemaReferences'),
   }
+
+  if ($ref) {
+    mapped.$ref = $ref
+  }
+
+  return resolveSchema(mapped)
 }
 
 export const mapSchemaDefinitionsResponseToSchemaDefinitions: MapFunction<SchemaDefinitionsResponse, SchemaDefinitions> = function(source: SchemaDefinitionsResponse): SchemaDefinitions {
@@ -24,10 +31,16 @@ export const mapSchemaPropertiesResponseToSchemaProperties: MapFunction<SchemaPr
 }
 
 export const mapSchemaPropertyResponseToSchemaProperty: MapFunction<SchemaPropertyResponse, SchemaProperty> = function(source: SchemaPropertyResponse): SchemaProperty {
-  const { properties, ...rest } = source
+  const { properties, $ref, ...rest } = source
 
-  return {
+  const mapped: SchemaProperty = {
     ...mapSnakeToCamelCase({ ...rest }),
     properties: this.map('SchemaPropertiesResponse', properties, 'SchemaProperties'),
   }
+
+  if ($ref) {
+    mapped.$ref = $ref
+  }
+
+  return mapped
 }
