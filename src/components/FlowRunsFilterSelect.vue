cda2853503143a3cc6dced152427cdbe91c7b165
Jennifer Grange <jenny@prefect.io>
2022-09-17 23:43:28 -0400

    add a flow run select component to the flowruns header

---
+++ src/components/FlowRunsFilterSelect.vue
@@ -0,0 +1,30 @@
+<template>
+  <p-select v-model="selectedFilter" :options="options" class="flow-runs-filter-select" />
+</template>
+
+<script setup lang="ts">
+  import { computed } from 'vue'
+
+  const props = defineProps<{
+    selected: string,
+  }>()
+
+  const emit = defineEmits<{
+    (event: 'update:selected', value: string): void,
+  }>()
+
+  const selectedFilter = computed({
+    get() {
+      return props.selected
+    },
+    set(value: string) {
+      emit('update:selected', value)
+    },
+  })
+
+  const options: { label: string, value: string }[] = [
+    { label: 'One Week (default)', value: 'week' },
+    { label: 'One Day', value: 'day' },
+    { label: 'No Scheduled Runs', value: 'noScheduled' },
+  ]
+</script>
--- src/components/PageHeadingFlowRuns.vue
+++ src/components/PageHeadingFlowRuns.vue
@@ -1,9 +1,31 @@
 <template>
-  <page-heading class="page-heading-flow-runs" :crumbs="crumbs" />
+  <page-heading class="page-heading-flow-runs" :crumbs="crumbs">
+    <template #actions>
+      <FlowRunsFilterSelect v-model:selected="selectedFilter" />
+    </template>
+  </page-heading>
 </template>
 
 <script lang="ts" setup>
+  import { computed } from 'vue'
+  import  FlowRunsFilterSelect from '@/components/FlowRunsFilterSelect.vue'
   import PageHeading from '@/components/PageHeading.vue'
 
   const crumbs = [{ text: 'Flow Runs' }]
+  const emit = defineEmits<{
+    (event: 'update:selected', value: string): void,
+  }>()
+
+  const props = defineProps<{
+    selected: string,
+  }>()
+
+  const selectedFilter = computed({
+    get() {
+      return props.selected
+    },
+    set(value: string) {
+      emit('update:selected', value)
+    },
+  })
 </script>
