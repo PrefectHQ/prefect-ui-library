39e05ce1138c9387fead6695e5b869d5632be93d
Craig Harshbarger <pleek91@gmail.com>
2022-09-19 11:56:43 -0500

    Account for properties with an undefined type possibly having `anyOf` or `allOf` references

--- src/services/schemas/properties/SchemaPropertyAny.ts
+++ src/services/schemas/properties/SchemaPropertyAny.ts
@@ -1,3 +1,4 @@
+import { SchemaPropertyComponentWithProps } from '../utilities'
 import { SchemaPropertyService } from './SchemaPropertyService'
 import { JsonInput } from '@/components'
 import { SchemaValue } from '@/types/schemas'
@@ -5,8 +6,15 @@ import { parseUnknownJson, stringifyUnknownJson } from '@/utilities/json'
 
 export class SchemaPropertyAny extends SchemaPropertyService {
 
-  public readonly component = this.withProps(JsonInput)
   public readonly default = ''
+
+  protected get component(): SchemaPropertyComponentWithProps {
+    if (this.has('anyOf') || this.has('allOf')) {
+      return null
+    }
+
+    return this.withProps(JsonInput)
+  }
 
   protected request(value: SchemaValue): unknown {
     return parseUnknownJson(value)
