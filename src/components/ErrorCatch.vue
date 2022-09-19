ddbdca7dc72fc931cb90f9cd3842543d6c8c2e2f
nicholas <znicholasbrown@gmail.com>
2022-09-19 15:59:08 -0400

    Add error catch component

---
+++ src/components/ErrorCatch.vue
@@ -0,0 +1,23 @@
+<template>
+  <template v-if="!error">
+    <slot />
+  </template>
+  <template v-else>
+    <slot name="error" :error="error">
+      <div class="error-catch">
+        <h1>Something went wrong</h1>
+        <p>{{ error }}</p>
+      </div>
+    </slot>
+  </template>
+</template>
+
+<script lang="ts" setup>
+  import { onErrorCaptured, ref } from 'vue'
+
+  const error = ref()
+
+  onErrorCaptured((err) => {
+    error.value = err
+  })
+</script>
