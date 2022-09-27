ff2a3b9e84f93ac684466cabdcfbba80ba40f3fb
Marichka Offen <marichka@prefect.io>
2022-09-27 17:11:54 -0400

    Add count subscription and use usePaginatedSubscription

--- src/components/FlowRunsControlsList.vue
+++ src/components/FlowRunsControlsList.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="flow-runs-controls_list">
     <div class="flow-runs-controls_list__controls">
-      <ResultsCount :count="flowRuns.length" class="mr-auto" label="Flow run" />
+      <ResultsCount :count="flowRunCount" class="mr-auto" label="Flow run" />
       <StateSelect v-model:selected="state" empty-message="All run states" class="flow-runs-controls_list__state-select" />
       <FlowRunsSort v-model="sort" class="flow-runs-controls_list__flow-runs-sort" />
     </div>
-    <FlowRunList v-if="flowRuns.length" :flow-runs="flowRuns" disabled :selected="[]" />
-    <PEmptyResults v-else>
+
+    <FlowRunList :selected="[]" :flow-runs="flowRuns" disabled @bottom="flowRunsSubscription.loadMore" />
+
+    <PEmptyResults v-if="empty">
       <template #message>
         No runs from the last 7 days
+      </template>
+      <template v-if="hasFilters" #actions>
+        <p-button size="sm" secondary @click="clear">
+          Clear Filters
+        </p-button>
       </template>
     </PEmptyResults>
   </div>
@@ -18,6 +25,7 @@
   import { useSubscription } from '@prefecthq/vue-compositions'
   import { computed, ref } from 'vue'
   import { ResultsCount, StateSelect, FlowRunsSort, FlowRunList } from '@/components'
+  import { usePaginatedSubscription } from '@/compositions/usePaginatedSubscription'
   import { StateType } from '@/models'
   import { flowRunsApiKey, mapper } from '@/services'
   import { FlowRunSortValues, UnionFilters } from '@/types'
@@ -31,7 +39,7 @@
 
   const state = ref<StateType[]>([])
   const sort = ref<FlowRunSortValues>('EXPECTED_START_TIME_DESC')
-
+  const hasFilters = computed(() => state.value.length)
 
   const flowRunsFilter = computed<UnionFilters>(() => {
     const runFilter: UnionFilters = {
@@ -46,12 +54,25 @@
         },
       }
     }
+    // else {
+    //   runFilter.flow_runs!.state = {}
+    // }
 
     return runFilter
   })
 
-  const flowRunsSubscription = useSubscription(flowRunsApi.getFlowRuns, [flowRunsFilter], { interval: 30000 })
+  const flowRunCountSubscription = useSubscription(flowRunsApi.getFlowRunsCount, [flowRunsFilter], { interval: 30000 })
+  const flowRunCount = computed(() => flowRunCountSubscription.response)
+
+
+  const flowRunsSubscription = usePaginatedSubscription(flowRunsApi.getFlowRuns, [flowRunsFilter], { interval: 30000 })
   const flowRuns = computed(() => flowRunsSubscription.response ?? [])
+
+  const empty = computed(() => flowRunsSubscription.executed && flowRuns.value.length === 0)
+
+  function clear(): void {
+    state.value = []
+  }
 </script>
 
 <style>
