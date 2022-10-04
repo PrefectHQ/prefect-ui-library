19a7ded9bcedd6fb40995a2e313a410dab71453f
Marichka Offen <marichka@prefect.io>
2022-10-04 17:00:32 -0400

    Add multi-delete functionality to FlowRunFilteredList

--- src/components/FlowRunFilteredList.vue
+++ src/components/FlowRunFilteredList.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="flow-runs-controls_list">
-    <div class="flow-runs-controls_list__controls">
+  <div class="flow-run-filtered-list">
+    <div class="flow-run-filtered-list__controls">
       <ResultsCount :count="flowRunCount" class="mr-auto" label="Flow run" />
-      <StateSelect :selected="state" empty-message="All run states" class="flow-runs-controls_list__state-select" @update:selected="updateState" />
-      <FlowRunsSort v-model="sort" class="flow-runs-controls_list__flow-runs-sort" />
+      <StateSelect :selected="state" empty-message="All run states" class="flow-run-filtered-list__state-select" @update:selected="updateState" />
+      <FlowRunsSort v-model="sort" class="flow-run-filtered-list__flow-runs-sort" />
+      <Transition name="slide-fade">
+        <p-button v-if="selectedFlowRuns.length > 0" danger icon="TrashIcon" @click="open" />
+      </Transition>
     </div>
-
-    <FlowRunList :selected="[]" :flow-runs="flowRuns" disabled @bottom="flowRunsSubscription.loadMore" />
-
+    <FlowRunList v-model:selected="selectedFlowRuns" :flow-runs="flowRuns" :disabled="disabled" @bottom="flowRunsSubscription.loadMore" />
     <PEmptyResults v-if="empty">
       <template #message>
         No runs from the last 7 days
@@ -19,13 +20,23 @@
       </template>
     </PEmptyResults>
   </div>
+  <ConfirmDeleteModal
+    v-model:showModal="showModal"
+    name="selected flow runs"
+    label="Flow Runs"
+    @delete="deleteFlowRuns(selectedFlowRuns)"
+  />
 </template>
 
 <script lang="ts" setup>
+  import { showToast } from '@prefecthq/prefect-design'
   import { useSubscription } from '@prefecthq/vue-compositions'
   import { computed, onMounted, ref } from 'vue'
   import { ResultsCount, StateSelect, FlowRunsSort, FlowRunList } from '@/components'
+  import ConfirmDeleteModal from '@/components/ConfirmDeleteModal.vue'
+  import { useShowModal } from '@/compositions'
   import { usePaginatedSubscription } from '@/compositions/usePaginatedSubscription'
+  import { localization } from '@/localization'
   import { StateType } from '@/models'
   import { flowRunsApiKey, mapper } from '@/services'
   import { FlowRunSortValues, UnionFilters } from '@/types'
@@ -36,14 +47,16 @@
   const props = defineProps<{
     flowRunFilter: UnionFilters,
     states?: StateType[],
+    disabled?: boolean,
   }>()
 
   const emit = defineEmits<{
     (event: 'update:states', value: StateType[]): void,
+    (event: 'delete'): void,
   }>()
 
+  const selectedFlowRuns = ref<string[]>([])
   const state = ref<StateType[]>(props.states ?? [])
-
   const updateState = (newValue: string | string[] | null): void => {
     state.value = newValue as StateType[]
     emit('update:states', state.value)
@@ -85,6 +98,30 @@
     state.value = []
   }
 
+  const { showModal, open, close } = useShowModal()
+  const deleteFlowRuns = async (flowRuns: string[]): Promise<void> => {
+    const toastMessage = computed(() => {
+      if (flowRuns.length === 1) {
+        return 'Flow run deleted'
+      }
+      return `${flowRuns.length} flow runs deleted`
+    })
+    try {
+      close()
+      const deleteFlowRuns = flowRuns.map(flowRunsApi.deleteFlowRun)
+      await Promise.all(deleteFlowRuns)
+
+      showToast(toastMessage, 'success')
+
+      await Promise.all([flowRunCountSubscription.refresh(), flowRunsSubscription.refresh()])
+
+      selectedFlowRuns.value = []
+      clear()
+    } catch (error) {
+      showToast(localization.error.delete('Flow Run'), 'error')
+    }
+  }
+
   onMounted(() => {
     if (props.states) {
       state.value = props.states
@@ -93,12 +130,12 @@
 </script>
 
 <style>
-.flow-runs-controls_list { @apply
+.flow-run-filtered-list { @apply
   grid
   gap-2
 }
 
-.flow-runs-controls_list__controls { @apply
+.flow-run-filtered-list__controls { @apply
   flex
   gap-2
   items-center
@@ -106,13 +143,25 @@
   sm:flex-row
 }
 
-.flow-runs-controls_list__state-select { @apply
+.flow-run-filtered-list__state-select { @apply
   w-full
   sm:w-52
 }
 
-.flow-runs-controls_list__flow-runs-sort { @apply
+.flow-run-filtered-list__flow-runs-sort { @apply
   w-full
   sm:w-fit
+}
+
+.slide-fade-enter-active {
+  transition: all 0.4s ease 0.1s;
+}
+.slide-fade-leave-active {
+  transition: all 0.1s ease;
+}
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(25px);
+  opacity: 0;
 }
 </style>
