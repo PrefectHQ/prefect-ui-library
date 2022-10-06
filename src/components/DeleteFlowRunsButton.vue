932ded3dfa93aed87f8c72f1521d76926eed0b8d
Marichka Offen <marichka@prefect.io>
2022-10-06 10:15:47 -0400

    Update DeleteFlowRunsButton.vue

--- src/components/DeleteFlowRunsButton.vue
+++ src/components/DeleteFlowRunsButton.vue
@@ -56,10 +56,10 @@
 
 <style>
   .delete-flow-runs-button-slide-enter-active {
-  transition: all 0.3s ease;
+  transition: all 0.4s ease;
 }
 .delete-flow-runs-button-slide-leave-active {
-  transition: all 0.3s ease;
+  transition: all 0.4s ease;
 }
 .delete-flow-runs-button-slide-enter-from,
 .delete-flow-runs-button-slide-leave-to {
