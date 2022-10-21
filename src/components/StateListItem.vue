75fc41b449d0674b02201c51339a2ecf17e5c96b
Josh <josh.furlin@gmail.com>
2022-10-21 13:53:51 -0500

    replaces grid in StateListItem with simpler flex

--- src/components/StateListItem.vue
+++ src/components/StateListItem.vue
@@ -1,19 +1,21 @@
 <template>
   <p-list-item-input v-model:selected="model" :value="value" class="state-list-item" :class="classes" disabled>
-    <div class="state-list-item__content" :class="contentClasses">
-      <div class="state-list-item__name">
-        <slot name="name" />
+    <div class="state-list-item__content">
+      <div class="state-list-item__top-section">
+        <div class="state-list-item__name">
+          <slot name="name" />
+        </div>
+        <template v-if="$slots.tags || tags.length">
+          <div class="state-list-item__tags">
+            <slot name="tags">
+              <p-tag-wrapper class="task-run-list-item-input__tags" v-bind="{ tags, justify }" />
+            </slot>
+          </div>
+        </template>
       </div>
       <div class="state-list-item__meta">
         <slot name="meta" />
       </div>
-      <template v-if="$slots.tags || tags.length">
-        <div class="state-list-item__tags">
-          <slot name="tags">
-            <p-tag-wrapper class="task-run-list-item-input__tags" v-bind="{ tags, justify }" />
-          </slot>
-        </div>
-      </template>
       <template v-if="$slots.relationships">
         <div class="state-list-item__relationships">
           <slot name="relationships" />
@@ -25,7 +27,7 @@
 
 <script lang="ts" setup>
   import { PListItemInput, CheckboxModel, media } from '@prefecthq/prefect-design'
-  import { computed, useSlots } from 'vue'
+  import { computed } from 'vue'
   import { StateType } from '@/models/StateType'
 
   const props = defineProps<{
@@ -51,22 +53,6 @@
   const classes = computed(() => `state-list-item--${props.stateType ?? 'unknown'}`)
   const justify = computed(() => media.md ? 'right' : 'left')
   const tags = computed(() => props.tags ?? [])
-
-  const slots = useSlots()
-
-  const contentClasses = computed(()=> {
-    if ((slots.tags || tags.value.length) && slots.relationships) {
-      return 'state-list-item__content--with-tags-and-relationships'
-    }
-    if (slots.tags || tags.value.length) {
-      return 'state-list-item__content--with-tags'
-    }
-    if (slots.relationships) {
-      return 'state-list-item__content--with-relationships'
-    }
-    return ''
-  },
-  )
 </script>
 
 <style>
@@ -99,58 +85,29 @@
 }
 
 .state-list-item__content { @apply
-  grid
-  gap-x-4
-  gap-y-2;
-
-  grid-template-areas: 'name'
-                       'meta'
+  flex
+  flex-col
+  gap-2
+  items-start
 }
 
-.state-list-item__content--with-tags {
-  grid-template-areas: 'name'
-                       'tags'
-                       'meta'
-}
-
-.state-list-item__content--with-relationships {
-  grid-template-areas: 'name'
-                       'meta'
-                       'relationships'
-}
-
-.state-list-item__content--with-tags-and-relationships {
-  grid-template-areas: 'name'
-                       'tags'
-                       'meta'
-                       'relationships'
-}
-
-@screen md {
-  .state-list-item__content--with-tags {
-    grid-template-columns: min-content 1fr;
-    grid-template-areas: 'name tags'
-                         'meta meta'
-  }
-}
-
-@screen md {
-  .state-list-item__content--with-tags-and-relationships {
-    grid-template-columns: min-content 1fr;
-    grid-template-areas: 'name tags'
-                         'meta meta'
-                         'relationships relationships';
-  }
+.state-list-item__top-section { @apply
+  flex
+  flex-col
+  w-full
+  gap-2
+  md:flex-row
+  md:gap-4
 }
 
 .state-list-item__name { @apply
- text-base
- text-slate-700
- shrink-0
- whitespace-nowrap
- grow-0
- text-ellipsis
- overflow-hidden
+  text-base
+  text-slate-700
+  shrink-0
+  whitespace-nowrap
+  grow-0
+  text-ellipsis
+  overflow-hidden
 }
 
 .state-list-item__meta { @apply
@@ -160,20 +117,13 @@
   gap-2
   whitespace-nowrap
   mr-1
-}
-
-@screen md {
-  .state-list-item__meta { @apply
-    items-center
-    flex-wrap
-    sm:flex-row
-  }
+  md:flex-row
+  md:flex-wrap
+  md:items-center
 }
 
 .state-list-item__tags { @apply
-  place-self-end
   min-w-0
-  w-full
   grow
 }
 
@@ -186,30 +136,9 @@
   gap-2
   whitespace-nowrap
   mr-1
-}
-
-@screen md {
-  .state-list-item__relationships { @apply
-    items-center
-    flex-wrap
-    flex-row
-    gap-4
-  }
-}
-
-.state-list-item__name {
-  grid-area: name;
-}
-
-.state-list-item__meta {
-  grid-area: meta;
-}
-
-.state-list-item__tags {
-  grid-area: tags;
-}
-
-.state-list-item__relationships {
-  grid-area: relationships;
+  md:items-center
+  md:flex-wrap
+  md:flex-row
+  md:gap-4
 }
 </style>
