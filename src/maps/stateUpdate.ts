b8d9a07010a51880c2cc9b75e320bd3166be2b2a
Jenny G <40272060+zhen0@users.noreply.github.com>
2022-10-25 17:36:57 -0400

    Update src/maps/stateUpdate.ts
    Co-authored-by: pleek91 <craig@prefect.io>

--- src/maps/stateUpdate.ts
+++ src/maps/stateUpdate.ts
@@ -2,7 +2,11 @@ import { StateType, StateUpdate, StateUpdateRequest } from '@/models'
 import { MapFunction } from '@/services/Mapper'
 
 export const mapStateUpdateToStateUpdateRequest: MapFunction<StateUpdate, StateUpdateRequest> = function(source: StateUpdate): StateUpdateRequest {
-  const uppercaseType: Uppercase<StateType> = source.state.type.toLocaleUpperCase() as Uppercase<StateType>
-  return { state: { ...source.state, type: uppercaseType } }
+return { 
+  state: { 
+    ...source.state, 
+    type: this.map('ServerStateType', source.type, 'StateType') 
+  } 
+}
 }
 
