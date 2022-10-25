abf795a9f0ec8d4baf8fb8d34e18402d203f64e7
Jennifer Grange <jenny@prefect.io>
2022-10-25 19:51:06 -0400

    fix mapper

--- src/maps/stateUpdate.ts
+++ src/maps/stateUpdate.ts
@@ -1,12 +1,12 @@
-import { StateType, StateUpdate, StateUpdateRequest } from '@/models'
+import { StateUpdate, StateUpdateRequest } from '@/models'
 import { MapFunction } from '@/services/Mapper'
 
 export const mapStateUpdateToStateUpdateRequest: MapFunction<StateUpdate, StateUpdateRequest> = function(source: StateUpdate): StateUpdateRequest {
-return { 
-  state: { 
-    ...source.state, 
-    type: this.map('ServerStateType', source.type, 'StateType') 
-  } 
-}
+  return {
+    state: {
+      ...source.state,
+      type: this.map('StateType', source.state.type, 'ServerStateType'),
+    },
+  }
 }
 
