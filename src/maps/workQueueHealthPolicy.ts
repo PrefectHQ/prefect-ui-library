5a8124fa91c4d0639ee997f4e21df69fdab0502b
Josh <josh.furlin@gmail.com>
2022-10-26 10:23:37 -0500

    implements mapSnakeToCamelCase util for work queue health policy map

--- src/maps/workQueueHealthPolicy.ts
+++ src/maps/workQueueHealthPolicy.ts
@@ -1,9 +1,9 @@
 import { WorkQueueHealthPolicy, WorkQueueHealthPolicyResponse } from '@/models'
 import { MapFunction } from '@/services/Mapper'
+import { mapSnakeToCamelCase } from '@/utilities'
 
 export const mapWorkQueueHealthPolicyResponseToWorkQueueHealthPolicy: MapFunction<WorkQueueHealthPolicyResponse, WorkQueueHealthPolicy> = function(source: WorkQueueHealthPolicyResponse): WorkQueueHealthPolicy {
   return {
-    maximumLateRuns: source.maximum_late_runs,
-    maximumSecondsSinceLastPolled: source.maximum_seconds_since_last_polled,
+    ...mapSnakeToCamelCase(source),
   }
 }
