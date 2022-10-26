533cc7082a9b837cacb026736abdec5f95f96f24
Craig Harshbarger <pleek91@gmail.com>
2022-10-26 14:36:59 -0500

    Add a mock for flowResponse

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -14,6 +14,7 @@ import { randomDate, randomDateString } from './date'
 import { randomDeployment } from './deployment'
 import { randomEmail } from './email'
 import { randomFlow } from './flow'
+import { randomFlowResponse } from './flowResponse'
 import { randomFlowRun } from './flowRun'
 import { randomFlowRunGraph, randomGraphNode } from './flowRunGraph'
 import { randomFlowRunHistory } from './flowRunHistory'
@@ -89,4 +90,5 @@ export const mocks = {
   workQueueFilter: randomWorkQueueFilter,
   workQueueHealthPolicy: randomWorkQueueHealthPolicy,
   workQueueStatus: randomWorkQueueStatus,
+  flowResponse: randomFlowResponse,
 }
