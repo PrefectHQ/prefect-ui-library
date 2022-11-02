baba3b8250e498a12284141b495652d4244e3543
Josh <josh.furlin@gmail.com>
2022-11-02 14:04:21 -0500

    makes all IconTexts use p-links, adds DateIconText

---
+++ src/components/DateIconText.vue
@@ -0,0 +1,16 @@
+<template>
+  <p-icon-text icon="CalendarIcon" class="date-icon-text">
+    <span>{{ label }}</span>
+  </p-icon-text>
+</template>
+
+<script lang="ts" setup>
+  import { computed } from 'vue'
+  import { formatDateTimeNumeric } from '@/utilities'
+
+  const props = defineProps<{
+    date: Date | null,
+  }>()
+
+  const label = computed(() => props.date == null ? 'None' : formatDateTimeNumeric(props.date))
+</script>
