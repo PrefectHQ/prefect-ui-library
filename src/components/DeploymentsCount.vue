a2ddc8f898b051cf11646b056b8c15f08f77295d
Marichka Offen <marichka@prefect.io>
2022-11-09 12:02:56 -0500

    Add visibility tracker

--- src/components/DeploymentsCount.vue
+++ src/components/DeploymentsCount.vue
@@ -1,12 +1,14 @@
 <template>
-  <template v-if="deploymentsCount">
-    {{ deploymentsCount }}
-  </template>
+  <div ref="el">
+    <template v-if="visible && deploymentsCount">
+      {{ deploymentsCount }}
+    </template>
+  </div>
 </template>
 
 <script lang="ts" setup>
-  import { useSubscriptionWithDependencies } from '@prefecthq/vue-compositions'
-  import { computed } from 'vue'
+  import { useIntersectionObserver, useSubscriptionWithDependencies } from '@prefecthq/vue-compositions'
+  import { computed, onMounted, ref } from 'vue'
   import { useWorkspaceApi } from '@/compositions'
 
   const props = defineProps<{
@@ -29,4 +31,22 @@
 
   const deploymentsCountSubscription = useSubscriptionWithDependencies(api.deployments.getDeploymentsCount, deploymentsCountFilter)
   const deploymentsCount = computed(() => deploymentsCountSubscription.response ?? null)
+
+  const visible = ref(false)
+  const el = ref<HTMLDivElement>()
+
+  function intersect(entries: IntersectionObserverEntry[]): void {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        visible.value = true
+        disconnect()
+      }
+    })
+  }
+
+  const { observe, disconnect } = useIntersectionObserver(intersect)
+
+  onMounted(() => {
+    observe(el)
+  })
 </script>
