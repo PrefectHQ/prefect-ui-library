5bd7fe2874db522a60c3eee24390f7881400d991
Josh <josh.furlin@gmail.com>
2022-11-09 14:49:29 -0600

    adds FlowRunDetails demo

---
+++ demo/sections/flowRuns/FlowRunDetails.vue
@@ -0,0 +1,28 @@
+<template>
+  <ComponentPage title="FlowRunDetails" :demos="demos">
+    <FlowRunDetails :flow-run="flowRun" />
+
+
+    <template #no-relations>
+      <FlowRunDetails :flow-run="flowRunWithNoRelations" />
+    </template>
+  </ComponentPage>
+</template>
+
+<script lang="ts" setup>
+  import FlowRunDetails from '@/components/FlowRunDetails.vue'
+  import ComponentPage from '@/demo/components/ComponentPage.vue'
+  import { useFlowRunMock } from '@/demo/compositions/useFlowRunsMock'
+  import { DemoSection } from '@/demo/types/demoSection'
+
+  const demos: DemoSection[] = [
+    { title: 'Disabled', description: 'With selection disabled' },
+    { title: 'No Relations', description: 'With no deployment or work queue' },
+  ]
+
+  const flowRun = useFlowRunMock()
+  const flowRunWithNoRelations = useFlowRunMock({
+    deploymentId: null,
+    workQueueName: null,
+  })
+</script>
