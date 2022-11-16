48fc7ffe611edf4985b490cd005f07465e11f79b
Marichka Offen <marichka@prefect.io>
2022-11-16 12:29:20 -0500

    Switch PageHeadingTaskRun to take TaskRunId and FlowRunId props instead of entire object

--- src/components/PageHeadingTaskRun.vue
+++ src/components/PageHeadingTaskRun.vue
@@ -1,13 +1,13 @@
 <template>
-  <page-heading v-if="taskRunDetails" class="page-heading-task-run" :crumbs="crumbs">
+  <page-heading v-if="taskRun" class="page-heading-task-run" :crumbs="crumbs">
     <template #after-crumbs>
-      <StateBadge :state="taskRunDetails.state" />
+      <StateBadge :state="taskRun.state" />
     </template>
     <template #actions>
       <p-icon-button-menu>
         <template #default>
           <p-overflow-menu-item v-if="showChangeStateMenuItemButton" label="Change state" @click="openChangeStateModal" />
-          <copy-overflow-menu-item label="Copy ID" :item="taskRunDetails.id" />
+          <copy-overflow-menu-item label="Copy ID" :item="taskRun.id" />
           <p-overflow-menu-item v-if="can.delete.task_run" label="Delete" @click="openDeleteModal" />
         </template>
       </p-icon-button-menu>
@@ -15,7 +15,7 @@
         v-model:showModal="showDeleteModal"
         label="Task Run"
         :name="taskRun.name!"
-        @delete="deleteTaskRun(taskRun.id)"
+        @delete="deleteTaskRun(taskRunId)"
       />
 
       <ConfirmStateChangeModal
@@ -36,31 +36,32 @@
   import { useWorkspaceApi } from '@/compositions'
   import { useCan } from '@/compositions/useCan'
   import { localization } from '@/localization'
-  import { StateUpdateDetails, TaskRun, terminalStateType } from '@/models'
+  import { StateUpdateDetails, terminalStateType } from '@/models'
   import { flowRunRouteKey } from '@/router'
   import { deleteItem, inject } from '@/utilities'
 
   const props = defineProps<{
-    taskRun: TaskRun,
+    taskRunId: string,
+    flowRunId: string,
   }>()
 
   const can = useCan()
   const api = useWorkspaceApi()
   const flowRunRoute = inject(flowRunRouteKey)
+  const taskRunSubscription =  useSubscription(api.taskRuns.getTaskRun, [props.taskRunId])
+  const taskRun = computed(() => taskRunSubscription.response)
 
-  const flowRunSubscription = useSubscription(api.flowRuns.getFlowRun, [props.taskRun.flowRunId])
+  const flowRunSubscription = useSubscription(api.flowRuns.getFlowRun, [props.flowRunId])
   const flowRunName = computed(() => flowRunSubscription.response?.name)
 
-  const taskRunSubscription =  useSubscription(api.taskRuns.getTaskRun, [props.taskRun.id])
-  const taskRunDetails = computed(() => taskRunSubscription.response)
 
   const crumbs = computed(() => [
-    { text: flowRunName.value ?? '', to: flowRunRoute(props.taskRun.flowRunId) },
-    { text: props.taskRun.name ?? '' },
+    { text: flowRunName.value ?? '', to: flowRunRoute(props.flowRunId) },
+    { text: taskRun.value?.name ?? '' },
   ])
 
   const showChangeStateMenuItemButton = computed(() => {
-    if (can.update.task_run && props.taskRun.stateType && terminalStateType.includes(props.taskRun.stateType)) {
+    if (can.update.task_run && taskRun.value?.stateType && terminalStateType.includes(taskRun.value.stateType)) {
       return true
     }
 
@@ -86,7 +87,7 @@
 
   const changeTaskRunState = async (values: StateUpdateDetails): Promise<void> => {
     try {
-      await api.taskRuns.setTaskRunState(props.taskRun.id, { state: values })
+      await api.taskRuns.setTaskRunState(props.taskRunId, { state: values })
       taskRunSubscription.refresh()
       showToast(localization.success.changeTaskRunState, 'success')
     } catch (error) {
