b21eb8925ab75c05b4bce1b58af1760682f7f4aa
Marichka Offen <marichka@prefect.io>
2022-11-18 09:49:51 -0500

    Update useFilter.ts

--- src/compositions/useFilter.ts
+++ src/compositions/useFilter.ts
@@ -102,9 +102,15 @@ export function useFilter(filters: MaybeRef<UseFilterArgs>): ComputedRef<UnionFi
 
     if (startDate.value) {
       response.flow_runs ??= {}
-      response.flow_runs.expected_start_time ??= {}
+      if (response.flow_runs.start_time) {
+        response.flow_runs.start_time ??= {}
 
-      response.flow_runs.expected_start_time.after_ = mapper.map('Date', startDate.value, 'string')
+        response.flow_runs.start_time.after_ = mapper.map('Date', startDate.value, 'string')
+      } else {
+        response.flow_runs.expected_start_time ??= {}
+
+        response.flow_runs.expected_start_time.after_ = mapper.map('Date', startDate.value, 'string')
+      }
     }
 
     if (endDate.value) {
