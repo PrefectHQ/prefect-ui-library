73ce80a650d44ab0e6b96d3e5a9fcca27a2f852e
Marichka Offen <marichka@prefect.io>
2022-11-21 14:19:38 -0500

    Update StateType.ts

--- src/models/StateType.ts
+++ src/models/StateType.ts
@@ -34,3 +34,7 @@ export type StuckStateType = typeof stuckStateTypes[number]
 export function isStuckStateType(value: string): value is StuckStateType {
   return stuckStateTypes.includes(value as StuckStateType)
 }
+
+export function isPausedStateName(value: string): boolean {
+  return value === 'Paused'
+}
