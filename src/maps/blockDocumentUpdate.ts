fa26a2c401ccc13063417984edf7ecd273712671
Jennifer Grange <jenny@prefect.io>
2022-11-23 21:44:22 -0500

    hybrid

--- src/maps/blockDocumentUpdate.ts
+++ src/maps/blockDocumentUpdate.ts
@@ -3,10 +3,10 @@ import { BlockDocumentUpdate } from '@/models/BlockDocumentUpdate'
 import { MapFunction } from '@/services/Mapper'
 
 export const mapBlockDocumentUpdateToBlockDocumentUpdateRequest: MapFunction<BlockDocumentUpdate, BlockDocumentUpdateRequest> = function(source: BlockDocumentUpdate): BlockDocumentUpdateRequest {
-  const { blockSchema, data: values } = source
+  const { blockSchema, data: values, mergeExistingData } = source
   const schema = blockSchema.fields
   const data = this.map('SchemaValues', { values, schema }, 'SchemaValuesRequest')
 
-  return data
+  return { data, 'merge_existing_data': mergeExistingData }
 
 }
--- src/services/WorkspaceBlockDocumentsApi.ts
+++ src/services/WorkspaceBlockDocumentsApi.ts
@@ -37,7 +37,8 @@ export class WorkspaceBlockDocumentsApi extends WorkspaceApi implements IWorkspa
   }
 
   public updateBlockDocument(blockDocumentId: string, blockDocument: BlockDocumentUpdate): Promise<void> {
-    return this.patch(`/${blockDocumentId}`, { data: mapper.map('BlockDocumentUpdate', blockDocument, 'BlockDocumentUpdateRequest'), 'merge_existing_data': false })
+    const request = mapper.map('BlockDocumentUpdate', { mergeExistingData: false, ...blockDocument }, 'BlockDocumentUpdateRequest')
+    return this.patch(`/${blockDocumentId}`, request)
   }
 
   public deleteBlockDocument(blockDocumentId: string): Promise<void> {
