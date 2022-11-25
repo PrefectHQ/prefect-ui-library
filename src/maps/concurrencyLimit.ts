abb74a4d83be6f6a011a0c80dfd27b0496887941
Jennifer Grange <jenny@prefect.io>
2022-11-24 20:52:49 -0500

    add to index

--- src/maps/concurrencyLimit.ts
+++ src/maps/concurrencyLimit.ts
@@ -2,7 +2,7 @@ import { ConcurrencyLimitResponse } from '@/models/api/ConcurrencyLimitResponse'
 import { ConcurrencyLimit } from '@/models/ConcurrencyLimit'
 import { MapFunction } from '@/services/Mapper'
 
-export const ConcurrencyLimitResponseToConcurrencyLimit: MapFunction<ConcurrencyLimitResponse, ConcurrencyLimit> = function(source: ConcurrencyLimitResponse): ConcurrencyLimit {
+export const mapConcurrencyLimitResponseToConcurrencyLimit: MapFunction<ConcurrencyLimitResponse, ConcurrencyLimit> = function(source: ConcurrencyLimitResponse): ConcurrencyLimit {
   const { tag, id, created, updated } = source
 
   return {
