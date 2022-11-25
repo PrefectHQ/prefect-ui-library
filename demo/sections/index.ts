ae54a9cda5676f6b90f75afa87d4ee1e38081f47
Jennifer Grange <jenny@prefect.io>
2022-11-24 21:23:41 -0500

    remove from index so hidden until components are built

--- demo/sections/index.ts
+++ demo/sections/index.ts
@@ -1,6 +1,5 @@
 import { Section } from '@/demo/router/routeRecords'
 import { blocks } from '@/demo/sections/blocks'
-// import { concurrencyLimits as concurrency } from '@/demo/sections/concurrencyLimits'
 import { deployments } from '@/demo/sections/deployments'
 import { flowRuns } from '@/demo/sections/flowRuns'
 import { general } from '@/demo/sections/general'
@@ -10,7 +9,6 @@ import { states } from '@/demo/sections/states'
 export const sections: Section = {
   general,
   blocks,
-  // concurrency,
   deployments,
   flowRuns,
   notifications,
