13f0c8ed1cdc0a7a04c3b6263fb6ef5411bed37c
Marichka Offen <marichka@prefect.io>
2022-11-28 17:15:51 -0500

    Add utility to combine Override params and Default params

---
+++ src/utilities/parameters.ts
@@ -0,0 +1,19 @@
+import { computed, ref, Ref } from 'vue'
+import { SchemaProperties, SchemaValues } from '@/types/schemas'
+
+export function mergeParametersObjects(flowRunParameters: SchemaValues | Ref<SchemaValues>, deploymentParameters: SchemaProperties | Ref<SchemaProperties>): string {
+  const flowRunParamRef = ref(flowRunParameters)
+  const deploymentParamRef = ref(deploymentParameters)
+
+  const parametersVault = ref(flowRunParamRef.value)
+
+  for (const [key, value] of Object.entries(deploymentParamRef.value)) {
+    if (!Object.keys(parametersVault.value).includes(key)) {
+      parametersVault.value[key] = value?.default
+    }
+  }
+
+  const parameters = computed(() => Object.keys(parametersVault.value).length !== 0 ? JSON.stringify(parametersVault.value, undefined, 2) : '{}')
+
+  return parameters.value
+}
