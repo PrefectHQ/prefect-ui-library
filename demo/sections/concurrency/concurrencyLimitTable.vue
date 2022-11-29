1fa432e5dd3f15938e1613943479b711b5a2c042
Jennifer Grange <jenny@prefect.io>
2022-11-28 19:34:30 -0500

    add mocker for table

--- demo/sections/concurrency/concurrencyLimitTable.vue
+++ demo/sections/concurrency/concurrencyLimitTable.vue
@@ -7,4 +7,12 @@
 <script lang="ts" setup>
   import ConcurrencyLimitsTable from '@/components/ConcurrencyLimitsTable.vue'
   import ComponentPage from '@/demo/components/ComponentPage.vue'
+  import { useSeeds } from '@/demo/compositions/useSeeds'
+  import { mocker } from '@/services'
+
+  const concurrencyLimit = mocker.createMany('concurrencyLimit', 5)
+  console.log(concurrencyLimit)
+  useSeeds({
+    concurrencyLimit,
+  })
 </script>
