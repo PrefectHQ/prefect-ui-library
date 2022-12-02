da4b47cefe8872889a6518c84efc8df8889365e1
Jennifer Grange <jenny@prefect.io>
2022-12-02 09:53:53 -0500

    add an empty state for concurrency limits

--- demo/sections/concurrency/index.ts
+++ demo/sections/concurrency/index.ts
@@ -3,4 +3,5 @@ import { Section } from '@/demo/router/routeRecords'
 export const concurrency: Section = {
   AddConcurrencyLimitModal: () => import ('./ConcurrencyLimitModal.vue'),
   ConcurrencyLimitsTable: () => import ('./ConcurrencyLimitsTable.vue'),
+  ConcurrencyLimitsEmptyState: () => import ('./ConcurrencyLimitsPageEmptyState.vue'),
 }
