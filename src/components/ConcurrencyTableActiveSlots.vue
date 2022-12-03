60494030f86d111060e66ac5a29f6239ce6327ad
Jennifer Grange <jenny@prefect.io>
2022-12-03 08:17:11 -0500

    move link to tag name

--- src/components/ConcurrencyTableActiveSlots.vue
+++ src/components/ConcurrencyTableActiveSlots.vue
@@ -1,19 +1,14 @@
 <template>
   <template v-if="!taskRunCount">
-    <p-link :to="routes.concurrencyLimit(concurrencyLimit.id)">
-      None
-    </p-link>
+    None
   </template>
   <template v-else>
-    <p-link :to="routes.concurrencyLimit(concurrencyLimit.id)">
-      {{ taskRunCount }}
-    </p-link>
+    {{ taskRunCount }}
   </template>
 </template>
 
   <script lang="ts" setup>
   import { computed } from 'vue'
-  import { useWorkspaceRoutes } from '@/compositions'
   import { ConcurrencyLimit } from '@/models'
 
   const props = defineProps<{
@@ -21,5 +16,4 @@
   }>()
 
   const taskRunCount = computed(()=> props.concurrencyLimit.activeSlots?.length)
-  const routes = useWorkspaceRoutes()
   </script>
