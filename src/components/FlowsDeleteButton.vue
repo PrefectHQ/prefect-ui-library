0103b4aa35396249201257ccc7ced52da87168b5
Marichka Offen <40467112+marichka-offen@users.noreply.github.com>
2022-12-05 12:46:37 -0500

    Feature: Bulk deletion of flows in the UI (#855)
    * Create delete flows button
    
    * Create selectedCount component and update ResultsCount
    
    by abstracting isNumber utility into it's own file
    
    * Add multi-delete functionality to flows table
    
    * Update FlowsTable.vue
    
    * Update FlowsTable.vue
    
    * Update FlowsTable.vue
    
    * Update FlowsTable.vue
    
    * Update DeleteFlowsButton.vue
    
    * Rename DeleteFlowsButton to FlowsDeleteButton to match pattern of other components
    
    * Update class names in FlowsDeleteButton
    
    * Add finally
    
    * Update FlowsDeleteButton.vue
    
    * Use get/set and  and @update:model-value

---
+++ src/components/FlowsDeleteButton.vue
@@ -0,0 +1,63 @@
+<template>
+  <Transition name="flows-delete-button-transition">
+    <p-button v-if="selected.length > 0" danger icon="TrashIcon" @click="open" />
+  </Transition>
+  <ConfirmDeleteModal
+    v-model:showModal="showModal"
+    name="selected flows"
+    label="Flows"
+    @delete="deleteFlows(selected)"
+  />
+</template>
+
+<script lang="ts" setup>
+  import { showToast } from '@prefecthq/prefect-design'
+  import { computed } from 'vue'
+  import ConfirmDeleteModal from '@/components/ConfirmDeleteModal.vue'
+  import { useShowModal, useWorkspaceApi } from '@/compositions'
+  import { localization } from '@/localization'
+
+  defineProps<{
+    selected: string[],
+  }>()
+
+  const emit = defineEmits<{
+    (event: 'delete'): void,
+  }>()
+
+  const { showModal, open, close } = useShowModal()
+
+  const api = useWorkspaceApi()
+
+  const deleteFlows = async (flows: string[]): Promise<void> => {
+    const toastMessage = computed(() => {
+      if (flows.length === 1) {
+        return localization.success.delete('Flow')
+      }
+      return localization.success.delete(`${flows.length} flows`)
+    })
+
+    try {
+      const deleteFlows = flows.map(api.flows.deleteFlow)
+      await Promise.all(deleteFlows)
+      showToast(toastMessage, 'success')
+      emit('delete')
+    } catch (error) {
+      showToast(localization.error.delete('flows'), 'error')
+    } finally {
+      close()
+    }
+  }
+</script>
+
+<style>
+.flows-delete-button-transition-enter-active,
+.flows-delete-button-transition-leave-active {
+  transition: opacity 0.25s ease;
+}
+
+.flows-delete-button-transition-enter-from,
+.flows-delete-button-transition-leave-to {
+  opacity: 0;
+}
+</style>
