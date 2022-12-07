23e48544c547e19881706c7cb63e8d4e51ef940f
Nicola Reyes <39339887+nicolascribbles@users.noreply.github.com>
2022-12-07 15:36:26 -0700

    Update PageHeadingFlowRuns.vue

--- src/components/PageHeadingFlowRuns.vue
+++ src/components/PageHeadingFlowRuns.vue
@@ -1,14 +1,24 @@
 <template>
   <page-heading class="page-heading-flow-runs" :crumbs="crumbs">
-    <template #actions>
+    <template v-if="!hideActions" #actions>
       <SavedFilters />
     </template>
   </page-heading>
 </template>
 
 <script lang="ts" setup>
+  import { defineProps, withDefaults, computed } from 'vue'
   import PageHeading from '@/components/PageHeading.vue'
   import  SavedFilters from '@/components/SavedFilters.vue'
+
+
+  const props = withDefaults(defineProps<{
+    hideActions?: boolean,
+  }>(), {
+    hideActions: false,
+  })
+
+  const hideActions = computed(() => props.hideActions)
 
   const crumbs = [{ text: 'Flow Runs' }]
 </script>
