cfa42b1f0a66cf408a9c9b83fc6a3782f56b23c8
Craig Harshbarger <pleek91@gmail.com>
2022-12-15 14:54:56 -0600

    lint fix

--- src/components/FlowRunFilteredList.vue
+++ src/components/FlowRunFilteredList.vue
@@ -75,7 +75,7 @@
       }
     }
 
-    return { ...runFilter, flow_runs: { ...flowRunsFilter } }
+    return { ...runFilter, 'flow_runs': { ...flowRunsFilter } }
   })
 
   const flowRunCountSubscription = useSubscription(api.flowRuns.getFlowRunsCount, [filter], { interval: 30000 })
