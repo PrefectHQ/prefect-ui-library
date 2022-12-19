32828c403721361f6ecffd02161bc85dcf35e12b
Marichka Offen <marichka@prefect.io>
2022-12-19 11:38:35 -0500

    Move api models to api folder

--- src/models/WorkerFlowRunResponse.ts
+++
@@ -1,7 +0,0 @@
-import { FlowRunResponse } from '@/models'
-
-export type WorkerFlowRunResponse = {
-  worker_pool_id: string,
-  worker_pool_queue_id: string,
-  flow_run: FlowRunResponse,
-}
--- src/models/WorkerFlowRunsRequest.ts
+++
@@ -1,6 +0,0 @@
-export type WorkerFlowRunsRequest = Partial<{
-  worker_pool_queue_names: string[],
-  scheduled_before: string,
-  scheduled_after: string,
-  limit: number,
-}>
