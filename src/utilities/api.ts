bf960a9c13168665694f3d761c2287b66b7e85d6
Marichka Offen <marichka@prefect.io>
2022-12-19 15:42:42 -0500

    Update api.ts

--- src/utilities/api.ts
+++ src/utilities/api.ts
@@ -13,6 +13,8 @@ import { WorkspaceLogsApi } from '@/services/WorkspaceLogsApi'
 import { WorkspaceNotificationsApi } from '@/services/WorkspaceNotificationsApi'
 import { WorkspaceSavedSearchesApi } from '@/services/WorkspaceSavedSearchesApi'
 import { WorkspaceTaskRunsApi } from '@/services/WorkspaceTaskRunsApi'
+import { WorkspaceWorkerPoolApi } from '@/services/WorkspaceWorkerPoolApi'
+import { WorkspaceWorkerPoolQueueApi } from '@/services/WorkspaceWorkerPoolQueueApi'
 import { WorkspaceWorkQueuesApi } from '@/services/WorkspaceWorkQueuesApi'
 
 // We want the return type to be inferred
@@ -32,6 +34,8 @@ export function createApi(workspaceConfig: WorkspaceApiConfig) {
     taskRuns: createActions(new WorkspaceTaskRunsApi(workspaceConfig)),
     workQueues: createActions(new WorkspaceWorkQueuesApi(workspaceConfig)),
     savedSearches: createActions(new WorkspaceSavedSearchesApi(workspaceConfig)),
+    workerPools: createActions(new WorkspaceWorkerPoolApi(workspaceConfig)),
+    workerPoolQueue: createActions(new WorkspaceWorkerPoolQueueApi(workspaceConfig)),
   }
 }
 
