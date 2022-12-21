9d31f5ba84fa14d7daa98742e27d06b26b458a98
Evan Sutherland <stackoverfloweth@gmail.com>
2022-12-21 10:54:24 -0600

    removed old PermissionString from RouteMeta

--- src/vue-router.d.ts
+++
@@ -1,8 +0,0 @@
-import 'vue-router'
-import { PermissionString } from '@/types/permissions'
-
-declare module 'vue-router' {
-  interface RouteMeta {
-    can?: PermissionString | PermissionString[],
-  }
-}
