974bcaa7c86cc519ed126fd56d3055831c24ed0f
nicholas <znicholasbrown@gmail.com>
2022-12-21 16:19:45 -0500

    Update actions

--- src/components/PageHeadingWorkerPool.vue
+++ src/components/PageHeadingWorkerPool.vue
@@ -1,16 +1,17 @@
 <template>
   <page-heading class="page-heading-worker-pool" :crumbs="crumbs">
-    <template #after-crumbs>
-      <router-link v-if="can.update.worker_pool" :to="routes.workerPoolEdit(workerPool.name)">
-        <p-button inset size="xs" icon="PlusIcon" />
-      </router-link>
+    <template #actions>
+      <WorkerPoolToggle :worker-pool="workerPool" @update="emit('update')" />
+      <WorkerPoolMenu :worker-pool="workerPool" :show-all="!media.sm" @delete="handleDelete" />
     </template>
   </page-heading>
 </template>
 
 <script lang="ts" setup>
-  import PageHeading from '@/components/PageHeading.vue'
-  import { useWorkspaceRoutes, useCan } from '@/compositions'
+  import { media } from '@prefecthq/prefect-design'
+  import { useRouter } from 'vue-router'
+  import { PageHeading, WorkerPoolToggle, WorkerPoolMenu } from '@/components'
+  import { useWorkspaceRoutes } from '@/compositions'
   import { WorkerPool } from '@/models'
 
   const props = defineProps<{
@@ -18,7 +19,15 @@
   }>()
 
   const routes = useWorkspaceRoutes()
+  const router = useRouter()
 
-  const can = useCan()
+  const emit = defineEmits<{
+    (event: 'update' | 'delete'): void,
+  }>()
+
   const crumbs = [{ text: 'Worker Pools', to: routes.workerPools() }, { text: props.workerPool.name }]
+
+  const handleDelete = (): void => {
+    router.back()
+  }
 </script>
