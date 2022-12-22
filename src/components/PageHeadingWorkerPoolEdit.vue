f84a82fdc01a0a900e7f8cc2327af79d9b71fd16
Marichka Offen <marichka@prefect.io>
2022-12-22 12:56:19 -0500

    Create edit form heading

---
+++ src/components/PageHeadingWorkerPoolEdit.vue
@@ -0,0 +1,22 @@
+<template>
+  <PageHeading :crumbs="crumbs" />
+</template>
+
+<script lang="ts" setup>
+  import { computed } from 'vue'
+  import PageHeading from '@/components/PageHeading.vue'
+  import { useWorkspaceRoutes } from '@/compositions'
+  import { WorkerPool } from '@/models'
+
+  const props = defineProps<{
+    workerPool: WorkerPool,
+  }>()
+
+  const routes = useWorkspaceRoutes()
+
+  const crumbs = computed(() => [
+    { text: 'Worker Pools', to: routes.workerPools() },
+    { text: props.workerPool.name, to: routes.workerPool(props.workerPool.name) },
+    { text: 'Edit' },
+  ])
+</script>
