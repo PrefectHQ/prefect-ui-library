5e0c23615f242c6d46642ded4d355378142d8c4c
Nicholas Brown <znicholasbrown@gmail.com>
2022-12-22 15:39:53 -0500

    Chore: Add missing worker pool headers to demo app (#960)
    * Add create and edit page headings to the demo app
    
    * Remove heading wrapper (uneeded)

--- demo/sections/workers/WorkerHeadings.vue
+++ demo/sections/workers/WorkerHeadings.vue
@@ -8,11 +8,17 @@
         <PageHeadingWorkerPool :worker-pool="workerPool" />
       </div>
     </template>
+    <template #worker-pool-create>
+      <PageHeadingWorkerPoolCreate :worker-pool="workerPool" />
+    </template>
+    <template #worker-pool-edit>
+      <PageHeadingWorkerPoolEdit :worker-pool="workerPool" />
+    </template>
   </ComponentPage>
 </template>
 
 <script lang="ts" setup>
-  import { PageHeadingWorkerPool, PageHeadingWorkerPools } from '@/components'
+  import { PageHeadingWorkerPool, PageHeadingWorkerPoolCreate, PageHeadingWorkerPoolEdit, PageHeadingWorkerPools } from '@/components'
   import ComponentPage from '@/demo/components/ComponentPage.vue'
   import { useWorkerPoolMock } from '@/demo/compositions/useWorkerPoolMock'
   import { DemoSection } from '@/demo/types/demoSection'
@@ -20,6 +26,8 @@
   const demos: DemoSection[] = [
     { title: 'Worker Pools' },
     { title: 'Worker Pool' },
+    { title: 'Worker Pool Create' },
+    { title: 'Worker Pool Edit' },
   ]
 
   const workerPool = useWorkerPoolMock()
