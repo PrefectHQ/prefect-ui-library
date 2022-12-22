c3c1835ddc112bc7870397a64a87378659ca71f9
nicholas <znicholasbrown@gmail.com>
2022-12-22 15:30:42 -0500

    Add create and edit page headings to the demo app

--- demo/sections/workers/WorkerHeadings.vue
+++ demo/sections/workers/WorkerHeadings.vue
@@ -8,11 +8,21 @@
         <PageHeadingWorkerPool :worker-pool="workerPool" />
       </div>
     </template>
+    <template #worker-pool-create>
+      <div class="worker-headings__header-container">
+        <PageHeadingWorkerPoolCreate :worker-pool="workerPool" />
+      </div>
+    </template>
+    <template #worker-pool-edit>
+      <div class="worker-headings__header-container">
+        <PageHeadingWorkerPoolEdit :worker-pool="workerPool" />
+      </div>
+    </template>
   </ComponentPage>
 </template>
 
 <script lang="ts" setup>
-  import { PageHeadingWorkerPool, PageHeadingWorkerPools } from '@/components'
+  import { PageHeadingWorkerPool, PageHeadingWorkerPoolCreate, PageHeadingWorkerPoolEdit, PageHeadingWorkerPools } from '@/components'
   import ComponentPage from '@/demo/components/ComponentPage.vue'
   import { useWorkerPoolMock } from '@/demo/compositions/useWorkerPoolMock'
   import { DemoSection } from '@/demo/types/demoSection'
@@ -20,6 +30,8 @@
   const demos: DemoSection[] = [
     { title: 'Worker Pools' },
     { title: 'Worker Pool' },
+    { title: 'Worker Pool Create' },
+    { title: 'Worker Pool Edit' },
   ]
 
   const workerPool = useWorkerPoolMock()
