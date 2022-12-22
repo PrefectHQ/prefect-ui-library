6e5204e2e1b0e0f517c39ea8343b5bf3bcaa8937
nicholas <znicholasbrown@gmail.com>
2022-12-22 18:47:30 -0500

    Fix broken export, add demo component

---
+++ demo/sections/general/OTable.vue
@@ -0,0 +1,11 @@
+<template>
+  <ComponentPage title="OTable">
+    <OTable :data="[]" :columns="[]" />
+  </ComponentPage>
+</template>
+
+<script lang="ts" setup>
+  import { OTable } from '@/components'
+  import ComponentPage from '@/demo/components/ComponentPage.vue'
+  import { mocker } from '@/services'
+</script>
