2ae7bb97d63873f03ed27b3b52ee9660d3b69871
nicholas <znicholasbrown@gmail.com>
2022-12-22 15:42:27 -0500

    Scaffold demo page

---
+++ demo/sections/workers/WorkerTables.vue
@@ -0,0 +1,13 @@
+<template>
+  <ComponentPage title="Worker Tables" :demos="demos">
+    <template #worker-queues-table />
+  </ComponentPage>
+</template>
+
+<script lang="ts" setup>
+  import { } from '@/components'
+  import ComponentPage from '@/demo/components/ComponentPage.vue'
+  import { DemoSection } from '@/demo/types/demoSection'
+
+  const demos: DemoSection[] = [{ title: 'Worker Queues Table' }]
+</script>
--- demo/sections/workers/index.ts
+++ demo/sections/workers/index.ts
@@ -3,4 +3,5 @@ import { Section } from '@/demo/router/routeRecords'
 export const workers: Section = {
   WorkerHeadings: () => import('./WorkerHeadings.vue'),
   WorkerPoolCard: () => import('./WorkerPoolCard.vue'),
+  WorkerTables: () => import('./WorkerTables.vue'),
 }
