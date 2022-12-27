dde06d99a963119e6845d810c7633661c1d9609b
Evan Sutherland <stackoverfloweth@gmail.com>
2022-12-27 09:57:28 -0600

    added demo page for filter group component

--- demo/sections/flowRuns/index.ts
+++ demo/sections/flowRuns/index.ts
@@ -1,7 +1,8 @@
 import { Section } from '@/demo/router/routeRecords'
 
 export const flowRuns: Section = {
-  FlowRunDetails: () => import ('./FlowRunDetails.vue'),
-  FlowRunList: () => import ('./FlowRunList.vue'),
-  FlowRunListItem: () => import ('./FlowRunListItem.vue'),
+  FlowRunDetails: () => import('./FlowRunDetails.vue'),
+  FlowRunList: () => import('./FlowRunList.vue'),
+  FlowRunListItem: () => import('./FlowRunListItem.vue'),
+  FlowRunsFilterGroup: () => import('./FlowRunsFilterGroup.vue'),
 }
