ed0686eafc9225c30b4f9fd29676455b825b0744
Marichka Offen <marichka@prefect.io>
2022-12-28 15:26:24 -0500

    Remove toggle flashing

--- src/components/WorkerPoolToggle.vue
+++ src/components/WorkerPoolToggle.vue
@@ -1,10 +1,10 @@
 <template>
-  <p-toggle v-if="can.update.worker_pool" v-model="internalValue" :state="state" />
+  <p-toggle v-if="can.update.worker_pool" v-model="internalValue" />
 </template>
 
 <script lang="ts" setup>
-  import { PToggle, showToast, State } from '@prefecthq/prefect-design'
-  import { computed, reactive } from 'vue'
+  import { PToggle, showToast } from '@prefecthq/prefect-design'
+  import { computed } from 'vue'
   import { useCan, useWorkspaceApi } from '@/compositions'
   import { localization } from '@/localization'
   import { WorkerPool } from '@/models'
@@ -29,11 +29,7 @@
     },
   })
 
-  const state = reactive({ pending: false } as State)
-
   const toggle = async (value: boolean): Promise<void> => {
-    state.pending = true
-
     try {
       if (value) {
         await api.workerPools.resumeWorkerPool(props.workerPool.name)
@@ -51,8 +47,6 @@
       showToast(message)
 
       console.error(error)
-    } finally {
-      state.pending = false
     }
   }
 </script>
