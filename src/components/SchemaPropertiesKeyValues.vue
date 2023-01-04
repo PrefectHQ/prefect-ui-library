bafbcbae178ca9e2ed3a698d1a16341bd53182c8
nicholas <znicholasbrown@gmail.com>
2023-01-04 13:35:24 -0500

    Add positional sorting to the schema properties key values component

--- src/components/SchemaPropertiesKeyValues.vue
+++ src/components/SchemaPropertiesKeyValues.vue
@@ -1,6 +1,6 @@
 <template>
   <PContent class="schema-properties-key-values">
-    <template v-for="(property, key) in schema.properties" :key="key">
+    <template v-for="[key, property] in sortedSchemaProperties" :key="key">
       <SchemaProperty v-if="property" :property="property" :value="getPropertyValue(key)" />
     </template>
   </PContent>
@@ -8,6 +8,7 @@
 
 <script lang="ts" setup>
   import { PContent } from '@prefecthq/prefect-design'
+  import { computed } from 'vue'
   import SchemaProperty from '@/components/SchemaPropertyKeyValue.vue'
   import { Schema, SchemaValues } from '@/types/schemas'
 
@@ -19,4 +20,9 @@
   function getPropertyValue(blockSchemaPropertyKey: string): unknown {
     return props.values[blockSchemaPropertyKey]
   }
+
+  const sortedSchemaProperties = computed(() => {
+    const properties = Object.entries(props.schema.properties ?? {})
+    return properties.sort(([, propA], [, propB]) => (propA?.position ?? 0) - (propB?.position ?? 0))
+  })
 </script>
