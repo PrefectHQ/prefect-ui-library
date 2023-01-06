3c3f27cefa703c399e0dbd9f1b1f577f60ae26c5
Marichka Offen <marichka@prefect.io>
2023-01-06 16:11:55 -0500

    Make LateIndicator useable by workPool and poolQueue

--- src/components/WorkersLateIndicator.vue
+++ src/components/WorkersLateIndicator.vue
@@ -1,8 +1,16 @@
 <template>
-  <p-tag v-if="lateRunsCount" class="worker-pool-late-indicator">
+  <p-tag v-if="lateRunsCount" class="workers-late-indicator">
     {{ lateRunsCount }} {{ toPluralString('Late run', lateRunsCount) }}
   </p-tag>
 </template>
+
+<script lang="ts">
+  export default {
+    name: 'WorkersLateIndicator',
+    expose: [],
+    inheritAttrs: false,
+  }
+</script>
 
 <script lang="ts" setup>
   import { useSubscription } from '@prefecthq/vue-compositions'
@@ -12,19 +20,22 @@
 
   const props = defineProps<{
     workerPoolName: string,
+    workerPoolQueueNames?: string[],
   }>()
+
+  const workerPoolQueueNames = computed(() => props.workerPoolQueueNames ?? [])
 
   const api = useWorkspaceApi()
   const { workerPoolName } = toRefs(props)
 
-  const workerPoolScheduledRunsSubscription = useSubscription(api.workerPools.getWorkerPoolScheduledRuns, [workerPoolName.value, {}], { interval: 30000 })
+  const workerPoolScheduledRunsSubscription = useSubscription(api.workerPools.getWorkerPoolScheduledRuns, [workerPoolName.value, { workerPoolQueueNames: workerPoolQueueNames.value }], { interval: 30000 })
   const workerPoolScheduledRuns = computed(() => workerPoolScheduledRunsSubscription.response ?? [])
 
   const lateRunsCount = computed(() => workerPoolScheduledRuns.value.filter(run => run.flowRun.stateName === 'Late').length)
 </script>
 
 <style>
-.worker-pool-late-indicator { @apply
+.workers-late-indicator { @apply
   bg-state-scheduled-100
   text-state-scheduled-700
   }
