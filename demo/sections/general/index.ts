0ed26bbb3d20e7d50446d2616dae18fc5b92fdb7
Craig Harshbarger <pleek91@gmail.com>
2023-01-24 11:28:06 -0600

    Remove old unused demo page

--- demo/sections/general/index.ts
+++ demo/sections/general/index.ts
@@ -1,12 +1,11 @@
 import { Section } from '@/demo/router/routeRecords'
 
 export const general: Section = {
-  Query: () => import ('./Query.vue'),
-  CodeBanner: () => import ('./CodeBanner.vue'),
-  CodeSnippet: () => import ('./CodeSnippet.vue'),
-  CodeView: () => import ('./CodeView.vue'),
-  ColorModeSelect: () => import ('./ColorModeSelect.vue'),
-  ColorModeSelectOption: () => import ('./ColorModeSelectOption.vue'),
-  ConfirmDeleteModal: () => import ('./ConfirmDeleteModal.vue'),
-  CopyOverflowMenuItem: () => import ('./CopyOverflowMenuItem.vue'),
+  CodeBanner: () => import('./CodeBanner.vue'),
+  CodeSnippet: () => import('./CodeSnippet.vue'),
+  CodeView: () => import('./CodeView.vue'),
+  ColorModeSelect: () => import('./ColorModeSelect.vue'),
+  ColorModeSelectOption: () => import('./ColorModeSelectOption.vue'),
+  ConfirmDeleteModal: () => import('./ConfirmDeleteModal.vue'),
+  CopyOverflowMenuItem: () => import('./CopyOverflowMenuItem.vue'),
 }
