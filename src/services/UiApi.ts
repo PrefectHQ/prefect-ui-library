4ac6c36b4513a7ef72fe382d6e59b04055d20751
Craig Harshbarger <pleek91@gmail.com>
2023-01-26 14:57:53 -0600

    Fix the ui filter

--- src/services/UiApi.ts
+++ src/services/UiApi.ts
@@ -1,6 +1,6 @@
 import { InjectionKey } from 'vue'
 import { UiFlowRunHistoryResponse } from '@/models/api/UiFlowRunHistoryResponse'
-import { FlowRunsHistoryFilter } from '@/models/Filters'
+import { FlowRunsFilter } from '@/models/Filters'
 import { UiFlowRunHistory } from '@/models/UiFlowRunHistory'
 import { Api, ApiRoute } from '@/services/Api'
 import { mapper } from '@/services/Mapper'
@@ -9,8 +9,8 @@ export class UiApi extends Api {
 
   protected route: ApiRoute = '/ui'
 
-  public async getFlowRunHistory(filter: FlowRunsHistoryFilter): Promise<UiFlowRunHistory[]> {
-    const request = mapper.map('FlowRunsHistoryFilter', filter, 'FlowRunsHistoryFilterRequest')
+  public async getFlowRunHistory(filter: FlowRunsFilter): Promise<UiFlowRunHistory[]> {
+    const request = mapper.map('FlowRunsFilter', filter, 'FlowRunsFilterRequest')
     const { data } = await this.post<UiFlowRunHistoryResponse[]>('/flow_runs/history', request)
 
     return mapper.map('UiFlowRunHistoryResponse', data, 'UiFlowRunHistory')
