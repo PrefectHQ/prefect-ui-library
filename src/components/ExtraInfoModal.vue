5328d85b44288e0ab527112d555a9c68f99d007d
Jennifer Grange <jenny@prefect.io>
2023-02-01 12:01:32 -0500

    extract code from RBAC info modal to make a extra info general extra info modal

---
+++ src/components/ExtraInfoModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <button v-bind="attrs" type="button" @click.stop.prevent="show = true">
+    <p-icon icon="QuestionMarkCircleIcon" solid />
+  </button>
+  <p-modal v-model:show-modal="show" :title="title">
+    <slot />
+    <template #cancel>
+      <p-button inset @click="show = false">
+        Close
+      </p-button>
+    </template>
+  </p-modal>
+</template>
+
+<script lang="ts">
+  export default {
+    inheritAttrs: false,
+  }
+</script>
+
+<script lang="ts" setup>
+  import { ref, useAttrs } from 'vue'
+
+  const attrs = useAttrs()
+
+  const show = ref(false)
+
+  defineProps({
+    title: {
+      type: String,
+      required: true,
+    },
+  })
+</script>
