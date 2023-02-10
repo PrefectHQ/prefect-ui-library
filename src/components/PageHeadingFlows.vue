45d04ac7ba45b5bd122f18a30370f5e1bcb89101
Evan Sutherland <stackoverfloweth@gmail.com>
2023-02-10 15:30:47 -0600

    added cta to flows page heading

--- src/components/PageHeadingFlows.vue
+++ src/components/PageHeadingFlows.vue
@@ -1,9 +1,19 @@
 <template>
-  <page-heading class="page-heading-flows" :crumbs="crumbs" />
+  <PageHeading class="page-heading-flows" :crumbs="crumbs">
+    <template v-if="can.access.collections" #after-crumbs>
+      <p-button :to="routes.flowCollections()" secondary>
+        Add from Collection
+      </p-button>
+    </template>
+  </PageHeading>
 </template>
 
 <script lang="ts" setup>
   import PageHeading from '@/components/PageHeading.vue'
+  import { useCan, useWorkspaceRoutes } from '@/compositions'
+
+  const can = useCan()
+  const routes = useWorkspaceRoutes()
 
   const crumbs = [{ text: 'Flows' }]
 </script>
