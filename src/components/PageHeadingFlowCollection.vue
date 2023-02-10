4b1fed2bab011837db2bc4dd5332a235e21ecfb8
Evan Sutherland <stackoverfloweth@gmail.com>
2023-02-10 15:30:36 -0600

    new page heading components

---
+++ src/components/PageHeadingFlowCollection.vue
@@ -0,0 +1,21 @@
+<template>
+  <page-heading class="page-heading-flow-collection" :crumbs="crumbs" />
+</template>
+
+<script lang="ts" setup>
+  import { computed } from 'vue'
+  import PageHeading from '@/components/PageHeading.vue'
+  import { useWorkspaceRoutes } from '@/compositions'
+
+  const props = defineProps<{
+    name: string,
+  }>()
+
+  const routes = useWorkspaceRoutes()
+
+  const crumbs = computed(() => [
+    { text: 'Flows', to: routes.flows() },
+    { text: 'Collections', to: routes.flowCollections() },
+    { text: props.name },
+  ])
+</script>
---
+++ src/components/PageHeadingFlowCollections.vue
@@ -0,0 +1,15 @@
+<template>
+  <page-heading class="page-heading-flow-collections" :crumbs="crumbs" />
+</template>
+
+<script lang="ts" setup>
+  import PageHeading from '@/components/PageHeading.vue'
+  import { useWorkspaceRoutes } from '@/compositions'
+
+  const routes = useWorkspaceRoutes()
+
+  const crumbs = [
+    { text: 'Flows', to: routes.flows() },
+    { text: 'Collections' },
+  ]
+</script>
