64ae3bf97562830ce631115b8a6081253de0004b
Evan Sutherland <stackoverfloweth@gmail.com>
2023-02-13 11:01:38 -0600

    using actual api, now in staging

--- src/services/CollectionsApi.ts
+++ src/services/CollectionsApi.ts
@@ -1,16 +1,14 @@
 import { CollectionItem, CollectionItemResponse } from '@/models'
 import { Api, ApiRoute } from '@/services/Api'
 import { mapper } from '@/services/Mapper'
-import { mocker } from '@/services/Mocker'
 
 export class CollectionsApi extends Api {
 
   protected override route: ApiRoute = '/collections'
 
   public getFlowCollection(): Promise<CollectionItem[]> {
-    return Promise.resolve(mocker.createMany('collectionItem', 10))
-    // return this.get<CollectionItemResponse[]>('views/flow')
-    //   .then(({ data }) => mapper.map('CollectionItemResponse', data, 'CollectionItem'))
+    return this.get<CollectionItemResponse[]>('views/flow')
+      .then(({ data }) => mapper.map('CollectionItemResponse', data, 'CollectionItem'))
   }
 }
 
