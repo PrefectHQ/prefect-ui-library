b8c68e1b02040447598b796a69de6a70e9ac67e0
nicholas <znicholasbrown@gmail.com>
2023-02-16 11:05:34 -0500

    Add artifact card component

--- demo/sections/artifacts/ArtifactCards.vue
+++ demo/sections/artifacts/ArtifactCards.vue
@@ -1,23 +1,31 @@
 <template>
   <ComponentPage title="ArtifactCards" :demos="demos">
     <template #description>
-      <ArtifactSummaryCard :artifact="artifact" />
+      <ArtifactCard :artifact="artifact" />
     </template>
 
     <template #summary-card>
       <ArtifactSummaryCard :artifact="artifact" />
+    </template>
+
+    <template #card>
+      <ArtifactCard :artifact="artifact" />
     </template>
   </ComponentPage>
 </template>
 
 <script lang="ts" setup>
   import { computed } from 'vue'
+  import ArtifactCard from '@/components/ArtifactCard.vue'
   import ArtifactSummaryCard from '@/components/ArtifactSummaryCard.vue'
   import ComponentPage from '@/demo/components/ComponentPage.vue'
   import { useArtifactsMock } from '@/demo/compositions/useArtifactsMock'
   import { DemoSection } from '@/demo/types/demoSection'
 
-  const demos: DemoSection[] = [{ title: 'Summary Card', description: 'An artifact summary card used for lists of artifacts or brief overlays' }]
+  const demos: DemoSection[] = [
+    { title: 'Summary Card', description: 'An artifact summary card used for lists of artifacts or brief overlays' },
+    { title: 'Card', description: 'A card used to render the internal markdown of an artifact' },
+  ]
 
   const start = new Date()
   start.setHours(0)
---
+++ src/components/ArtifactCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <p-card class="artifact-card">
+    <p-markdown-renderer />
+  </p-card>
+</template>
+
+<script lang="ts" setup>
+  import { Artifact } from '@/models'
+
+  defineProps<{
+    artifact: Artifact,
+  }>()
+</script>
+
+<style>
+.artifact-card { @apply
+  max-w-full
+  overflow-auto
+}
+</style>
