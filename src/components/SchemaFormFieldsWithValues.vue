fe853f4c33f9fff28009e9bf0291e74a963f79e7
Marichka Offen <marichka@prefect.io>
2023-02-23 18:31:22 -0500

    Bring SchemaFormFieldsWithValues over from nebula

---
+++ src/components/SchemaFormFieldsWithValues.vue
@@ -0,0 +1,44 @@
+<template>
+  <SchemaFormFields :schema="schema" class="schema-form-fields" />
+</template>
+
+<script lang="ts" setup>
+  import { useValidation } from '@prefecthq/vue-compositions'
+  import { computed } from 'vue'
+  import { Schema, SchemaValues, WorkerSchema } from '@/types';
+  import { getSchemaDefaultValues, mapper } from '@/services';
+  import { useReactiveForm } from '@/compositions';
+  import SchemaFormFields from '@/components/SchemaFormFields.vue';
+
+  const props = defineProps<{
+    schema: Schema | WorkerSchema,
+    values: SchemaValues | null | undefined,
+  }>(
+  )
+  const emit = defineEmits<{
+    (event: 'update:values', value: SchemaValues): void,
+  }>()
+
+  const values = computed({
+    get() {
+      if (!props.values) {
+        return getSchemaDefaultValues(props.schema)
+      }
+
+      return mapper.map('SchemaValuesResponse', { values: props.values, schema: props.schema }, 'SchemaValues')
+    },
+    set(values) {
+      emit('update:values', mapper.map('SchemaValues', { values, schema: props.schema }, 'SchemaValuesRequest'))
+    },
+  })
+
+  const { validate, errors } = useReactiveForm(values, {
+    initialValues: values.value,
+  })
+
+  useValidation(values, 'Parameters', async () => {
+    await validate()
+
+    return Object.entries(errors.value).length === 0
+  })
+</script>
