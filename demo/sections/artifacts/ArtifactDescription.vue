60135d7d56cb4933e6f8ec4cea2921b7ea7d10be
nicholas <znicholasbrown@gmail.com>
2023-03-01 16:19:16 -0500

    Add ArtifactDescription component to demo

--- demo/sections/artifacts/ArtifactDescription.vue
+++ demo/sections/artifacts/ArtifactDescription.vue
@@ -1,10 +1,11 @@
 <template>
   <ComponentPage title="ArtifactDescription" :demos="demos">
-    {{ artifact }}
+    <ArtifactDescription :artifact="artifact" />
   </ComponentPage>
 </template>
 
 <script lang="ts" setup>
+  import ArtifactDescription from '@/components/ArtifactDescription.vue'
   import ComponentPage from '@/demo/components/ComponentPage.vue'
   import { useArtifactMock } from '@/demo/compositions/useArtifactsMock'
   import { DemoSection } from '@/demo/types/demoSection'
