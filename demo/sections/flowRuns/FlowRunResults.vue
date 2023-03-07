7262900dd3e82b6a7a3f6e4dc031100ab7d147b1
nicholas <znicholasbrown@gmail.com>
2023-03-07 16:04:17 -0500

    Add a task runs mock

--- demo/sections/flowRuns/FlowRunResults.vue
+++ demo/sections/flowRuns/FlowRunResults.vue
@@ -10,6 +10,8 @@
   import FlowRunResults from '@/components/FlowRunResults.vue'
   import ComponentPage from '@/demo/components/ComponentPage.vue'
   import { useFlowRunMock } from '@/demo/compositions/useFlowRunsMock'
+  import { useTaskRunsMock } from '@/demo/compositions/useTaskRunsMock'
 
   const flowRun = useFlowRunMock()
+  useTaskRunsMock(10, { flowRunId: flowRun.id })
 </script>
