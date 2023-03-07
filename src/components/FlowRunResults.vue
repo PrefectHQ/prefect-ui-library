9e3ba0b2222030a400c046b8691cdf4c8c78f16e
nicholas <znicholasbrown@gmail.com>
2023-03-07 17:07:03 -0500

    Add FlowRunResults to export

--- src/components/FlowRunResults.vue
+++ src/components/FlowRunResults.vue
@@ -72,11 +72,9 @@
 
 .flow-run-results__list--grid { @apply
   grid
-  grid-cols-1
-  gap-4
-  md:grid-cols-1
-  lg:grid-cols-2
-  xl:grid-cols-3
+  gap-4;
+
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
 }
 
 .flow-run-results__list--rows { @apply
