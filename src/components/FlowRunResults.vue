82d06ee58ce00c3021d0d71662f71913a3eb3a26
nicholas <znicholasbrown@gmail.com>
2023-03-07 16:28:22 -0500

    Add list vs grid views

--- src/components/FlowRunResults.vue
+++ src/components/FlowRunResults.vue
@@ -1,19 +1,39 @@
 <template>
   <div class="flow-run-results">
-    {{ results }}
+    <div class="flow-run-results__button-group-container">
+      <slot name="actions" />
+      <p-button-group v-model="view" :options="viewOptions" />
+    </div>
+
+    <div class="flow-run-results__list" :class="classes.list">
+      <template v-for="result in results" :key="result.id">
+        <ArtifactCard :artifact="result" :condense="condense" />
+      </template>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+  import { ButtonGroupOption } from '@prefecthq/prefect-design'
   import { useSubscription } from '@prefecthq/vue-compositions'
-  import { computed } from 'vue'
+  import { computed, ref } from 'vue'
+  import ArtifactCard from '@/components/ArtifactCard.vue'
   import { useWorkspaceApi } from '@/compositions'
   import { FlowRun } from '@/models'
   import { ArtifactsFilter } from '@/models/Filters'
 
+  type ViewOption = 'grid' | 'rows'
   const props = defineProps<{
     flowRun: FlowRun,
+    view?: ViewOption,
   }>()
+
+  const view = ref<ViewOption>(props.view ?? 'grid')
+  const viewOptions: ButtonGroupOption[] = [
+    { label: '', value: 'grid', icon: 'ViewGridIcon' },
+    { label: '', value: 'rows', icon: 'ViewListIcon' },
+  ]
+  const condense = computed(() => view.value === 'rows')
 
   const api = useWorkspaceApi()
   const resultsFilter = computed<ArtifactsFilter>(() => {
@@ -25,8 +45,43 @@
   })
   const resultsSubscription = useSubscription(api.artifacts.getArtifacts, [resultsFilter])
   const results = computed(() => resultsSubscription.response ?? [])
+
+  const classes = computed(() => {
+    return {
+      list: {
+        'flow-run-results__list--grid': view.value === 'grid',
+        'flow-run-results__list--rows': view.value === 'rows',
+      },
+    }
+  })
 </script>
 
-<style>
 
+<style>
+.flow-run-results { @apply
+  flex
+  flex-col
+  gap-4
+}
+
+.flow-run-results__button-group-container { @apply
+  flex
+  justify-end
+  gap-4
+}
+
+.flow-run-results__list--grid { @apply
+  grid
+  grid-cols-1
+  gap-4
+  md:grid-cols-1
+  lg:grid-cols-2
+  xl:grid-cols-3
+}
+
+.flow-run-results__list--rows { @apply
+  flex
+  flex-col
+  gap-4
+}
 </style>
