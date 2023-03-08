518c6f4551f1e0fb9201f1b43c841aac455f1580
nicholas <znicholasbrown@gmail.com>
2023-03-07 19:44:02 -0500

    Add some data parsing artifacts

--- src/components/ArtifactData.vue
+++ src/components/ArtifactData.vue
@@ -7,6 +7,8 @@
 <script lang="ts" setup>
   import { computed } from 'vue'
   import ArtifactDataMarkdown from '@/components/ArtifactDataMarkdown.vue'
+  import ArtifactDataResult from '@/components/ArtifactDataResult.vue'
+  import ArtifactDataUnknown from '@/components/ArtifactDataUnknown.vue'
   import { Artifact } from '@/models'
 
   const props = defineProps<{
@@ -16,11 +18,13 @@
   const component = computed(() => {
     switch (props.artifact.type) {
       case 'result':
+        return ArtifactDataResult
       case 'markdown':
+        return ArtifactDataMarkdown
       case 'table':
       case 'unknown':
       default:
-        return ArtifactDataMarkdown
+        return ArtifactDataUnknown
     }
   })
 </script>
---
+++ src/components/ArtifactDataUnknown.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="artifact-data-unknown">
+    {{ data }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { computed } from 'vue'
+  import { UnknownArtifact } from '@/models'
+
+  const props = defineProps<{
+    artifact: UnknownArtifact,
+  }>()
+
+  const data = computed(() => props.artifact.data)
+</script>
+
+<style>
+.artifact-data-unknown { @apply
+  max-w-full
+  overflow-auto
+}
+</style>
