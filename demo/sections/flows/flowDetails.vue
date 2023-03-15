b8bcf6fc1b558371446a9fea9592ba65e8542bac
Jennifer Grange <jenny@prefect.io>
2023-03-15 12:22:34 -0400

    add flow details demo

---
+++ demo/sections/flows/flowDetails.vue
@@ -0,0 +1,13 @@
+<template>
+  <ComponentPage title="FlowRunDetails">
+    <FlowDetails :flow="flow" />
+  </ComponentPage>
+</template>
+
+<script lang="ts" setup>
+  import FlowDetails from '@/components/FlowDetails.vue'
+  import ComponentPage from '@/demo/components/ComponentPage.vue'
+  import { useFlowMock } from '@/demo/compositions/useFlowMock'
+
+  const flow = useFlowMock()
+</script>
