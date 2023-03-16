884141287f7635b11a59c10a95cc1bb9dc7d5426
nicholas <znicholasbrown@gmail.com>
2023-03-16 13:06:27 -0400

    Use active color mode composition in demo

--- demo/sections/general/ColorModeSelect.vue
+++ demo/sections/general/ColorModeSelect.vue
@@ -1,15 +1,15 @@
 <template>
   <ComponentPage title="ColorModeSelect">
     <div class="h-96">
-      <ColorModeSelect v-model:selected="selected" />
+      <ColorModeSelect v-model:selected="activeColorMode" />
     </div>
   </ComponentPage>
 </template>
 
 <script lang="ts" setup>
-  import { ref } from 'vue'
   import ColorModeSelect from '@/components/ColorModeSelect.vue'
   import ComponentPage from '@/demo/components/ComponentPage.vue'
+  import { activeColorMode } from '@/demo/utilities/colorMode'
 
-  const selected = ref(null)
+  // const { value: selected } = useColorTheme()
 </script>
