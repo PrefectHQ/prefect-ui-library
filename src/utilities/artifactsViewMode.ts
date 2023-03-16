2531379d8c3a19e02099835f587cb1b1c91eff02
nicholas <znicholasbrown@gmail.com>
2023-03-16 13:22:38 -0400

    Use a cache-key

--- src/utilities/artifactsViewMode.ts
+++ src/utilities/artifactsViewMode.ts
@@ -1,10 +1,10 @@
 import { useLocalStorage } from '@prefecthq/vue-compositions'
 import { computed, WritableComputedRef } from 'vue'
 import { ViewOption } from '@/types/flowRunResults'
+import { getCacheKey } from '@/utilities/cache'
 
-const viewModeLocalStorageKey = 'prefect-ui-library-artifacts-view-mode'
+const viewModeLocalStorageKey = getCacheKey('prefect-ui-library-artifacts-view-mode')
 const defaultValue: ViewOption = 'grid'
-
 
 export function useActiveViewMode(): { activeViewMode: WritableComputedRef<ViewOption> } {
   const { value: viewMode, set: setViewMode } = useLocalStorage<ViewOption>(viewModeLocalStorageKey, defaultValue)
