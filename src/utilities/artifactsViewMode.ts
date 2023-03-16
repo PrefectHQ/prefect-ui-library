2dc46ec5c545dbf9397fbf9b8ff07a416110493c
nicholas <znicholasbrown@gmail.com>
2023-03-16 12:23:58 -0400

    add console statements to composition

--- src/utilities/artifactsViewMode.ts
+++ src/utilities/artifactsViewMode.ts
@@ -9,9 +9,12 @@ const { value: viewMode, set: setViewMode } = useLocalStorage<ViewOption | null>
 
 export const activeViewMode = computed({
   get() {
+    console.log(viewMode.value)
     return viewMode.value
   },
   set(value: ViewOption | null) {
+    console.log('before', viewMode.value)
     setViewMode(value)
+    console.log('after', viewMode.value)
   },
 })
