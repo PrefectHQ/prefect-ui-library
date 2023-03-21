a791865d8440bb3c9913f1bc2036bc97fd738e6b
nicholas <znicholasbrown@gmail.com>
2023-03-21 15:39:59 -0400

    Scaffold demo sectoin

---
+++ demo/sections/artifacts/ArtifactTimeline.vue
@@ -0,0 +1,16 @@
+<template>
+  <ComponentPage title="ArtifactTimeline">
+    <ArtifactTimeline :artifact-key="key" />
+  </ComponentPage>
+</template>
+
+<script lang="ts" setup>
+  import ArtifactTimeline from '@/components/ArtifactTimeline.vue'
+  import ComponentPage from '@/demo/components/ComponentPage.vue'
+  import { useArtifactsMock } from '@/demo/compositions/useArtifactsMock'
+  import { mocker } from '@/services'
+
+
+  const key = `${mocker.create('adjective')}-${mocker.create('noun')}`
+  useArtifactsMock(50, { key })
+</script>
