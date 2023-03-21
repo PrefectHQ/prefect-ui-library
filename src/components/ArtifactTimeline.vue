5ae30f6e13150ff04f724c774dfb2256f5e0ac04
nicholas <znicholasbrown@gmail.com>
2023-03-21 18:51:06 -0400

    Add ArtifactTimeline component - query for artifacts with a given key

--- src/components/ArtifactTimeline.vue
+++ src/components/ArtifactTimeline.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="artifact-timeline">
-    {{ artifactKey }}
+    {{ artifacts }}
   </div>
 </template>
 
 <script lang="ts" setup>
+  import { useSubscription } from '@prefecthq/vue-compositions'
+  import { computed } from 'vue'
+  import { useWorkspaceApi } from '@/compositions'
+  import { ArtifactsFilter } from '@/models/Filters'
+
   const props = defineProps<{
     artifactKey: string,
   }>()
+
+  const api = useWorkspaceApi()
+  const artifactsFilter = computed<ArtifactsFilter>(() => {
+    return {
+      artifacts: {
+        key: [props.artifactKey],
+      },
+    }
+  })
+  const artifactsSubscription = useSubscription(api.artifacts.getArtifacts, [artifactsFilter])
+  const artifacts = computed(() => artifactsSubscription.response ?? [])
 </script>
 
 <style>
---
+++ src/components/ArtifactTimelineItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="artifact-timeline">
+    {{ artifactKey }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const props = defineProps<{
+    artifactKey: string,
+  }>()
+</script>
+
+<style>
+
+</style>
