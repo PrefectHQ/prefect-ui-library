42f891a690c5a2f97193d333632624a85b8234cd
nicholas <znicholasbrown@gmail.com>
2023-03-27 11:47:17 -0400

    Update filters

--- src/components/ArtifactCollections.vue
+++ src/components/ArtifactCollections.vue
@@ -9,7 +9,7 @@
 
     <RowGridLayoutList v-if="artifactsLoaded" :items="artifacts">
       <template #default="{ item }: { item: Artifact }">
-        <router-link :to="routes.artifact(item.id)">
+        <router-link :to="routes.artifactKey(item.id)">
           <ArtifactCard :artifact="item" class="artifact-collections__artifact-card" />
         </router-link>
       </template>
