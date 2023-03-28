c60cb608d0b550b0c653d5fe7e0d6eae7c0329f2
nicholas <znicholasbrown@gmail.com>
2023-03-28 18:26:59 -0400

    Add basic search

--- src/components/ArtifactDataTable.vue
+++ src/components/ArtifactDataTable.vue
@@ -1,12 +1,18 @@
 <template>
-  <p-table :data="data">
-    <!-- hello! -->
-  </p-table>
+  <p-layout-table>
+    <template #header-end>
+      <SearchInput v-model="search" />
+    </template>
+
+    <p-table :data="filteredData" />
+  </p-layout-table>
 </template>
 
 <script lang="ts" setup>
   import { TableData } from '@prefecthq/prefect-design'
-  import { computed } from 'vue'
+  import { useDebouncedRef } from '@prefecthq/vue-compositions'
+  import { computed, ref } from 'vue'
+  import { SearchInput } from '..'
   import { TableArtifact } from '@/models'
 
   const props = defineProps<{
@@ -14,6 +20,21 @@
   }>()
 
   const data = computed<TableData[]>(() => props.artifact.data)
+
+  const search = ref('')
+  const searchDebounced = useDebouncedRef(search, 250)
+
+  const filteredData = computed<TableData[]>(() => {
+    if (!searchDebounced.value) {
+      return data.value
+    }
+
+    const correctedSearch = searchDebounced.value.toLowerCase()
+    return data.value.filter((row) => {
+      const correctedJoinedValues = Object.values(row).join(' ').toLowerCase()
+      return correctedJoinedValues.includes(correctedSearch)
+    })
+  })
 </script>
 
 <style>
