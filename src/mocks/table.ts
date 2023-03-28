9e4255e51bed031109c50a0ef6c3f27df9f54c6d
nicholas <znicholasbrown@gmail.com>
2023-03-28 17:51:00 -0400

    Fix reducer

--- src/mocks/table.ts
+++ src/mocks/table.ts
@@ -2,7 +2,7 @@ import { TableData } from '@prefecthq/prefect-design'
 import { MockFunction } from '@/services/Mocker'
 import { choice } from '@/utilities'
 
-export type MockTableDataTypes = 'noun' | 'number' | 'boolean' | 'date' | 'email' | 'runName'
+export type MockTableDataTypes = 'noun' | 'number' | 'boolean' | 'dateString' | 'email' | 'runName'
 
 export type MockTableOverrides = {
   keys?: string[],
@@ -18,21 +18,16 @@ export const randomTable: MockFunction<TableData[], [Partial<MockTableOverrides>
   const { columnCount = defaultColumnCount, rowCount = defaultRowCount } = overrides
 
   const defaultKeys = this.createMany('noun', columnCount)
-  const defaultTypes: MockTableDataTypes[] = ['noun', 'number', 'boolean', 'date', 'email', 'runName']
+  const defaultTypes: MockTableDataTypes[] = ['noun', 'number', 'boolean', 'dateString', 'email', 'runName']
 
   const { keys = defaultKeys, types = defaultTypes } = overrides
 
-  const table: Record<string, unknown>[] = []
+  const table: Record<string, unknown>[] = Array.from({ length: rowCount }, () => {
+    const data: Record<string, unknown> = keys.reduce(
+      (acc, key) => ({ ...acc, [key]: this.create(choice(types)) }),
+      {})
 
-  keys.forEach((key) => {
-    const type: MockTableDataTypes = choice(types)
-    const values = this.createMany(type, rowCount)
-
-    values.forEach((value, index) => {
-      table[index] = table[index] || {}
-      table[index][key] = value
-    })
+    return data
   })
-
   return table
 }
