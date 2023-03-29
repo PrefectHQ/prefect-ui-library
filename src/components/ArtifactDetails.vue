90a3e2b8b42b14db6384d843ddc192c6f96c34dc
nicholas <znicholasbrown@gmail.com>
2023-03-29 13:23:55 -0400

    Update filters

---
+++ src/components/ArtifactDetails.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="artifact-details">
+    <template v-if="can.read.flow_run && flowRun">
+      <p-divider />
+
+      <p-heading :heading="heading">
+        Flow Run
+      </p-heading>
+
+      <p-key-value label="Start Time" :alternate="alternate">
+        <template #value>
+          <FlowRunStartTime :flow-run="flowRun" />
+        </template>
+      </p-key-value>
+
+      <p-key-value label="Duration" :alternate="alternate">
+        <template #value>
+          <DurationIconText :duration="flowRun.duration" />
+        </template>
+      </p-key-value>
+
+      <p-key-value label="Created" :value="formatDateTimeNumeric(flowRun.created)" :alternate="alternate" />
+
+      <template v-if="flowRun.createdBy">
+        <p-key-value label="Created By" :value="flowRun.createdBy.displayValue" :alternate="alternate" />
+      </template>
+
+      <p-key-value label="Last Updated" :value="formatDateTimeNumeric(flowRun.updated)" :alternate="alternate" />
+
+      <p-key-value label="Tags" :alternate="alternate">
+        <template v-if="flowRun.tags?.length" #value>
+          <p-tags :tags="flowRun.tags!" />
+        </template>
+      </p-key-value>
+
+      <p-key-value label="State Message" :alternate="alternate">
+        <template v-if="stateMessage" #value>
+          <p-text-truncate :text="stateMessage" />
+        </template>
+      </p-key-value>
+    </template>
+
+    <template v-if="can.read.task_run && taskRun">
+      <p-divider />
+
+      <p-heading :heading="heading">
+        Task Run
+      </p-heading>
+
+      <p-key-value label="Created" :value="formatDateTimeNumeric(taskRun.created)" :alternate="alternate" />
+
+      <p-key-value label="Last Updated" :value="formatDateTimeNumeric(taskRun.updated)" :alternate="alternate" />
+
+      <p-key-value label="Tags" :alternate="alternate">
+        <template v-if="taskRun.tags?.length" #value>
+          <p-tags :tags="taskRun.tags!" />
+        </template>
+      </p-key-value>
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { PKeyValue, PTags } from '@prefecthq/prefect-design'
+  import { computed } from 'vue'
+  import { FlowRunStartTime, DurationIconText } from '@/components'
+  import { useCan, useFlowRun, useTaskRun } from '@/compositions'
+  import { Artifact } from '@/models'
+  import { formatDateTimeNumeric } from '@/utilities/dates'
+
+  const props = defineProps<{
+    artifact: Artifact,
+    alternate?: boolean,
+  }>()
+
+  const can = useCan()
+  const heading = computed(() => props.alternate ? 6 : 5)
+
+  const flowRunId = computed(() => props.artifact.flowRunId)
+  const { flowRun } = useFlowRun(flowRunId)
+  const stateMessage = computed(() => flowRun.value?.state?.message)
+
+  const taskRunId = computed(() => props.artifact.taskRunId)
+  const { taskRun } = useTaskRun(taskRunId)
+</script>
+
+<style>
+.flow-run-details { @apply
+  flex
+  flex-col
+  gap-3
+  items-start
+}
+
+.flow-run-details__work-queue-value { @apply
+  flex
+  items-center
+  gap-1
+}
+
+.flow-run-details__tags { @apply
+  mb-1
+  mr-1
+}
+
+.flow-run-details .flow-run-details__small-radar { @apply
+  h-[250px]
+  w-[250px]
+}
+
+.flow-run-details__small-radar-link { @apply
+  cursor-pointer
+  inline-block
+}
+</style>
