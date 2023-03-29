3aa39fc5a5dffa9cf5499e4970c9326d2a7aca57
nicholas <znicholasbrown@gmail.com>
2023-03-29 16:05:45 -0400

    Add some dynamic icons

--- src/components/ArtifactKeyIconText.vue
+++ src/components/ArtifactKeyIconText.vue
@@ -1,12 +1,13 @@
 <template>
   <p-link :to="routes.artifactKey(artifactId)" class="flow-run-icon-text">
-    <p-icon-text icon="PhotographIcon">
+    <p-icon-text :icon="icon">
       <span>{{ artifactKey }}</span>
     </p-icon-text>
   </p-link>
 </template>
 
 <script lang="ts" setup>
+  import { Icon } from '@prefecthq/prefect-design'
   import { computed, toRefs } from 'vue'
   import { useArtifact, useWorkspaceRoutes } from '@/compositions'
 
@@ -18,4 +19,18 @@
   const { artifactId } = toRefs(props)
   const { artifact } = useArtifact(artifactId)
   const artifactKey = computed(() => artifact.value?.key)
+
+  const icon = computed<Icon>(() => {
+    switch (artifact.value?.type) {
+      case 'markdown':
+        return 'DocumentTextIcon'
+      case 'table':
+        return 'TableIcon'
+      case 'result':
+        return 'CalculatorIcon'
+      case 'unknown':
+      default:
+        return 'FingerPrintIcon'
+    }
+  })
 </script>
