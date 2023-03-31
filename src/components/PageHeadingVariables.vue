0ad62f97701c338dba024ebeb25aad4a32b6465b
Nicholas Brown <znicholasbrown@gmail.com>
2023-03-30 21:53:55 -0400

    Feature: Variables page heading and create modal (#1285)
    * Add Variable model
    
    * Add variable model to model bucket
    
    * Add created, updated fields, key => name
    
    * Add variable response model
    
    * Add variable request models
    
    * Add variable create, edit, mappers
    
    * Add mappers to map bucket; CHORE: sort map bucket alphabetically
    
    * Add workspace variables api
    
    * Use patch not put
    
    * Update comment
    
    * Add PageHeadingVariables and VariableCreateModal components
    
    * Remove Partial models
    
    * Add tags to variable create modal
    
    * Add validation rules for value
    
    * Wire up VariableCreateModal
    
    * Remove bad access check and size
    
    * Remove vee-validate
    
    * Don't forget to close the modal
    
    * Used debouncedRef for name
    
    * Update en version of variableAlreadyExists to add article
    
    * Implement debounced isUnique check

---
+++ src/components/PageHeadingVariables.vue
@@ -0,0 +1,19 @@
+<template>
+  <page-heading class="page-heading-variables" :crumbs="crumbs">
+    <template v-if="can.create.variable" #after-crumbs>
+      <p-button inset icon="PlusIcon" @click="open" />
+    </template>
+  </page-heading>
+  <VariableCreateModal v-model="showModal" />
+</template>
+
+<script lang="ts" setup>
+  import PageHeading from '@/components/PageHeading.vue'
+  import { useShowModal } from '@/compositions'
+  import { useCan } from '@/compositions/useCan'
+
+  const crumbs = [{ text: 'Variables' }]
+
+  const can = useCan()
+  const { showModal, open } = useShowModal()
+</script>
