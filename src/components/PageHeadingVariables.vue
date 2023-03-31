11f4a36dd669c4dfef5b7d71bacffd4a5351b9d7
nicholas <znicholasbrown@gmail.com>
2023-03-30 20:16:41 -0400

    Wire up VariableCreateModal

--- src/components/PageHeadingVariables.vue
+++ src/components/PageHeadingVariables.vue
@@ -1,16 +1,19 @@
 <template>
   <page-heading class="page-heading-variables" :crumbs="crumbs">
     <template v-if="can.create.variable" #after-crumbs>
-      <!-- Open VariableCreateModal -->
+      <p-button v-if="can.create.work_pool" inset size="xs" icon="PlusIcon" @click="open" />
     </template>
   </page-heading>
+  <VariableCreateModal v-model="showModal" />
 </template>
 
 <script lang="ts" setup>
   import PageHeading from '@/components/PageHeading.vue'
+  import { useShowModal } from '@/compositions'
   import { useCan } from '@/compositions/useCan'
 
   const crumbs = [{ text: 'Variables' }]
 
   const can = useCan()
+  const { showModal, open } = useShowModal()
 </script>
