f60d300c47855b36e6f810d31f15f3f18dfdab15
nicholas <znicholasbrown@gmail.com>
2023-03-30 22:23:24 -0400

    Add comment for TODO

--- src/components/VariableEditModal.vue
+++ src/components/VariableEditModal.vue
@@ -59,6 +59,10 @@
 
   const api = useWorkspaceApi()
 
+  // TODO: It's possible the better behavior would be to allow the user to
+  // overwrite the other variable but in that case it's not clear
+  // what should happen with this one. For now, we'll just prevent
+  // the user from creating a variable with the same name.
   const isUnique: ValidationRule<string | undefined> = async (value, label, { signal, source, previousValue }) => {
     if (value === previousValue) {
       return
