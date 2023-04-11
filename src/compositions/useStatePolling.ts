095ef48c6a82e814399ddc82332cbc96ceababcd
Craig Harshbarger <pleek91@gmail.com>
2023-04-11 14:15:20 -0500

    Add a composition for dynamically creating subscription options for things with states

---
+++ src/compositions/useStatePolling.ts
@@ -0,0 +1,31 @@
+import { SubscriptionOptions } from '@prefecthq/vue-compositions'
+import { ComputedRef, computed, unref } from 'vue'
+import { MaybeRef, PrefectStateNames } from '@/types'
+
+type StateResource = {
+  stateName: string | null,
+}
+
+const stateNamesThatShouldPoll: (string | null)[] = ['Pending', 'Running'] satisfies PrefectStateNames[]
+
+export function useStatePolling(stateName: MaybeRef<string | null>, interval?: number): ComputedRef<SubscriptionOptions>
+export function useStatePolling(resource: MaybeRef<StateResource>, interval?: number): ComputedRef<SubscriptionOptions>
+export function useStatePolling(resourceOrStateName: MaybeRef<StateResource | string | null>, interval: number = 5000): ComputedRef<SubscriptionOptions> {
+  const shouldPoll = computed(() => {
+    const arg = unref(resourceOrStateName)
+
+    if (arg === null) {
+      return false
+    }
+
+    if (typeof arg === 'object') {
+      return stateNamesThatShouldPoll.includes(arg.stateName)
+    }
+
+    return stateNamesThatShouldPoll.includes(arg)
+  })
+
+  return computed<SubscriptionOptions>(() => ({
+    interval: shouldPoll.value ? interval : undefined,
+  }))
+}
