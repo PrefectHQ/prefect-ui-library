9b99489244e2ccdeb827681572e99c29d00ebe43
nicholas <znicholasbrown@gmail.com>
2023-04-13 13:45:38 -0400

    Scaffold deployments subscription

--- src/components/FlowListItem.vue
+++ src/components/FlowListItem.vue
@@ -1,18 +1,70 @@
 <template>
-  <StateListItem class="flow-list-item">
-    hello
-  </StateListItem>
+  <div class="flow-list-item-container">
+    <StateListItem v-bind="attrs" class="flow-list-item" :tags="tags">
+      <template #name>
+        <p-link :to="routes.flow(flow.id)">
+          <p-heading :heading="5">
+            {{ flow.name }}
+          </p-heading>
+        </p-link>
+      </template>
+    </StateListItem>
+
+    <template v-if="deploymentsSubscription.loading">
+      <p-loading-icon />
+    </template>
+
+    <template v-else-if="deployments.length">
+      {{ deployments }}
+    </template>
+
+    <template v-else>
+      No deployments
+    </template>
+  </div>
 </template>
 
+<script lang="ts">
+  export default {
+    name: 'FlowListItem',
+    expose: [],
+    inheritAttrs: false,
+  }
+</script>
+
 <script lang="ts" setup>
+  import { useSubscription } from '@prefecthq/vue-compositions'
+  import { computed, useAttrs } from 'vue'
   import { StateListItem } from '@/components'
+  import { useWorkspaceApi, useWorkspaceRoutes } from '@/compositions'
+  import { Flow } from '@/models'
+
+  const props = defineProps<{
+    flow: Flow,
+  }>()
+
+  const attrs = useAttrs()
+
+  const api = useWorkspaceApi()
+  const routes = useWorkspaceRoutes()
+
+  const deploymentsSubscription = useSubscription(
+    api.deployments.getDeployments,
+    [
+      {
+        flows: {
+          id: [props.flow.id],
+        },
+      },
+    ],
+  )
+  const deployments = computed(() => deploymentsSubscription.response ?? [])
+
+  const tags = computed(() => deployments.value.map((deployment) => deployment.tags ?? []).flat())
 </script>
 
 <style>
 .flow-list-item { @apply
-  flex
-  flex-col
-  gap-y-2
-  text-base
+  rounded-b-none
 }
 </style>
