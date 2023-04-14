d978a256b587ecc1890ed7a800fb3604917d505c
nicholas <znicholasbrown@gmail.com>
2023-04-14 12:08:45 -0400

    Add filter intersection to mock flows api

--- demo/services/mockWorkspaceFlowsApi.ts
+++ demo/services/mockWorkspaceFlowsApi.ts
@@ -1,7 +1,24 @@
+import { KeyedDataStoreFindCallback } from './KeyedDataStore'
 import { MockApi } from './MockApi'
 import { Flow } from '@/models'
 import { FlowsFilter } from '@/models/Filters'
 import { IWorkspaceFlowsApi } from '@/services'
+
+const flowsItemIntersectsFilter = (filter: FlowsFilter): KeyedDataStoreFindCallback<Flow> => {
+  return (flow: Flow): boolean => {
+    let filtered = false
+
+    if (filter.flows?.id?.length) {
+      filtered = !filter.flows.id.includes(flow.id)
+    }
+
+    if (!filtered && filter.flows?.name?.length && flow.name) {
+      filtered = !filter.flows.name.includes(flow.name)
+    }
+
+    return !filtered
+  }
+}
 
 export class MockWorkspaceFlowsApi extends MockApi implements IWorkspaceFlowsApi {
 
@@ -10,19 +27,32 @@ export class MockWorkspaceFlowsApi extends MockApi implements IWorkspaceFlowsApi
   }
 
   public async getFlows(filter: FlowsFilter = {}): Promise<Flow[]> {
-    if (Object.keys(filter).length) {
-      console.warn('MockWorkspaceFlowsApi has not implemented the filter argument of the getFlows method')
+    const { limit = 200, offset = 0, sort = 'CREATED_DESC' } = filter
+    let flows = await this.flows.findAll(flowsItemIntersectsFilter(filter))
+
+    switch (sort) {
+      /* eslint-disable id-length */
+      case 'CREATED_DESC':
+        flows = flows.sort((a, b) => b.created.getTime() - a.created.getTime())
+        break
+      case 'NAME_ASC':
+        flows = flows.sort((a, b) => a.name.localeCompare(b.name))
+        break
+      case 'NAME_DESC':
+        flows = flows.sort((a, b) => b.name.localeCompare(a.name))
+        break
+      default:
+        console.warn(`MockWorkspaceFlowsApi has not implemented the sort argument for ${sort} of the getFlows method`)
+        break
+      /* eslint-enable id-length */
     }
 
-    return await this.flows.getAll()
+    flows = flows.slice(offset, offset + limit)
+    return flows
   }
 
   public async getFlowsCount(filter: FlowsFilter = {}): Promise<number> {
-    if (Object.keys(filter).length) {
-      console.warn('MockWorkspaceFlowsApi has not implemented the filter argument of the getFlows method')
-    }
-
-    return await this.flows.count()
+    return await this.flows.count(flowsItemIntersectsFilter(filter))
   }
 
   public async deleteFlow(flowId: string): Promise<void> {
