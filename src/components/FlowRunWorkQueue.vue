516d3b0a7f8b77b472c0a7ea95f0bccc8c0d17fc
Jennifer Grange <jenny@prefect.io>
2023-05-16 22:01:12 -0400

    add icon to flow run q

--- src/components/FlowRunWorkQueue.vue
+++ src/components/FlowRunWorkQueue.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="flow-run-work-queue">
-    <span>Work Queue</span> <WorkQueueIconText :work-queue-name="workQueueName" :work-pool-name="workPoolName" />
+    <span>Work Queue</span>
+    <WorkQueueIconText :work-queue-name="workQueueName" :work-pool-name="workPoolName" />
+    <WorkQueueStatusIcon :work-queue-name="workQueueName" :work-pool-name="workPoolName" />
   </div>
 </template>
 
 <script lang="ts" setup>
   import WorkQueueIconText from '@/components/WorkQueueIconText.vue'
+  import WorkQueueStatusIcon from '@/components/WorkQueueStatusIcon.vue'
 
   defineProps<{
     workQueueName: string,
