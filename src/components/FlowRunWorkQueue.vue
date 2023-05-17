fdd4007e9178bdc25b22958668e649ca275f0e66
Jennifer Grange <jenny@prefect.io>
2023-05-16 23:18:07 -0400

    add check for state to work queue status icon so we don't show it on completed runs

--- src/components/FlowRunWorkQueue.vue
+++ src/components/FlowRunWorkQueue.vue
@@ -2,18 +2,23 @@
   <div class="flow-run-work-queue">
     <span>Work Queue</span>
     <WorkQueueIconText :work-queue-name="workQueueName" :work-pool-name="workPoolName" />
-    <WorkQueueStatusIcon :work-queue-name="workQueueName" :work-pool-name="workPoolName" />
+    <WorkQueueStatusIcon v-if="isNotTerminal && workPoolName" :work-queue-name="workQueueName" :work-pool-name="workPoolName" />
   </div>
 </template>
 
 <script lang="ts" setup>
+  import { computed } from 'vue'
   import WorkQueueIconText from '@/components/WorkQueueIconText.vue'
   import WorkQueueStatusIcon from '@/components/WorkQueueStatusIcon.vue'
+  import { isTerminalStateType } from '@/models'
 
-  defineProps<{
+  const props = defineProps<{
     workQueueName: string,
     workPoolName?: string | null,
+    flowRunState?: string | null,
   }>()
+
+  const isNotTerminal = computed(() => props.flowRunState && !isTerminalStateType(props.flowRunState))
 </script>
 
 <style>
