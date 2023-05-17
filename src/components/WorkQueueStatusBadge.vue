1c8d7400e751fe803b8e0c498b60194103d96a1f
Jennifer Grange <jenny@prefect.io>
2023-05-17 11:45:47 -0400

    revert work queue status badge

--- src/components/WorkQueueStatusBadge.vue
+++ src/components/WorkQueueStatusBadge.vue
@@ -1,7 +1,7 @@
 <template>
   <template v-if="workQueue && workQueueStatus">
     <p-tag class="work-queue-status-badge" :class="classes">
-      <WorkQueueStatusIcon :work-queue-name="workQueueName" :work-pool-name="workPoolName" class="work-queue-status-badge__icon" />
+      <WorkQueueStatusIcon :work-queue-name="workQueueName" class="work-queue-status-badge__icon" />
       {{ label }}
     </p-tag>
   </template>
@@ -11,15 +11,13 @@
   import { computed } from 'vue'
   import WorkQueueStatusIcon from '@/components/WorkQueueStatusIcon.vue'
   import { useWorkQueueStatus } from '@/compositions'
-  import { WorkPool, WorkQueue } from '@/models'
+  import { WorkQueue } from '@/models'
 
   const props = defineProps<{
     workQueue: WorkQueue,
-    workPool: WorkPool,
   }>()
 
   const workQueueName = computed(() => props.workQueue.name)
-  const workPoolName = computed(() => props.workPool.name)
   const workQueueId = computed(() => props.workQueue.id)
   const { workQueueStatus } = useWorkQueueStatus(workQueueId)
   const healthy = computed(() => workQueueStatus.value?.healthy)
