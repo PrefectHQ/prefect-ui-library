f24a6341819797cb94f84be8388bc0b24dda31f5
nicholas <znicholasbrown@gmail.com>
2023-05-23 14:24:01 -0400

    Add test

--- src/services/schemas/constants.spec.ts
+++ src/services/schemas/constants.spec.ts
@@ -1,7 +1,9 @@
-import { describe, expect } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { MAX_SCHEMA_PROPERTY_LEVEL } from '@/services/schemas/constants'
 
 
 describe('constants', () => {
-  expect(MAX_SCHEMA_PROPERTY_LEVEL).toBe(2)
+  it('exports MAX_SCHEMA_PROPERTY_LEVEL', () => {
+    expect(MAX_SCHEMA_PROPERTY_LEVEL).toBe(2)
+  })
 })
