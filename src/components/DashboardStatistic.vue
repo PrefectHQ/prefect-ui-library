ca5a527e5cc3da3dc067b2d17d120a4a821894e8
Craig Harshbarger <pleek91@gmail.com>
2023-06-02 15:05:33 -0500

    add a filter type and a statistic component

---
+++ src/components/DashboardStatistic.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="dashboard-statistic">
+    <span class="dashboard-statistic__value">{{ value.toLocaleString() }}</span>
+    <template v-if="isDefined(label)">
+      <span class="dashboard-statistic__label">{{ label.toLocaleString() }}</span>
+    </template>
+    <template v-if="isDefined(meta)">
+      <span class="dashboard-statistic__meta">{{ meta.toLocaleString() }}</span>
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { isDefined } from '@prefecthq/prefect-design'
+
+  defineProps<{
+    value: string | number,
+    label?: string,
+    meta?: string,
+  }>()
+</script>
+
+<style>
+.dashboard-statistic { @apply
+  inline-flex
+  items-end
+  gap-1
+  text-sm
+}
+
+.dashboard-statistic__value { @apply
+  font-semibold
+}
+
+.dashboard-statistic__meta { @apply
+  text-slate-500
+}
+</style>
