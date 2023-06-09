670c2bda9f9dae77bcf18bc549e311822721247e
Brandon Reid <brandonreidak@gmail.com>
2023-06-09 08:52:13 -0300

    add limit to work queues

--- src/components/WorkPoolQueueStatusArray.vue
+++ src/components/WorkPoolQueueStatusArray.vue
@@ -1,14 +1,17 @@
 <template>
-  <div v-if="workPoolQueues.length <= maxWorkQueues" class="work-pool-queue-status-array">
-    <WorkPoolQueueStatusIcon
-      v-for="workQueue in workPoolQueues"
-      :key="workQueue.id"
-      :work-queue-name="workQueue.name"
-      :work-pool-name="workPool.name"
-      class="work-pool-queue-status-badge__icon"
-    />
+  <div class="work-pool-queue-status-array">
+    <template v-if="!showTooMany && workPoolQueues.length > 0">
+      <WorkPoolQueueStatusIcon
+        v-for="workQueue in workPoolQueues"
+        :key="workQueue.id"
+        :work-queue-name="workQueue.name"
+        :work-pool-name="workPool.name"
+        class="work-pool-queue-status-badge__icon"
+      />
+    </template>
+    <span v-if="!showTooMany && workPoolQueues.length < 1" class="work-pool-queue-status-array__none">N/A</span>
+    <span v-if="showTooMany">Too many to show here.</span>
   </div>
-  <span v-else>{{ workPoolQueues.length }}</span>
 </template>
 
 <script lang="ts" setup>
@@ -25,8 +28,12 @@
   const maxWorkQueues = 50
   const api = useWorkspaceApi()
 
-  const workPoolQueuesSubscription = useSubscription(api.workPoolQueues.getWorkPoolQueues, [props.workPool.name])
+  const workPoolQueuesSubscription = useSubscription(
+    api.workPoolQueues.getWorkPoolQueues,
+    [props.workPool.name, { limit: maxWorkQueues }],
+  )
   const workPoolQueues = computed(() => workPoolQueuesSubscription.response ?? [])
+  const showTooMany = computed(() => workPoolQueues.value.length >= maxWorkQueues)
 </script>
 
 <style>
@@ -37,5 +44,9 @@
   justify-center
   gap-1
   max-w-full
+}
+
+.work-pool-queue-status-array__none { @apply
+  text-slate-500
 }
 </style>
