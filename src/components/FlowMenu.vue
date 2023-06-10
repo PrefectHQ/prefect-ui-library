3c81a3982921636f3d9e5618dacff5cb319b1110
AmanSal1 <amansalwan1@gmail.com>
2023-06-10 10:29:46 +0530

    I have made changes to the FlowMenu.vue file to add the functionality of deleting a flow directly from the overflow menu.

--- src/components/FlowMenu.vue
+++ src/components/FlowMenu.vue
@@ -1,9 +1,18 @@
 <template>
   <p-icon-button-menu v-bind="$attrs">
-    <copy-overflow-menu-item label="Copy ID" :item="flow.id" />
+      <copy-overflow-menu-item label="Copy ID" :item="flow.id" />
+    <p-overflow-menu-item label="Delete" :value="flow.id" @click="openDeleteModal" />
     <slot v-bind="{ flow }" />
   </p-icon-button-menu>
+  <ConfirmDeleteModal
+    v-if="flow"
+    v-model:showModal="showDeleteModal"
+    label="Flow"
+    :name="flow.name!"
+    @delete="deleteFlow(flow.id)"
+  />
 </template>
+
 
 <script lang="ts">
   export default {
@@ -16,8 +25,23 @@
 <script lang="ts" setup>
   import CopyOverflowMenuItem from '@/components/CopyOverflowMenuItem.vue'
   import { Flow } from '@/models'
+  import {ConfirmDeleteModal} from "@/components/index"
+  import { deleteItem } from '@/utilities'
+  import {useShowModal, useWorkspaceApi} from "@/compositions";
 
   defineProps<{
     flow: Flow,
+
+
   }>()
+
+   const api = useWorkspaceApi()
+  const { showModal: showDeleteModal, open: openDeleteModal } = useShowModal()
+
+   const emit = defineEmits(['delete'])
+
+  const deleteFlow = async (id: string): Promise<void> => {
+      await deleteItem(id, api.flows.deleteFlow, 'Flow')
+      emit('delete', id)
+  }
 </script>
