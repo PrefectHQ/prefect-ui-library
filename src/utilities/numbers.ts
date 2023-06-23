6fa2058d66e493efe56dd474819526cb08bb8fb7
Craig Harshbarger <pleek91@gmail.com>
2023-06-23 11:54:45 -0500

    Avoid doing math on floats

--- src/utilities/numbers.ts
+++ src/utilities/numbers.ts
@@ -5,8 +5,10 @@ export function isNumber(value: unknown): value is number {
 export function toPercent(x: number, y: number): number | undefined {
   const decimal = x / y
   const percent = Math.round((decimal + Number.EPSILON) * 10000) / 100
+
   if (isNaN(percent)) {
     return undefined
   }
+
   return percent
 }
