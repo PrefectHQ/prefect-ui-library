f1fcf8f0bad0aad2f5e3633c83a5161c97fd2eb8
Craig Harshbarger <pleek91@gmail.com>
2023-07-14 09:15:35 -0500

    Remove the term "Work Pool Queue" in favor of just "Work Queue"

--- src/components/WorkPoolQueuesDeleteButton.vue
+++ src/components/WorkPoolQueuesDeleteButton.vue
@@ -4,8 +4,8 @@
   </Transition>
   <ConfirmDeleteModal
     v-model:showModal="showModal"
-    name="selected work pool queues"
-    label="Work Pool Queues"
+    name="selected work queues"
+    label="Work Queues"
     @delete="deleteWorkPoolQueues(workPoolQueues)"
   />
 </template>
@@ -34,9 +34,9 @@
   const deleteWorkPoolQueues = async (workPoolQueues: WorkPoolQueue[]): Promise<void> => {
     const toastMessage = computed(() => {
       if (workPoolQueues.length === 1) {
-        return localization.success.delete('Work pool queue')
+        return localization.success.delete('Work queue')
       }
-      return localization.success.delete(`${workPoolQueues.length} work pool queues`)
+      return localization.success.delete(`${workPoolQueues.length} work queues`)
     })
 
     try {
@@ -45,7 +45,7 @@
       showToast(toastMessage, 'success')
       emit('delete')
     } catch (error) {
-      showToast(localization.error.delete('work pool queues'), 'error')
+      showToast(localization.error.delete('work queues'), 'error')
     } finally {
       close()
     }
