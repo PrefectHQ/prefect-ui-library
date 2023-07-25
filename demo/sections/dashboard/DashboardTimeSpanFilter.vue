c2a19cb98a8cfb84c1d7aebffbf45d5b55f73572
AmanSal1 <amansalwan1@gmail.com>
2023-07-25 23:50:01 +0530

    Demo section against FlowRunStateTypeTab component

--- demo/sections/dashboard/DashboardTimeSpanFilter.vue
+++ demo/sections/dashboard/DashboardTimeSpanFilter.vue
@@ -1,101 +1,47 @@
 <template>
-  <ComponentPage title="DashboardTimeSpanFilter">
-    <TimeSpanFilter v-model:selected="selected" />
-    <p-tabs v-model:selected="selected" :tabs="tabs" class="flow-run-state-type-tabs">
-      <template #heading="{ tab }">
-        <FlowRunStateTypeCount :state-type="getTabStates(tab.label)" :filter="getStateTypeFilter(tab.label)" />
-      </template>
-      <template #content="{ tab }">
-        <p-content>
-          <FlowRunList :flow-runs="getFlowRuns(tab.label)" class="color-mode-default" />
-        </p-content>
-      </template>
-    </p-tabs>
+  <ComponentPage title="FlowRunStateTypeTab">
+    <FlowRunStateTypeTab :filter="myFlowRunsFilter" />
   </ComponentPage>
 </template>
 
-
 <script lang="ts" setup>
-  import { secondsInDay } from 'date-fns'
-  import { ref, computed } from 'vue'
-  import { useRouteQueryParam } from '@prefecthq/vue-compositions'
-  import FlowRunsAccordion from '@/components/FlowRunsAccordion.vue'
-  import FlowRunStateTypeCount from '@/components/FlowRunStateTypeCount.vue'
-  import FlowRunStateTypeTabDescription from '@/components/FlowRunStateTypeTabDescription.vue'
-  import TimeSpanFilter from '@/components/TimeSpanFilter.vue'
-  import ComponentPage from '@/demo/components/ComponentPage.vue'
-  import { useFlowRunsCount } from '@/compositions'
-  import { FlowRunsFilter } from '@/models/Filters'
-  import { StateType } from '@/models/StateType'
-  import FlowRunList from '@/components/FlowRunList.vue'
-  import { useFlowRunsMock } from '@/demo/compositions/useFlowRunsMock'
 
-  const selected = ref(secondsInDay)
+  import { ref } from 'vue';
+  import { secondsInDay } from 'date-fns';
+  import ComponentPage from '@/demo/components/ComponentPage.vue';
+  import {FlowRunStateTypeTab} from "@/components"; // Adjust the import path if needed
+  import { useFlowRunsMock } from "@/demo/compositions/useFlowRunsMock";
 
-  const props = defineProps<{
-    filter?: FlowRunsFilter,
-  }>()
 
-  const tabStates: Record<string, StateType[]> = {
-    failed: ['failed', 'crashed'],
-    running: ['running', 'pending', 'cancelling'],
-    completed: ['completed'],
-    scheduled: ['scheduled', 'paused'],
-    cancelled: ['cancelled'],
-  }
-
-  const { count: cancelledCount } = useFlowRunsCount(getStateTypeFilter('cancelled'))
-
-  const tabs = computed(() => {
-    const tabNames = Object.keys(tabStates)
-
-    if (!cancelledCount.value || cancelledCount.value < 1) {
-      tabNames.filter(value => value !== 'cancelled')
-    }
-
-    return tabNames
-  })
-
-  function getTabStates(tab: string): StateType[] {
-    return tabStates[tab]
-  }
-
-  function getStateTypeFilter(tab: string): FlowRunsFilter {
-    const types = getTabStates(tab)
-
-    return {
-      ...props.filter,
-      flowRuns: {
-        ...props.filter?.flowRuns,
-        state: {
-          ...props.filter?.flowRuns?.state,
-          type: types,
-        },
+  const selected = ref(secondsInDay);
+ const myFlowRunsFilter = {
+    flowRuns: {
+      state: {
+        type: ['running', 'pending','failed'],
       },
-      sort: 'EXPECTED_START_TIME_DESC',
-    }
-  }
 
-function getFlowRuns(tab: string) {
-  // Retrieve the flow runs data
+    },
+
+  };
+
+ function getFlowRuns() {
+  const selectedStateTypes = myFlowRunsFilter.flowRuns.state.type;
   const flowRunsData = useFlowRunsMock(7);
 
-  // Get the StateType array corresponding to the selected tab
-  const stateTypesForTab = getTabStates(tab);
-
   // Filter flow runs based on their state type matching the selected tab
-  const filteredFlowRuns = flowRunsData.filter((flowRun) => {
-    return stateTypesForTab.includes(flowRun.state.type);
+  const filteredFlowRuns = flowRunsData.filter((flowrun) => {
+    // Perform a type check to ensure flowrun.state is not null before making the comparison
+    if (flowrun.state !== null) {
+      // Convert flowrun.state to a string before comparison
+      const stateAsString = flowrun.state.toString();
+      return selectedStateTypes.includes(stateAsString);
+    }
+    return false; // If flowrun.state is null, exclude it from the filtered result
   });
 
   return filteredFlowRuns;
 }
 
 
-</script>
 
-<style>
-.flow-run-state-type-tabs .p-tab {
-  @apply flex-grow flex items-center justify-center;
-}
-</style>
+</script>
