45574574c57fd688f8ce59d1002051a197749051
Jennifer Grange <jenny@prefect.io>
2023-08-20 13:49:12 -0400

    add active slots models

---
+++ src/models/ConcurrencyV2ActiveSlots.ts
@@ -0,0 +1,5 @@
+export type ConcurrencyV2ActiveSlots = {
+  id: string,
+  name: string,
+  limit: number,
+}
---
+++ src/models/ConcurrencyV2IncrementActiveSlots.ts
@@ -0,0 +1,7 @@
+export type ConcurrencyV2IncrementActiveSlots = {
+  slots: number,
+  names: [
+    string
+  ],
+  mode: string,
+}
