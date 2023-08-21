4aa1172c3832038c2299b8aac6f5a3ed26b73840
Jennifer Grange <jenny@prefect.io>
2023-08-20 22:58:19 -0400

    add a basic v2 concurrency limits table

---
+++ src/components/ConcurrencyLimitsV2Table.vue
@@ -0,0 +1,62 @@
+<template>
+  <ConcurrencyLimitsPageEmptyState v-if="empty && loaded" />
+  <p-table v-else class="concurrency-limits-table__table" :columns="columns" :data="concurrencyLimits">
+    <template #tag="{ row }">
+      <p-link :to="routes.concurrencyLimit(row.id)">
+        {{ row.name }}
+      </p-link>
+    </template>
+    <template #active-task-runs="{ row }">
+      {{ row.activeSlots }}
+    </template>
+
+    <template #action-heading>
+      <span />
+    </template>
+
+
+    <template #empty-state>
+      <PEmptyResults>
+        <template #message>
+          No task concurrency limits
+        </template>
+      </PEmptyResults>
+    </template>
+  </p-table>
+</template>
+
+<script lang="ts" setup>
+  import { TableColumn } from '@prefecthq/prefect-design'
+  import { useSubscription } from '@prefecthq/vue-compositions'
+  import { computed } from 'vue'
+  import { ConcurrencyLimitsPageEmptyState } from '@/components'
+  import { useWorkspaceApi, useWorkspaceRoutes } from '@/compositions'
+  import { ConcurrencyV2Limit } from '@/models/ConcurrencyV2Limit'
+
+  const api = useWorkspaceApi()
+  const columns: TableColumn<ConcurrencyV2Limit>[] = [
+    {
+      property: 'name',
+      label: 'Name',
+    },
+    {
+      property: 'limit',
+      label: 'Limit',
+    },
+    {
+      property: 'activeSlots',
+      label: 'Active Slots',
+    },
+    {
+      label: 'Action',
+      width: '42px',
+    },
+  ]
+
+  const concurrencyLimitSubscription = useSubscription(api.concurrencyLimitsV2.getConcurrencyV2Limits)
+  const concurrencyLimits = computed(() => concurrencyLimitSubscription.response ?? [])
+  const empty = computed(() => concurrencyLimitSubscription.executed && !concurrencyLimits.value.length)
+  const loaded = computed(() => concurrencyLimitSubscription.executed)
+
+  const routes = useWorkspaceRoutes()
+</script>
