3c6987b1fd7c58afb837b99a86202f416288e175
Jennifer Grange <jenny@prefect.io>
2023-08-24 22:26:02 -0400

    add modal to menu

--- src/components/ConcurrencyLimitV2Menu.vue
+++ src/components/ConcurrencyLimitV2Menu.vue
@@ -2,7 +2,9 @@
   <p-icon-button-menu v-bind="$attrs">
     <copy-overflow-menu-item label="Copy ID" :item="concurrencyLimit.id" />
     <p-overflow-menu-item v-if="can.delete.concurrency_limit" label="Delete" @click="open" />
+    <p-overflow-menu-item v-if="can.read.concurrency_limit" label="Edit" @click="openEdit" />
   </p-icon-button-menu>
+  <ConcurrencyLimitsV2UpdateModal v-model:showModal="showEditModal" :concurrency-limit="concurrencyLimit" />
   <ConfirmDeleteModal
     v-model:showModal="showModal"
     :name="concurrencyLimit.name"
@@ -20,7 +22,7 @@
 </script>
 
 <script lang="ts" setup>
-  import { CopyOverflowMenuItem, ConfirmDeleteModal } from '@/components'
+  import { CopyOverflowMenuItem, ConfirmDeleteModal, ConcurrencyLimitsV2UpdateModal } from '@/components'
   import { useShowModal, useCan, useWorkspaceApi } from '@/compositions'
   import { ConcurrencyV2Limit } from '@/models'
   import { deleteItem } from '@/utilities'
@@ -36,6 +38,8 @@
   const can = useCan()
 
   const { showModal, open, close } = useShowModal()
+
+  const { showModal: showEditModal, open: openEdit } = useShowModal()
 
   const api = useWorkspaceApi()
 
