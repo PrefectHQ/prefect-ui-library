c20736dd536a15662f428bf4795ea011e4600398
Craig Harshbarger <pleek91@gmail.com>
2023-08-30 17:31:52 -0500

    Update the filters to match the other pages

--- src/components/ArtifactCollections.vue
+++ src/components/ArtifactCollections.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="artifact-collections">
     <div class="artifact-collections__filters">
-      <ResultsCount :label="localization.info.artifact" :count="artifactsCount" class="artifact-collection-list__results" />
-      <SearchInput v-model="searchTerm" :placeholder="localization.info.artifactSearch" :label="localization.info.artifactSearch" class="artifact-collection-list__search" />
-      <ArtifactTypeSelect v-model:selected="selectedType" class="artifact-collection-list__type" />
-      <ViewModeButtonGroup class="artifact-collections__view-mode-button-group" />
+      <ResultsCount :label="localization.info.artifact" :count="artifactsCount" class="artifact-collections__results" />
+      <SearchInput v-model="searchTerm" :placeholder="localization.info.artifactSearch" :label="localization.info.artifactSearch" class="artifact-collections__search" />
+      <ArtifactTypeSelect v-model:selected="selectedType" class="artifact-collections__type" />
+      <ViewModeButtonGroup class="artifact-collections__view-mode" />
     </div>
 
     <RowGridLayoutList v-if="artifactsLoaded" :items="artifacts">
@@ -78,35 +78,39 @@
 
 .artifact-collections__filters { @apply
   grid
-  md:flex
-  gap-2
-  justify-end
   items-center
+  gap-2
 }
 
 .artifact-collections__filters {
-  grid-template-columns: minmax(0, 1fr);
-  grid-template-areas: "search"
+  /* grid-template-columns: minmax(0, 1fr);
+  grid-template-areas: "results"
+                       "search"
                        "type"
-                       "results"
-                       "view-mode";
+                       "view-mode"; */
+
+  grid-template-areas: "search search"
+                        "type type"
+                        "results view-mode";
+  grid-template-columns: 1fr min-content
 
 }
 
-@screen sm {
+@screen md {
   .artifact-collections__filters {
-    grid-template-columns: minmax(0, 1fr) minmax(0, 1fr);
+    /* grid-template-columns: minmax(0, 1fr) minmax(0, 1fr);
     grid-template-areas: "search search"
                         "type type"
-                        "results results"
-                        "  view-mode";
+                        "results view-mode"
+                        "  "; */
+
+    grid-template-areas: "results search type view-mode";
+    grid-template-columns: 1fr max-content min-content min-content;
   }
 }
 
-.artifact-collections__results { @apply
-  mt-2
-  md:mt-0
-  md:mr-auto
+.artifact-collections__results {
+  grid-area: results;
 }
 
 .artifact-collections__search {
@@ -119,13 +123,5 @@
 
 .artifact-collections__type {
   grid-area: type;
-}
-
-.artifact-collections__results {
-  grid-area: results;
-}
-
-.artifact-collections__view-mode-button-group { @apply
-  ml-auto
 }
 </style>
