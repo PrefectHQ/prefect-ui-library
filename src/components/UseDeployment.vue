a9143d7358fb34477eb8c69bd04200791e3a9801
Craig Harshbarger <pleek91@gmail.com>
2023-09-13 16:35:34 -0500

    Add a component for useDeployment

---
+++ src/components/UseDeployment.vue
@@ -0,0 +1,13 @@
+<template>
+  <slot v-if="deployment" :deployment="deployment" />
+</template>
+
+<script lang="ts" setup>
+  import { useDeployment } from '@/compositions/useDeployment'
+
+  const props = defineProps<{
+    deploymentId: string | null | undefined,
+  }>()
+
+  const { deployment } = useDeployment(() => props.deploymentId)
+</script>
