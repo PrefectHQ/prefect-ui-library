2efdfc467dff199cd6ebd28842400ec3905776ed
Craig Harshbarger <pleek91@gmail.com>
2023-09-18 13:31:28 -0500

    Better types

--- src/services/pagination.ts
+++ src/services/pagination.ts
@@ -1,3 +1,4 @@
+import { Getter } from '@prefecthq/prefect-design'
 import { useSubscription, useSubscriptionWithDependencies } from '@prefecthq/vue-compositions'
 import { ComputedRef, Ref, computed, ref, toValue, watch } from 'vue'
 import { UseSubscriptions, useSubscriptions } from '@/compositions/useSubscriptions'
@@ -8,64 +9,53 @@ type PaginationFilter = {
   offset?: number,
 }
 
-type PaginationParameters<
-  TFilter extends PaginationFilter
-> = [filter?: TFilter, ...any[]]
-
-type PaginationFetchAction<
-  TFilter extends PaginationFilter,
-  TResponse extends unknown[]
-> = (...parameters: PaginationParameters<TFilter>) => Promise<TResponse>
+type PaginationParameters = [filter?: PaginationFilter, ...any[]]
+type PaginationFetchAction = (...parameters: PaginationParameters) => Promise<unknown[]>
+type PaginationCountAction = (...parameters: PaginationParameters) => Promise<number>
 
 type FetchSubscriptionAction<
-  TFilter extends PaginationFilter,
-  TResponse extends unknown[]
-> = (parameters: PaginationParameters<TFilter>[]) => Promise<TResponse>
-
-type PaginationCountAction<
-  TFilter extends PaginationFilter
-> = (...parameters: PaginationParameters<TFilter>) => Promise<number>
-
-type ParametersGetter<
-  TFilter extends PaginationFilter
-> = () => PaginationParameters<TFilter>
+  TFetch extends PaginationFetchAction
+> = (parameters: Parameters<TFetch>[]) => Promise<Awaited<ReturnType<TFetch>>>
 
 type PaginationMode = 'page' | 'infinite'
 
 type PaginationFactoryParameters<
-  TResponse extends unknown[],
-  TFilter extends PaginationFilter
+  TFetch extends PaginationFetchAction,
+  TCount extends PaginationCountAction
 > = {
-  fetchMethod: PaginationFetchAction<TFilter, TResponse>,
-  fetchParametersGetter: ParametersGetter<TFilter>,
-  countMethod: PaginationCountAction<TFilter>,
-  countParametersGetter: ParametersGetter<TFilter>,
+  fetchMethod: TFetch,
+  fetchParametersGetter: Getter<Parameters<TFetch>>,
+  countMethod: TCount,
+  countParametersGetter: Getter<Parameters<TCount>>,
   mode?: PaginationMode,
 }
 
-  type ApiPaginationFactory<
-    TResponse extends unknown[],
-    TFilter extends PaginationFilter
-  > = {
-    subscriptions: UseSubscriptions<PaginationCountAction<TFilter> | FetchSubscriptionAction<TFilter, TResponse>>,
-    results: ComputedRef<TResponse>,
-    total: ComputedRef<number>,
-    pages: ComputedRef<number>,
-    page: Ref<number>,
-    next: () => void,
-    previous: () => void,
-  }
+type ApiPaginationFactory<
+  TFetch extends PaginationFetchAction,
+  TCount extends PaginationCountAction
+> = {
+  subscriptions: UseSubscriptions<TCount | FetchSubscriptionAction<TFetch> | (() => undefined)>,
+  results: ComputedRef<Awaited<ReturnType<TFetch>>>,
+  total: ComputedRef<number>,
+  pages: ComputedRef<number>,
+  page: Ref<number>,
+  next: () => void,
+  previous: () => void,
+}
 
 export function apiPaginationFactory<
-  TResponse extends unknown[],
-  TFilter extends PaginationFilter
+  TFetch extends PaginationFetchAction,
+  TCount extends PaginationCountAction
 >({
   fetchMethod,
   fetchParametersGetter,
   countMethod,
   countParametersGetter,
   mode = 'page',
-}: PaginationFactoryParameters<TResponse, TFilter>): ApiPaginationFactory<TResponse, TFilter> {
+}: PaginationFactoryParameters<TFetch, TCount>): ApiPaginationFactory<TFetch, TCount> {
+
+  type TFetchFilter = Parameters<TFetch>[0]
+
   const page = ref(mode === 'page' ? 1 : 0)
   const pages = computed(() => Math.ceil(total.value / getLimit()))
 
@@ -79,16 +69,16 @@ export function apiPaginationFactory<
   const countSubscription = useSubscriptionWithDependencies(countMethod, countSubscriptionParameters)
   const total = computed(() => countSubscription.response ?? 0)
 
-  async function fetchSubscriptionAction(parameters: PaginationParameters<TFilter>[]): Promise<TResponse> {
+  async function fetchSubscriptionAction(parameters: Parameters<TFetch>[]): Promise<Awaited<ReturnType<TFetch>>> {
     const promises = parameters.map(parameters => useSubscription(fetchMethod, parameters).promise())
     const subscriptions = await Promise.all(promises)
     const response = subscriptions.flatMap(subscription => subscription.response)
 
-    return response as unknown as TResponse
+    return response as Awaited<ReturnType<TFetch>>
   }
 
-  const fetchSubscriptionParameters = ref(getFetchSubscriptionParameters()) as Ref<PaginationParameters<TFilter>[]>
-  const fetchSubscriptionDependantParameters = computed<null | [PaginationParameters<TFilter>[]]>(() => {
+  const fetchSubscriptionParameters = ref(getFetchSubscriptionParameters()) as Ref<Parameters<TFetch>[]>
+  const fetchSubscriptionDependantParameters = computed<null | [Parameters<TFetch>[]]>(() => {
     if (total.value === 0) {
       return null
     }
@@ -96,7 +86,7 @@ export function apiPaginationFactory<
     return [fetchSubscriptionParameters.value]
   })
   const fetchSubscription = useSubscriptionWithDependencies(fetchSubscriptionAction, fetchSubscriptionDependantParameters)
-  const results = computed(() => fetchSubscription.response ?? [] as unknown as TResponse)
+  const results = computed(() => fetchSubscription.response ?? [] as unknown as Awaited<ReturnType<TFetch>>)
 
   const subscriptions = useSubscriptions([
     countSubscription,
@@ -123,14 +113,14 @@ export function apiPaginationFactory<
     fetchSubscriptionParameters.value = getFetchSubscriptionParameters()
   }
 
-  function getFetchSubscriptionParameters(): PaginationParameters<TFilter>[] {
+  function getFetchSubscriptionParameters(): Parameters<TFetch>[] {
     const [filter, ...rest] = toValue(fetchParametersGetter)
     const filters = getFetchParametersForPages(page.value, filter)
 
-    return filters.map(filter => [filter, ...rest])
+    return filters.map(filter => [filter, ...rest]) as unknown as Parameters<TFetch>[]
   }
 
-  function getFetchParametersForPages(page: number, filter?: TFilter): TFilter[] {
+  function getFetchParametersForPages(page: number, filter?: TFetchFilter): TFetchFilter[] {
     if (mode === 'page') {
       return [getFetchFilterForPage(page, filter)]
     }
@@ -138,14 +128,14 @@ export function apiPaginationFactory<
     return repeat(page, page => getFetchFilterForPage(page, filter))
   }
 
-  function getFetchFilterForPage(page: number, filter?: TFilter): TFilter {
+  function getFetchFilterForPage(page: number, filter?: TFetchFilter): TFetchFilter {
     const limit = getLimit()
 
     return {
       ...filter,
       offset: limit * page,
       limit: limit,
-    } as TFilter
+    }
   }
 
   function getLimit(): number {
