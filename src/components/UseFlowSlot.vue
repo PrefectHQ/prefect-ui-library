456fbb22bcc1cef5e8bcf8c2a57469051d6cc3ed
Craig Harshbarger <pleek91@gmail.com>
2023-09-19 13:00:34 -0500

    Add a UseFlowSlot component

---
+++ src/components/UseFlowSlot.vue
@@ -0,0 +1,13 @@
+<template>
+  <slot v-if="flow" :flow="flow" />
+</template>
+
+<script lang="ts" setup>
+  import { useFlow } from '@/compositions/useFlow'
+
+  const props = defineProps<{
+    flowId: string | null | undefined,
+  }>()
+
+  const { flow } = useFlow(() => props.flowId)
+</script>
