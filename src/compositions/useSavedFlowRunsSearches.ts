34a58569478b8216af366a61e25021dc90a3456c
Collin Choy <collin@prefect.io>
2023-09-21 18:21:58 -0400

    Add handling for if customdefault is no longer in the workspace's saved searches.

--- src/compositions/useSavedFlowRunsSearches.ts
+++ src/compositions/useSavedFlowRunsSearches.ts
@@ -1,5 +1,5 @@
 import { useSubscription } from '@prefecthq/vue-compositions'
-import { computed, ComputedRef } from 'vue'
+import { computed, ComputedRef, watch } from 'vue'
 import { useCustomDefaultFlowRunsFilter } from '@/compositions/useCustomDefaultFlowRunsFilter'
 import { useWorkspaceApi } from '@/compositions/useWorkspaceApi'
 import { FlowRunsFilter, SavedSearch, SavedSearchCreate } from '@/models'
@@ -20,11 +20,21 @@ export function useSavedFlowRunsSearches(): UseSavedFlowRunsSearches {
   const api = useWorkspaceApi()
   const savedSearchesSubscription = useSubscription(api.savedSearches.getSavedSearches)
   const savedSearches = computed(() => builtinSavedSearches.concat(savedSearchesSubscription.response ?? []))
+
+  const { value: myCustomDefaultSavedSearchFilter, remove: removeDefault } = useCustomDefaultFlowRunsFilter()
+  const hasCustomDefault = computed(() => myCustomDefaultSavedSearchFilter.value !== null)
+  const myDefaultSavedFilter = computed(() => myCustomDefaultSavedSearchFilter.value ?? systemDefaultSavedSearch.filters)
+  const defaultFlowRunsFilter = computed(() => mapper.map('SavedSearchFilter', myDefaultSavedFilter.value, 'FlowRunsFilter'))
+
   const matchedCustomDefaultSavedSearch = computed(() => savedSearches.value.find(search => search.name !== customSavedSearch.name && isSameFilter(search.filters, myDefaultSavedFilter.value)))
   const savedFlowRunsSearches = computed(() => savedSearches.value.map(search => ({ ...search, isDefault: matchedCustomDefaultSavedSearch.value?.name === search.name })))
 
-  // TODO: handle case where customDefault is not in savedSearches
-
+  // handle if customDefault is not in savedSearches i.e. someone else deleted it
+  watch([matchedCustomDefaultSavedSearch, savedSearchesSubscription], ([found, subscription]) => {
+    if (!found && subscription.executed) {
+      removeDefault()
+    }
+  })
 
   async function deleteSavedSearch(savedSearchId: string): Promise<void> {
     await api.savedSearches.deleteSavedSearch(savedSearchId)
@@ -39,11 +49,6 @@ export function useSavedFlowRunsSearches(): UseSavedFlowRunsSearches {
     savedSearchesSubscription.refresh()
     return savedSearch
   }
-
-  const { value: myCustomDefaultSavedSearchFilter, remove: removeDefault } = useCustomDefaultFlowRunsFilter()
-  const hasCustomDefault = computed(() => myCustomDefaultSavedSearchFilter.value !== null)
-  const myDefaultSavedFilter = computed(() => myCustomDefaultSavedSearchFilter.value ?? systemDefaultSavedSearch.filters)
-  const defaultFlowRunsFilter = computed(() => mapper.map('SavedSearchFilter', myDefaultSavedFilter.value, 'FlowRunsFilter'))
   return {
     savedFlowRunsSearches,
     createSavedSearch,
