6f5994c7b1832875e0cca9046630d77f49915c50
Nicholas Brown <znicholasbrown@gmail.com>
2023-10-11 10:42:33 -0400

    Enhancement: Deployment combobox option alignment (#1809)
    * Fix an issue with missing flow ids in deployment mocks
    
    * Simplify combobox option, fix alignment
    
    * Use getter for useFlow id
    
    * Remove unecessary ref

--- src/components/DeploymentComboboxOption.vue
+++ src/components/DeploymentComboboxOption.vue
@@ -1,48 +1,35 @@
 <template>
-  <div ref="element" class="deployment-combobox-option">
-    <span v-if="visible && flowResponse" class="deployment-combobox-option--flow-name">
-      {{ flowResponse.name }}
-      <p-icon icon="ChevronRightIcon" size="small" class="deployment-combobox-option--chevron" />
-    </span>
+  <div class="deployment-combobox-option">
+    <template v-if="flow">
+      <span class="deployment-combobox-option__flow-name">
+        {{ flow.name }}
+      </span>
+      <p-icon icon="ChevronRightIcon" size="small" class="deployment-combobox-option__chevron" />
+    </template>
     {{ deploymentName }}
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { useVisibilityObserver, useSubscriptionWithDependencies } from '@prefecthq/vue-compositions'
-  import { computed, ref } from 'vue'
-  import { useWorkspaceApi } from '@/compositions'
+  import { useFlow } from '@/compositions'
 
   const props = defineProps<{
     flowId: string,
     deploymentName?: string,
   }>()
 
-  const api = useWorkspaceApi()
-  const element = ref<HTMLDivElement>()
-  const { visible } = useVisibilityObserver(element, { disconnectWhenVisible: true })
-
-
-  const flowId = computed(() => props.flowId)
-  const flowArgs = computed<Parameters<typeof api.flows.getFlow> | null>(() => {
-    if (!visible.value) {
-      return null
-    }
-    return [flowId.value]
-  })
-
-  const flowSubscription = useSubscriptionWithDependencies(api.flows.getFlow, flowArgs)
-  const flowResponse = computed(() => flowSubscription.response)
+  const { flow } = useFlow(() => props.flowId)
 </script>
 
 <style>
-.deployment-combobox-option {
-  @apply flex items-center
+.deployment-combobox-option { @apply
+  flex
+  gap-1
+  items-center
 }
-.deployment-combobox-option--flow-name {
-  @apply inline-flex items-center
-}
-.deployment-combobox-option--chevron {
-  @apply w-3 h-3 mr-1
+
+.deployment-combobox-option__chevron { @apply
+  w-3
+  h-3
 }
 </style>
