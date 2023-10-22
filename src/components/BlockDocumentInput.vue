489484d9e456294cde173b3c374a1304460c65fb
Jennifer Grange <jenny@prefect.io>
2023-10-21 22:49:37 -0400

    add handling for single block doc input

--- src/components/BlockDocumentInput.vue
+++ src/components/BlockDocumentInput.vue
@@ -5,23 +5,29 @@
     <template v-if="blockDocuments.length">
       <BlockDocumentCombobox v-model:selected="model" v-bind="{ blockDocuments }" class="block-document-input__select" />
     </template>
-    <p-button v-if="blockTypeSlug" icon-append="PlusIcon" :to="withRedirect(routes.blockCreate(blockTypeSlug))">
+    <p-button v-if="blockTypeSlug" icon-append="PlusIcon" @click="handleOpen">
       Add
     </p-button>
+    <BlockCreateModal v-if="blockType" v-model:showModal="showModal" :provided-block-type="blockType" @refresh="handleRefresh" />
   </div>
 </template>
 
 <script lang="ts" setup>
   import { useSubscription } from '@prefecthq/vue-compositions'
   import { computed } from 'vue'
+  import { useRouter } from 'vue-router'
+  import BlockCreateModal from '@/components/BlockCreateModal.vue'
   import BlockDocumentCombobox from '@/components/BlockDocumentCombobox.vue'
   import LogoImage from '@/components/LogoImage.vue'
   import { useWorkspaceApi, useWorkspaceRoutes } from '@/compositions'
+  import { useShowModal } from '@/compositions/useShowModal'
+  import { BlockDocument } from '@/models'
   import { withRedirect } from '@/utilities/routes'
 
   const props = defineProps<{
     modelValue: string | null | undefined,
     blockTypeSlug: string,
+    useModal?: boolean,
   }>()
 
   const emit = defineEmits<{
@@ -44,6 +50,23 @@
   const blockType = computed(() => blockTypeSubscription.response)
   const blockDocumentsSubscription = useSubscription(api.blockTypes.getBlockDocumentsByBlockTypeSlug, [blockTypeSlug])
   const blockDocuments = computed(() => blockDocumentsSubscription.response ?? [])
+
+  const router = useRouter()
+
+  const { showModal, open, close } = useShowModal()
+
+  const handleOpen = (): void => {
+    if (props.useModal) {
+      open()
+    } else {
+      router.push(withRedirect(routes.blockCreate(props.blockTypeSlug)))
+    }
+  }
+  const handleRefresh = async (blockDocument: BlockDocument): Promise<void> => {
+    model.value = blockDocument.id
+    await blockDocumentsSubscription.refresh()
+    close()
+  }
 </script>
 
 <style>
