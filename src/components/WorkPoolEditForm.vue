eb9a5b09d05ad954da52054d70359543652eb43a
Jennifer Grange <jenny@prefect.io>
2023-11-01 10:49:13 -0400

    simplify

--- src/components/WorkPoolEditForm.vue
+++ src/components/WorkPoolEditForm.vue
@@ -13,7 +13,7 @@
         </template>
       </p-label>
 
-      <p-label v-if="!isPushWorkPool" label="Flow Run Concurrency (Optional)">
+      <p-label v-if="!workPool.isPushPool" label="Flow Run Concurrency (Optional)">
         <template #default="{ id }">
           <p-number-input :id="id" v-model="concurrencyLimit" placeholder="Unlimited" :min="0" />
         </template>
@@ -53,7 +53,6 @@
     workPool: WorkPool,
   }>()
 
-  const isPushWorkPool = computed(() => !!props.workPool.isPushPool)
 
   const api = useWorkspaceApi()
   const router = useRouter()
