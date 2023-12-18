c3704669bd82ca8592980c8dbbfef9b54ad7d371
Craig Harshbarger <pleek91@gmail.com>
2023-12-18 16:36:37 -0600

    Fix the saved search mapper for flow runs

--- src/maps/savedSearchFilter.ts
+++ src/maps/savedSearchFilter.ts
@@ -1,14 +1,13 @@
 import { FlowRunsFilter } from '@/models/Filters'
 import { SavedSearchFilter } from '@/models/SavedSearch'
 import { MapFunction } from '@/services/Mapper'
-import { asArray } from '@/utilities/arrays'
 
 export const mapSavedSearchFilterToFlowRunsFilter: MapFunction<SavedSearchFilter, FlowRunsFilter> = function(source) {
-  const flowIds = source.flow ? asArray(source.flow) : undefined
-  const deploymentIds = source.deployment ? asArray(source.deployment) : undefined
-  const workPoolNames = source.workPool ? asArray(source.workPool) : undefined
-  const tagNames = source.tag ? asArray(source.tag) : undefined
-  const stateNames = source.state ? asArray(source.state) : undefined
+  const flowIds = source.flow.length ? source.flow : undefined
+  const deploymentIds = source.deployment.length ? source.deployment : undefined
+  const workPoolNames = source.workPool.length ? source.workPool : undefined
+  const tagNames = source.tag.length ? source.tag : undefined
+  const stateNames = source.state.length ? source.state : undefined
   const { startDate, endDate } = this.map('DateRangeSelectValue', source.range, 'DateRange')
 
   return {
