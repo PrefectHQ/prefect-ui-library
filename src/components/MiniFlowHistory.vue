fbf7214407a8778c9789576601713c89a30deab3
Jennifer Grange <jenny@prefect.io>
2023-12-20 11:30:02 -0500

    ad type

--- src/components/MiniFlowHistory.vue
+++ src/components/MiniFlowHistory.vue
@@ -10,13 +10,14 @@
   import { computed } from 'vue'
   import { FlowRunsBarChart } from '@/components'
   import { mapper } from '@/services'
+  import { FlowStatsFilter } from '@/types/flow'
 
   const props = defineProps<{
     flowId: string,
     timeSpanInSeconds: number,
   }>()
 
-  const flowStats = computed(() => ({
+  const flowStats = computed<FlowStatsFilter>(() => ({
     flowId: props.flowId,
     range: { type: 'span', seconds: props.timeSpanInSeconds },
   }))
