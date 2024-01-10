cbd75e9672903ac385d8d5861cec7c245a4f8de5
Craig Harshbarger <pleek91@gmail.com>
2024-01-10 17:11:37 -0600

    Add a try catch when mapping saved filters in case of malformed data

--- src/services/WorkspaceSavedSearchesApi.ts
+++ src/services/WorkspaceSavedSearchesApi.ts
@@ -1,3 +1,4 @@
+import { isDefined } from '@prefecthq/prefect-design'
 import { SavedSearchResponse } from '@/models/api/SavedSearchResponse'
 import { SavedSearchesFilter } from '@/models/Filters'
 import { SavedSearch, SavedSearchCreate } from '@/models/SavedSearch'
@@ -11,7 +12,18 @@ export class WorkspaceSavedSearchesApi extends WorkspaceApi {
   public async getSavedSearches(filter: SavedSearchesFilter = {}): Promise<SavedSearch[]> {
     const request = mapper.map('SavedSearchesFilter', filter, 'SavedSearchesFilterRequest')
     const { data } = await this.post<SavedSearchResponse[]>('/filter', request)
-    return mapper.map('SavedSearchResponse', data, 'SavedSearch')
+
+    const mapped = data.map(savedSearch => {
+      try {
+        return mapper.map('SavedSearchResponse', savedSearch, 'SavedSearch')
+      } catch (error) {
+        console.error(error)
+      }
+
+      return undefined
+    }).filter(isDefined)
+
+    return mapped
   }
 
   public async getSavedSearch(id: string): Promise<SavedSearch> {
