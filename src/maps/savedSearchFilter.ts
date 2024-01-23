4990777a5677493b70a2f2474fb182c322621eca
nicholas <znicholasbrown@gmail.com>
2024-01-23 12:49:31 -0500

    Make object filters optional

--- src/maps/savedSearchFilter.ts
+++ src/maps/savedSearchFilter.ts
@@ -3,11 +3,11 @@ import { SavedSearchFilter } from '@/models/SavedSearch'
 import { MapFunction } from '@/services/Mapper'
 
 export const mapSavedSearchFilterToFlowRunsFilter: MapFunction<SavedSearchFilter, FlowRunsFilter> = function(source) {
-  const flowIds = source.flow.length ? source.flow : undefined
-  const deploymentIds = source.deployment.length ? source.deployment : undefined
-  const workPoolNames = source.workPool.length ? source.workPool : undefined
-  const tagNames = source.tag.length ? source.tag : undefined
-  const stateNames = source.state.length ? source.state : undefined
+  const flowIds = source.flow?.length ? source.flow : undefined
+  const deploymentIds = source.deployment?.length ? source.deployment : undefined
+  const workPoolNames = source.workPool?.length ? source.workPool : undefined
+  const tagNames = source.tag?.length ? source.tag : undefined
+  const stateNames = source.state?.length ? source.state : undefined
   const { startDate, endDate } = this.map('DateRangeSelectValue', source.range, 'DateRange')
 
   return {
