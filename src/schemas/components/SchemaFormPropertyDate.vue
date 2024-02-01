e23aebce2c64098d47b63bba29a84cb51f72d003
Craig Harshbarger <pleek91@gmail.com>
2024-02-01 14:43:01 -0600

    WIP on string formats

---
+++ src/schemas/components/SchemaFormPropertyDate.vue
@@ -0,0 +1,26 @@
+<template>
+  <p-date-input v-model="value" />
+</template>
+
+<script lang="ts" setup>
+  import { computed } from 'vue'
+  import { SchemaProperty } from '@/schemas/types/schema'
+
+  const props = defineProps<{
+    property: SchemaProperty & { type: 'string', format: 'date' },
+    value: string | null,
+  }>()
+
+  const emit = defineEmits<{
+    'update:value': [string | null],
+  }>()
+
+  const value = computed({
+    get() {
+      return props.value
+    },
+    set(value) {
+      emit('update:value', value)
+    },
+  })
+</script>
