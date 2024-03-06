dcfead02beae3c8807904a2db6296e9ec06035f5
Craig Harshbarger <pleek91@gmail.com>
2024-03-06 14:03:51 -0600

    Make sure boolean properties default to false

---
+++ src/schemas/components/SchemaFormPropertyBoolean.vue
@@ -0,0 +1,27 @@
+<template>
+  <p-toggle v-model="value" :state="state" />
+</template>
+
+<script lang="ts" setup>
+  import { State, isDefined } from '@prefecthq/prefect-design'
+  import { SchemaProperty } from '@/schemas/types/schema'
+
+  defineProps<{
+    // eslint-disable-next-line vue/no-unused-properties
+    property: SchemaProperty & { type: 'boolean' },
+    state: State,
+  }>()
+
+  const value = defineModel<boolean | undefined>({ default: undefined })
+
+  if (!isDefined(value.value)) {
+    value.value = false
+  }
+</script>
+
+<style>
+.schema-form-property-null { @apply
+  text-subdued
+  text-sm
+}
+</style>
