50e60c0d4d006857d5f60d14dcbc53e4e72106aa
nicholas <znicholasbrown@gmail.com>
2024-03-07 19:35:49 -0500

    Use flowruns filter instead of task runs filter

--- src/services/UiApi.ts
+++ src/services/UiApi.ts
@@ -1,6 +1,6 @@
 import { UiFlowRunHistoryResponse } from '@/models/api/UiFlowRunHistoryResponse'
 import { UiTaskRunCountsByStateResponse } from '@/models/api/UiTaskRunCountsByStateResponse'
-import { FlowRunsFilter } from '@/models/Filters'
+import { FlowRunsFilter, TaskRunsFilter } from '@/models/Filters'
 import { UiFlowRunHistory } from '@/models/UiFlowRunHistory'
 import { UiTaskRunCountsByState } from '@/models/UiTaskRunCountsByState'
 import { mapper } from '@/services/Mapper'
@@ -20,8 +20,8 @@ export class UiApi extends WorkspaceApi implements IUiApi {
     return mapper.map('UiFlowRunHistoryResponse', data, 'UiFlowRunHistory')
   }
 
-  public async getTaskRunsCountByState(filter: FlowRunsFilter): Promise<UiTaskRunCountsByState> {
-    const request = mapper.map('FlowRunsFilter', filter, 'FlowRunsFilterRequest')
+  public async getTaskRunsCountByState(filter: TaskRunsFilter): Promise<UiTaskRunCountsByState> {
+    const request = mapper.map('TaskRunsFilter', filter, 'TaskRunsFilterRequest')
     const { data } = await this.post<UiTaskRunCountsByStateResponse>('/task_runs/count', request)
 
     return mapper.map('UiTaskRunCountsByStateResponse', data, 'UiTaskRunCountsByState')
