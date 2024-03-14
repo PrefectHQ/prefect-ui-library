96d24b19163ea1b9aca22d104233ea876320ad1a
nicholas <znicholasbrown@gmail.com>
2024-03-14 14:45:37 -0400

    Add new flexible icon text count component

---
+++ src/components/IconTextCount.vue
@@ -0,0 +1,24 @@
+<template>
+  <p-icon-text :icon="icon" class="icon-text-count">
+    <template v-if="count > 0">
+      {{ count }} {{ formattedLabel }}
+    </template>
+    <template v-else>
+      None
+    </template>
+  </p-icon-text>
+</template>
+
+<script lang="ts" setup>
+  import { Icon } from '@prefecthq/prefect-design'
+  import { computed } from 'vue'
+  import { toPluralString } from '@/utilities'
+
+  const props = defineProps<{
+    count: number,
+    label?: string,
+    icon: Icon,
+  }>()
+
+  const formattedLabel = computed(() => props.label ? toPluralString(props.label, props.count) : '')
+</script>
