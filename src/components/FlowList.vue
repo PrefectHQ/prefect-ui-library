c915192b1442584a97081237fd407d7ad7dec41d
Jennifer Grange <jenny@prefect.io>
2024-03-25 14:29:39 -0400

    add to flows

--- src/components/FlowList.vue
+++ src/components/FlowList.vue
@@ -64,7 +64,7 @@
       </template>
 
       <template #empty-state>
-        <PEmptyResults>
+        <PEmptyResults v-if="subscriptions.executed">
           <template #message>
             No flows
           </template>
@@ -72,6 +72,11 @@
             <p-button small @click="clear">
               Clear Filters
             </p-button>
+          </template>
+        </PEmptyResults>
+        <PEmptyResults v-else>
+          <template #message>
+            Loading...
           </template>
         </PEmptyResults>
       </template>
