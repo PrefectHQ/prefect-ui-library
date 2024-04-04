0bfa2f00ac731b79aed40c56b5b99c1d1b03036c
Craig Harshbarger <pleek91@gmail.com>
2024-04-04 10:58:55 -0500

    Move validation logic into oss

--- src/services/WorkspaceAutomationsApi.ts
+++ src/services/WorkspaceAutomationsApi.ts
@@ -1,8 +1,11 @@
+import { isAxiosError, AxiosError } from 'axios'
 import { AutomationResponse } from '@/automations/types/api/automation'
 import { Automation } from '@/automations/types/automation'
 import { AutomationsFilter } from '@/automations/types/filter'
 import { mapper } from '@/services/Mapper'
 import { WorkspaceApi } from '@/services/WorkspaceApi'
+import { Require } from '@/types/utilities'
+import { httpStatus } from '@/utilities/httpStatus'
 
 export class WorkspaceAutomationsApi extends WorkspaceApi {
 
@@ -14,4 +17,36 @@ export class WorkspaceAutomationsApi extends WorkspaceApi {
     return mapper.map('AutomationResponse', data, 'Automation')
   }
 
+  public async validateTemplate(template: string): Promise<string | true> {
+    try {
+      await this.post('/templates/validate', template)
+
+      return true
+    } catch (error) {
+      if (isInvalidAutomationTemplateError(error)) {
+        const { line, message } = error.response.data.error
+
+        return `Error on line ${line}: ${message} `
+      }
+
+      throw error
+    }
+  }
+
+}
+
+type InvalidAutomationTemplateError = {
+  error: {
+    line: number,
+    message: string,
+    source: string,
+  },
+}
+
+function isInvalidAutomationTemplateError(error: unknown): error is Require<AxiosError<InvalidAutomationTemplateError>, 'response'> {
+  if (!isAxiosError(error)) {
+    return false
+  }
+
+  return httpStatus(error).is('UnprocessableEntity')
 }
