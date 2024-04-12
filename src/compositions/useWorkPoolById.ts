11f732b9764b6e44ce95accbdd5e64618d532e3c
Jennifer Grange <jenny@prefect.io>
2024-04-11 20:27:06 -0400

    add to api instead

--- src/compositions/useWorkPoolById.ts
+++ src/compositions/useWorkPoolById.ts
@@ -1,19 +1,18 @@
 import { useSubscriptionWithDependencies } from '@prefecthq/vue-compositions'
 import { computed, MaybeRefOrGetter, toRef, toValue } from 'vue'
-import { WorkPoolsFilter } from '..'
 import { useCan } from '@/compositions/useCan'
 import { useWorkspaceApi } from '@/compositions/useWorkspaceApi'
 import { WorkspaceWorkPoolsApi } from '@/services/WorkspaceWorkPoolsApi'
 import { Getter } from '@/types/reactivity'
 import { UseEntitySubscription } from '@/types/useEntitySubscription'
 
-export type UseWorkPoolId = UseEntitySubscription<WorkspaceWorkPoolsApi['getWorkPools'], 'workPool'>
+export type UseWorkPoolById = UseEntitySubscription<WorkspaceWorkPoolsApi['getWorkPoolById'], 'workPool'>
 
-export function useWorkPoolId(workPoolId: MaybeRefOrGetter<string | null | undefined>): UseWorkPoolId {
+export function useWorkPoolById(workPoolId: MaybeRefOrGetter<string | null | undefined>): UseWorkPoolById {
   const api = useWorkspaceApi()
   const can = useCan()
 
-  const getter: Getter<[WorkPoolsFilter] | null> = () => {
+  const getter: Getter<[string] | null> = () => {
     if (!can.read.work_pool) {
       return null
     }
@@ -24,18 +23,12 @@ export function useWorkPoolId(workPoolId: MaybeRefOrGetter<string | null | undef
       return null
     }
 
-    return [
-      {
-        workPools: {
-          id: [id],
-        },
-      },
-    ]
+    return [id]
   }
 
   const parameters = toRef(getter)
 
-  const subscription = useSubscriptionWithDependencies(api.workPools.getWorkPools, parameters)
+  const subscription = useSubscriptionWithDependencies(api.workPools.getWorkPoolById, parameters)
   const workPool = computed(() => subscription.response)
 
   return {
