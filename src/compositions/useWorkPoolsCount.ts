712282c7341bf60634ce9baa437215c1ef4b3eb1
Dylan Hughes <dylanbhughes@gmail.com>
2024-04-12 11:56:57 -0400

    include permissions check in useWorkPoolsCount

--- src/compositions/useWorkPoolsCount.ts
+++ src/compositions/useWorkPoolsCount.ts
@@ -1,18 +1,30 @@
-import { useSubscription } from '@prefecthq/vue-compositions'
-import { computed, MaybeRefOrGetter, toRef } from 'vue'
+import { useSubscriptionWithDependencies } from '@prefecthq/vue-compositions'
+import { computed, MaybeRefOrGetter, toRef, toValue } from 'vue'
+import { useCan } from '@/compositions/useCan'
 import { useWorkspaceApi } from '@/compositions/useWorkspaceApi'
 import { WorkPoolsFilter } from '@/models'
 import { WorkspaceWorkPoolsApi } from '@/services/WorkspaceWorkPoolsApi'
+import { Getter } from '@/types/reactivity'
 import { UseEntitySubscription } from '@/types/useEntitySubscription'
 
 export type UseWorkPoolsCount = UseEntitySubscription<WorkspaceWorkPoolsApi['getWorkPoolsCount'], 'workPoolCount'>
 
 export function useWorkPoolsCount(filter: MaybeRefOrGetter<WorkPoolsFilter> = {}): UseWorkPoolsCount {
   const api = useWorkspaceApi()
+  const can = useCan()
 
-  const filterRef = toRef(filter)
+  const getter: Getter<[WorkPoolsFilter] | null> = () => {
+    if (!can.read.work_pool) {
+      return null
+    }
 
-  const subscription = useSubscription(api.workPools.getWorkPoolsCount, [filterRef])
+    const filterValue = toValue(filter)
+
+    return [filterValue]
+  }
+
+  const parameters = toRef(getter)
+  const subscription = useSubscriptionWithDependencies(api.workPools.getWorkPoolsCount, parameters)
   const workPoolCount = computed(() => subscription.response)
 
   return {
