5aa91b78e0f1408e62256000702b9c9b0a3ee271
Craig Harshbarger <pleek91@gmail.com>
2024-04-15 15:25:07 -0500

    Tweak the run deployment action description

--- src/automations/components/AutomationActionDescriptionRunDeployment.vue
+++ src/automations/components/AutomationActionDescriptionRunDeployment.vue
@@ -1,9 +1,7 @@
 <template>
-  <p-content class="automation-action-description-run-deployment" v-bind="$attrs">
+  <div class="automation-action-description-run-deployment" v-bind="$attrs">
     <template v-if="action.deploymentId">
-      <template v-if="deployment">
-        <span>Run <p-link :to="routes.deployment(deployment.id)">{{ deployment.name }}</p-link> deployment</span>
-      </template>
+      <span class="automation_action-description-run-deployment__deployment">Run deployment: <DeploymentIconText :deployment-id="action.deploymentId" /></span>
 
       <p-button small @click="openParametersModal">
         Show parameters
@@ -19,7 +17,7 @@
     <template v-else>
       <span>Run deployment inferred from the triggering event</span>
     </template>
-  </p-content>
+  </div>
 
   <p-modal v-model:show-modal="showParametersModal" title="Parameters">
     <p-code-highlight :text="stringify(action.parameters)" lang="json" />
@@ -33,19 +31,17 @@
 <script lang="ts" setup>
   import { useBoolean } from '@prefecthq/vue-compositions'
   import { AutomationActionRunDeployment } from '@/automations/types/actions'
-  import { useDeployment, useWorkspaceRoutes } from '@/compositions'
+  import DeploymentIconText from '@/components/DeploymentIconText.vue'
   import { stringify } from '@/utilities'
 
   defineOptions({
     inheritAttrs: false,
   })
 
-  const props = defineProps<{
+  defineProps<{
     action: AutomationActionRunDeployment,
   }>()
 
-  const routes = useWorkspaceRoutes()
-  const { deployment } = useDeployment(() => props.action.deploymentId)
   const { value: showParametersModal, setTrue: openParametersModal } = useBoolean()
   const { value: showJobVariablesModal, setTrue: openJobVariablesModal } = useBoolean()
 </script>
@@ -55,6 +51,13 @@
   flex
   flex-wrap
   gap-2
+  items-center
+}
+
+.automation_action-description-run-deployment__deployment { @apply
+  flex
+  flex-wrap
+  gap-1
   items-center
 }
 </style>
