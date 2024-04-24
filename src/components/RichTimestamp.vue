a13050f23ffcbd5482dc0819b2a715a25a2ac728
nicholas <znicholasbrown@gmail.com>
2024-04-24 16:15:11 -0400

    Add rich timestamp component

---
+++ src/components/RichTimestamp.vue
@@ -0,0 +1,68 @@
+<template>
+  <p-tooltip class="rich-timestamp" :disabled="tooltipDisabled">
+    <template #content>
+      <slot>
+        <div class="rich-timestamp__tooltip">
+          {{ timestamp }}
+        </div>
+      </slot>
+    </template>
+
+    <slot name="text" v-bind="{ formattedText }">
+      {{ formattedText }}
+    </slot>
+  </p-tooltip>
+</template>
+
+<script lang="ts" setup>
+  import { computed } from 'vue'
+  import { formatDateTimeRelative, secondsToApproximateString, isDate, isNumber, formatDateTimeNumeric } from '@/utilities'
+
+  const props = defineProps<{
+    timestamp?: Date | number | null | undefined | string,
+    format?: 'relative' | 'approximate' | 'numeric',
+    disabled?: boolean,
+  }>()
+
+  const tooltipDisabled = computed(() => props.disabled)
+
+  const formattedText = computed(() => {
+    if (isDate(props.timestamp)) {
+      switch (props.format) {
+        case 'numeric':
+          return formatDateTimeNumeric(props.timestamp)
+        case 'approximate':
+        case 'relative':
+        default:
+          return formatDateTimeRelative(props.timestamp)
+      }
+    }
+
+    if (isNumber(props.timestamp)) {
+      switch (props.format) {
+        case 'relative':
+          return formatDateTimeRelative(new Date(props.timestamp))
+        case 'numeric':
+          return formatDateTimeNumeric(new Date(props.timestamp))
+        case 'approximate':
+        default:
+          return secondsToApproximateString(props.timestamp)
+      }
+    }
+
+    return props.timestamp
+  })
+</script>
+
+<style>
+.rich-timestamp { @apply
+  font-mono
+}
+
+.rich-timestamp__tooltip { @apply
+  text-xs
+  font-mono
+  px-1
+  py-0.5
+}
+</style>
