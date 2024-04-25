85c3d6959f9c1d43419aec8f4597a40f343a9f50
nicholas <znicholasbrown@gmail.com>
2024-04-24 20:30:50 -0400

    Search placeholder change :eyes:

--- src/components/DeploymentList.vue
+++ src/components/DeploymentList.vue
@@ -6,7 +6,7 @@
       <DeploymentsDeleteButton v-if="can.delete.deployment" :selected="selectedDeployments.map(deployment => deployment.id)" small @delete="deleteDeployments" />
 
       <template #controls>
-        <SearchInput v-model="deploymentNameLike" small placeholder="Search deployment names..." class="deployment-list__search-input" label="Search deployments" />
+        <SearchInput v-model="deploymentNameLike" small placeholder="Search deployments..." class="deployment-list__search-input" label="Search deployments" />
         <DeploymentTagsInput v-model:selected="filter.deployments.tags.name" small multiple />
       </template>
 
