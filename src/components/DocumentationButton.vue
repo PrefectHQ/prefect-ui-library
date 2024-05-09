cb0dc629cf22eb3c5162c65bfa64a030923334d0
Craig Harshbarger <pleek91@gmail.com>
2024-05-09 11:36:28 -0400

    Clean up documentation button

--- src/components/DocumentationButton.vue
+++ src/components/DocumentationButton.vue
@@ -1,11 +1,9 @@
 <template>
-  <a :href="to" class="documentation-button" target="_blank">
-    <p-button icon-append="ArrowTopRightOnSquareIcon">
-      <slot>
-        View Docs
-      </slot>
-    </p-button>
-  </a>
+  <p-button :to class="documentation-button" target="_blank" icon-append="ArrowTopRightOnSquareIcon">
+    <slot>
+      View Docs
+    </slot>
+  </p-button>
 </template>
 
 <script lang="ts" setup>
