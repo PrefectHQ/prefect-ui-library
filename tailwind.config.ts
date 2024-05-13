f08b2531d14bf05649910b70989d6e25fc53c75a
nicholas <znicholasbrown@gmail.com>
2024-05-13 17:42:44 -0400

    Fix tailwind config import

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
 import prefectDesignConfig from '@prefecthq/prefect-design/tailwind.config'
 import { Config } from 'tailwindcss/types/config'
-import baseConfig from '@/tailwind.config'
+
+// eslint-disable-next-line no-relative-import-paths/no-relative-import-paths
+import baseConfig from './src/tailwind.config'
 
 const config: Config = {
   content: ['./src/**/*.{vue,js,ts}'],
   presets: [prefectDesignConfig, baseConfig],
 }
 
+// eslint-disable-next-line import/no-default-export
 export default config
