8c96588ad7494d1d1ef2c973686905fb78a8c4d4
Will Raphaelson <william_raphaelson@alumni.brown.edu>
2024-05-13 16:30:29 -0500

    adding catchup modal

---
+++ src/components/CatchupDescriptionModal.vue
@@ -0,0 +1,10 @@
+<template>
+  <ExtraInfoModal title="Catchup">
+    When catchup is set to true, the scheduler will submit any runs in a state state for execution. When catchup is set to false,
+    the scheduler will only submit runs for execution that are scheduled to run in the future, cancelling and skipping any late runs.
+  </ExtraInfoModal>
+</template>
+
+<script lang="ts" setup>
+  import ExtraInfoModal from '@/components/ExtraInfoModal.vue'
+</script>
