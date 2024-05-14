1658e6c19582ea4a9bb5635f6e369980b3aaaaac
Will Raphaelson <william_raphaelson@alumni.brown.edu>
2024-05-13 22:18:28 -0500

    tightening up info modal copy

--- src/components/CatchupDescriptionModal.vue
+++ src/components/CatchupDescriptionModal.vue
@@ -1,7 +1,7 @@
 <template>
   <ExtraInfoModal title="Catchup">
-    When catchup is set to true, the scheduler will submit any runs in a state state for execution. When catchup is set to false,
-    the scheduler will only submit runs for execution that are scheduled to run in the future, cancelling and skipping any late runs.
+    When catchup is set to true, the scheduler will submit any runs in a late state for execution. When catchup is set to false,
+    the scheduler will only submit runs for execution that are scheduled to run now or in the future, cancelling and skipping any late runs.
   </ExtraInfoModal>
 </template>
 
