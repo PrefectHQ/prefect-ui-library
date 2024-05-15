7fb022ff32ef267a4b12591a24aee5533764e886
nicholas <znicholasbrown@gmail.com>
2024-05-15 18:42:53 -0400

    Add new page heading runs component

---
+++ src/components/PageHeadingRuns.vue
@@ -0,0 +1,34 @@
+<template>
+  <page-heading class="page-heading-runs" :crumbs="crumbs">
+    <template v-if="!hideActions" #actions>
+      <SavedFilters v-model:filter="filter" />
+    </template>
+  </page-heading>
+</template>
+
+<script lang="ts" setup>
+  import { computed } from 'vue'
+  import PageHeading from '@/components/PageHeading.vue'
+  import SavedFilters from '@/components/SavedFilters.vue'
+  import { SavedSearchFilter } from '@/models/SavedSearch'
+
+  const props = defineProps<{
+    filter: SavedSearchFilter,
+    hideActions?: boolean,
+  }>()
+
+  const emit = defineEmits<{
+    'update:filter': [SavedSearchFilter],
+  }>()
+
+  const crumbs = [{ text: 'Runs' }]
+
+  const filter = computed({
+    get() {
+      return props.filter
+    },
+    set(filter) {
+      emit('update:filter', filter)
+    },
+  })
+</script>
