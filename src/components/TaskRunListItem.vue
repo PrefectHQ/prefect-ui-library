87a03602429d04084299ddc3c840eba6d3bcd338
nicholas <znicholasbrown@gmail.com>
2024-05-15 21:15:46 -0400

    Show flow run info on task run list items if relevant

--- src/components/TaskRunListItem.vue
+++ src/components/TaskRunListItem.vue
@@ -1,52 +1,54 @@
 <template>
-  <StateListItem v-model:selected="model" v-bind="{ selectable, value, tags, stateType }" class="task-run-list-item">
-    <template #name>
-      <div class="task-run-list-item__breadcrumbs">
-        <template v-if="showFlow && flow">
-          <p-link :to="routes.flow(flow.id)">
-            {{ flow.name }}
+  <div ref="el" class="task-run-list-item">
+    <StateListItem v-model:selected="model" v-bind="{ selectable, value, tags, stateType }">
+      <template #name>
+        <div class="task-run-list-item__breadcrumbs">
+          <template v-if="showFlow && flow">
+            <p-link :to="routes.flow(flow.id)">
+              {{ flow.name }}
+            </p-link>
+            <p-icon icon="ChevronRightIcon" size="small" />
+          </template>
+
+          <template v-if="showFlowRun && flowRun">
+            <p-link :to="routes.flowRun(flowRun.id)">
+              {{ flowRun.name }}
+            </p-link>
+            <p-icon icon="ChevronRightIcon" size="small" />
+          </template>
+
+          <p-link :to="routes.taskRun(taskRun.id)">
+            <span>{{ taskRun.name }}</span>
           </p-link>
-          <p-icon icon="ChevronRightIcon" size="small" />
-        </template>
+        </div>
+      </template>
+      <template #meta>
+        <StateBadge :state="taskRun.state" />
+        <p-icon-text icon="ClockIcon">
+          {{ secondsToApproximateString(taskRun.duration) }}
+        </p-icon-text>
+        <p-icon-text class="flow-run-date-icon-text" icon="CalendarIcon">
+          <template v-if="taskRun.startTime">
+            {{ formatDateTimeNumeric(taskRun.startTime) }}
+          </template>
+          <template v-else-if="taskRun.expectedStartTime">
+            {{ formatDateTimeNumeric(taskRun.expectedStartTime) }}
+          </template>
+        </p-icon-text>
+      </template>
 
-        <template v-if="showFlowRun && flowRun">
-          <p-link :to="routes.flowRun(flowRun.id)">
-            {{ flowRun.name }}
-          </p-link>
-          <p-icon icon="ChevronRightIcon" size="small" />
-        </template>
-
-        <p-link :to="routes.taskRun(taskRun.id)">
-          <span>{{ taskRun.name }}</span>
-        </p-link>
-      </div>
-    </template>
-    <template #meta>
-      <StateBadge :state="taskRun.state" />
-      <p-icon-text icon="ClockIcon">
-        {{ secondsToApproximateString(taskRun.duration) }}
-      </p-icon-text>
-      <p-icon-text class="flow-run-date-icon-text" icon="CalendarIcon">
-        <template v-if="taskRun.startTime">
-          {{ formatDateTimeNumeric(taskRun.startTime) }}
-        </template>
-        <template v-else-if="taskRun.expectedStartTime">
-          {{ formatDateTimeNumeric(taskRun.expectedStartTime) }}
-        </template>
-      </p-icon-text>
-    </template>
-
-    <template v-if="showFlowRun && visible" #relationships>
-      <FlowRunDeployment v-if="flowRun?.deploymentId" :deployment-id="flowRun.deploymentId" />
-      <FlowRunWorkPool v-if="flowRun?.workPoolName" :work-pool-name="flowRun.workPoolName" />
-      <FlowRunWorkQueue
-        v-if="flowRun?.workQueueName"
-        :work-queue-name="flowRun.workQueueName"
-        :work-pool-name="flowRun.workPoolName"
-        :flow-run-state="flowRun.stateType"
-      />
-    </template>
-  </StateListItem>
+      <template v-if="showFlowRun && visible" #relationships>
+        <FlowRunDeployment v-if="flowRun?.deploymentId" :deployment-id="flowRun.deploymentId" />
+        <FlowRunWorkPool v-if="flowRun?.workPoolName" :work-pool-name="flowRun.workPoolName" />
+        <FlowRunWorkQueue
+          v-if="flowRun?.workQueueName"
+          :work-queue-name="flowRun.workQueueName"
+          :work-pool-name="flowRun.workPoolName"
+          :flow-run-state="flowRun.stateType"
+        />
+      </template>
+    </StateListItem>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -54,6 +56,9 @@
   import { useIntersectionObserver } from '@prefecthq/vue-compositions'
   import { computed, onMounted, ref } from 'vue'
   import { useRoute } from 'vue-router'
+  import FlowRunDeployment from '@/components/FlowRunDeployment.vue'
+  import FlowRunWorkPool from '@/components/FlowRunWorkPool.vue'
+  import FlowRunWorkQueue from '@/components/FlowRunWorkQueue.vue'
   import StateBadge from '@/components/StateBadge.vue'
   import StateListItem from '@/components/StateListItem.vue'
   import { useFlow, useFlowRun, useWorkspaceRoutes } from '@/compositions'
