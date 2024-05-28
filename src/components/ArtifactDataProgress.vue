d3448a4852d5f46612313f900865ba994eaa6c24
Dylan Hughes <dylanbhughes@gmail.com>
2024-05-28 16:05:34 -0400

    progress artifact data component

---
+++ src/components/ArtifactDataProgress.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="artifact-data-progress">
+    Progress: {{ Math.trunc(artifact.data) }}%
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { ProgressArtifact } from '@/models'
+
+  defineProps<{
+    artifact: ProgressArtifact,
+  }>()
+</script>
+
+<style>
+.artifact-data-progress { @apply
+  max-w-full
+}
+</style>
