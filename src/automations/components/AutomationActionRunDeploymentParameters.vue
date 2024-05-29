5c9675825a899025f193309111996ca19b44f724
Craig Harshbarger <pleek91@gmail.com>
2024-05-29 11:58:33 -0500

    Add a dismissable banner

--- src/automations/components/AutomationActionRunDeploymentParameters.vue
+++ src/automations/components/AutomationActionRunDeploymentParameters.vue
@@ -2,26 +2,35 @@
   <p-content class="automation-action-run-deployment-parameters">
     <SchemaInputV2 v-model:values="parameters" :schema :errors :kinds="['none', 'json', 'jinja', 'workspace_variable']" skip-default-value-initialization>
       <template #default="{ kind, setKind }">
-        <div class="automation-action-run-deployment-parameters__header">
-          <h3 class="deployment-form__heading">
-            Parameters
-          </h3>
-          <p-icon-button-menu small>
-            <p-overflow-menu-item v-if="kind !== 'none'" label="Use form input" @click="setKind('none')" />
-            <p-overflow-menu-item v-if="kind !== 'json'" label="Use JSON input" @click="setKind('json')" />
-            <p-overflow-menu-item v-if="kind !== 'jinja'" label="Use Jinja input" @click="setKind('jinja')" />
-          </p-icon-button-menu>
-        </div>
+        <p-content secondary>
+          <div class="automation-action-run-deployment-parameters__header">
+            <h3 class="deployment-form__heading">
+              Parameters
+            </h3>
+            <p-icon-button-menu small>
+              <p-overflow-menu-item v-if="kind !== 'none'" label="Use form input" @click="setKind('none')" />
+              <p-overflow-menu-item v-if="kind !== 'json'" label="Use JSON input" @click="setKind('json')" />
+              <p-overflow-menu-item v-if="kind !== 'jinja'" label="Use Jinja input" @click="setKind('jinja')" />
+            </p-icon-button-menu>
+          </div>
+
+          <template v-if="!dismissed">
+            <p-message dismissible @dismiss="dismiss">
+              Parameters defined for an automation action will be merged with the deployment's default parameters when the action is executed.
+            </p-message>
+          </template>
+        </p-content>
       </template>
     </SchemaInputV2>
   </p-content>
 </template>
 
 <script lang="ts" setup>
-  import { ValidationRule, useValidation } from '@prefecthq/vue-compositions'
+  import { ValidationRule, useLocalStorage, useValidation } from '@prefecthq/vue-compositions'
   import { computed } from 'vue'
   import { Deployment } from '@/models/Deployment'
   import { SchemaValuesV2, useSchemaValidationV2, SchemaInputV2, SchemaV2 } from '@/schemas'
+  import { getCacheKey } from '@/utilities'
   import { isRecord } from '@/utilities/object'
   import { timeout } from '@/utilities/time'
 
@@ -82,6 +91,12 @@
   }
 
   useValidation(parameters, isValidParameters)
+
+  const { value: dismissed, set: setDismissed } = useLocalStorage('automation-action-run-deployment-parameters-merge-message', false)
+
+  function dismiss(): void {
+    setDismissed(true)
+  }
 </script>
 
 <style>
