40422c7291f11dbd6b7c0fa0320817172a8a5676
Dylan Hughes <dylanbhughes@gmail.com>
2024-05-29 15:00:26 -0400

    ArtifactDataImage component

---
+++ src/components/ArtifactDataImage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="artifact-data-image">
+    <img loading="eager" :src="artifact.data">
+    <br>
+    <p>
+      <span>Image URL: </span>
+      <p-link :to="artifact.data">
+        {{ artifact.data }}
+      </p-link>
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { ImageArtifact } from '@/models'
+
+  defineProps<{
+    artifact: ImageArtifact,
+  }>()
+</script>
+
+<style>
+.artifact-data-image { @apply
+  max-w-full
+}
+</style>
