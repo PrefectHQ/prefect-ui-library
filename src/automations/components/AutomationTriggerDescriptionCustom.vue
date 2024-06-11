fbf61a8b5117603cf0b769b451eb0d8dbcf93266
Craig Harshbarger <pleek91@gmail.com>
2024-06-11 13:11:38 -0500

    Add a component for custom triggers

---
+++ src/automations/components/AutomationTriggerDescriptionCustom.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="automation-trigger-description-custom">
+    A custom trigger
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { AutomationTrigger } from '@/automations/types/triggers'
+
+  defineProps<{
+    trigger: AutomationTrigger,
+  }>()
+</script>
+
+<style>
+
+</style>
