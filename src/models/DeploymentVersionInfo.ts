e32d3e85cd3097b0cc203d5537507ff58bab0633
Devin Villarosa <devinvillarosa92.business@gmail.com>
2025-04-17 10:13:40 -0700

    enhancement: Updates DeploymentVersionInfo typings

--- src/models/DeploymentVersionInfo.ts
+++ src/models/DeploymentVersionInfo.ts
@@ -1,7 +1,36 @@
-export type DeploymentVersionInfo = {
-  type: string,
-  base: string | null,
-  branch: string | null,
+// Ref: prefect repo src/prefect/versioning/py
+
+type SimpleVersionInfo = {
+  type: 'prefect:simple',
   version: string,
-  url: string | null,
-} & Record<string, unknown>
+  branch?: string | null,
+  url?: string | null,
+}
+
+type GithubVersionInfo = {
+  type: 'vcs:github',
+  version: string,
+  branch: string,
+  url: string,
+  repository: string,
+}
+
+type GitVersionInfo = {
+  type: 'vcs:git',
+  version: string,
+  branch: string,
+  url: string,
+  repository: string,
+}
+
+type DockerVersionInfo = {
+  type: 'container:docker',
+  version: string,
+  branch: string,
+  url: string,
+  image_name: string,
+  registry: string,
+  image: string,
+}
+
+export type DeploymentVersionInfo = SimpleVersionInfo | GithubVersionInfo | GitVersionInfo | DockerVersionInfo
