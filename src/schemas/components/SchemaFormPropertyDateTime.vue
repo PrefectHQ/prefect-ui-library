3adb92abdbe8c9406364f771b96a90778a6c24da
nicholas <znicholasbrown@gmail.com>
2025-06-17 14:57:42 -0400

    Add timezone selector

--- src/schemas/components/SchemaFormPropertyDateTime.vue
+++ src/schemas/components/SchemaFormPropertyDateTime.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="schema-form-property-date-time">
-    <p-date-input v-model="value" show-time clearable />
+    <div class="schema-form-property-date-time__input-container">
+      <p-date-input v-model="value" show-time clearable class="schema-form-property-date-time__date-input" />
+      <TimezoneSelect v-model="timezone" class="schema-form-property-date-time__timezone-select" hide-unset />
+    </div>
     <p-code v-if="normalizedValue">
       {{ normalizedValue }}
     </p-code>
@@ -10,7 +13,8 @@
 <script lang="ts" setup>
   import { isNotNullish } from '@prefecthq/prefect-design'
   import { formatDate } from 'date-fns'
-  import { computed } from 'vue'
+  import { computed, onBeforeMount, ref, watchEffect } from 'vue'
+  import { TimezoneSelect } from '@/components'
   import { isInvalidDate } from '@/utilities'
 
   const props = defineProps<{
@@ -22,10 +26,22 @@
   }>()
 
   const DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+
   // Note that this handles fractional seconds
   const TIMEZONE_REGEX = /(\.\d{1,9})?([+-]\d{2}:?\d{2}|Z)$/
 
+  const timezone = ref<string>('UTC')
   const normalizedValue = computed(() => getNormalizedValue(props.value))
+
+  watchEffect(() => {
+    console.log('timezone', timezone.value)
+  })
+
+  onBeforeMount(() => {
+    if (isNotNullish(props.value)) {
+      timezone.value = getTimezoneNameFromString(props.value) ?? 'UTC'
+    }
+  })
 
   const value = computed({
     get() {
@@ -57,6 +73,16 @@
     return value.replace(TIMEZONE_REGEX, '$1')
   }
 
+  function getTimezoneNameFromString(value: string): string | null {
+    const date = new Date(value)
+
+    if (isInvalidDate(date)) {
+      return null
+    }
+
+    return date.toLocaleString('en-US', { timeZoneName: 'short' }).split(' ')[3]
+  }
+
   function getNormalizedValue(value: Date | string | null | undefined): string | null | undefined {
     if (value instanceof Date) {
       return formatDate(value, DATE_FORMAT)
@@ -76,3 +102,26 @@
     return value
   }
 </script>
+
+<style>
+.schema-form-property-date-time { @apply
+  flex
+  flex-col
+  gap-2
+}
+
+.schema-form-property-date-time__input-container { @apply
+  flex
+  gap-2
+}
+
+.schema-form-property-date-time__date-input { @apply
+  grow
+  shrink-0
+}
+
+.schema-form-property-date-time__timezone-select { @apply
+  w-1/4
+  shrink-0
+}
+</style>
